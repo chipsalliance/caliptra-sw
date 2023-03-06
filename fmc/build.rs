a8016d701c0d411601e4e84b5e7ba46fba666f5a
Anthony Rocha <Anthony.Rocha@amd.com>
2023-03-05 18:22:54 -0800

    Add linker ,build scripts and startup code
    Add start.S
    
    Add build.rs
    
    Change build.rs

---
+++ fmc/build.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    // Put the linker script somewhere the linker can find it.
+    fs::write(out_dir.join("memory.x"), include_bytes!("memory.x")).unwrap();
+    println!("cargo:rustc-link-search={}", out_dir.display());
+
+    println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rustc-link-arg=-Tmemory.x");
+
+    println!("cargo:rustc-link-arg=-Tfmc/link.x");
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
