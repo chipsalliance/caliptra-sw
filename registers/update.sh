a3311b978953a832999393af533daa4c0f2ab960
Kor Nielsen <kor@google.com>
2023-03-07 00:21:11 -0800

    Update register generator to use caliptra-rtl submodule.

---
+++ registers/update.sh
@@ -0,0 +1,11 @@
+# Licensed under the Apache-2.0 license
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [[ ! -f "../hw-latest/caliptra-rtl/.git" ]]; then
+    echo "caliptra-rtl submodule is not populated"
+    echo "Please run 'git submodule update --init'"
+    exit 1
+fi
+
+cargo run --release --manifest-path bin/generator/Cargo.toml -- ../hw-latest/caliptra-rtl src/
