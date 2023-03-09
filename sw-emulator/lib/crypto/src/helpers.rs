fe30d500c50d48cd566719c04b86cfb377870175
Kor Nielsen <kor@google.com>
2023-03-08 16:02:32 -0800

    Add additional unit tests to sw-emulator.
    These are necessary to guard against regressions in future refactoring
    commits that remove clippy lint warnings.

--- sw-emulator/lib/crypto/src/helpers.rs
+++ sw-emulator/lib/crypto/src/helpers.rs
@@ -1,5 +1,7 @@
 // Licensed under the Apache-2.0 license
 
+// TODO: These names need to make it clearer what they're actually doing. It's
+// swapping the bytes of every 4-byte chunk, regardless of the element type.
 pub trait EndianessTransform {
     fn change_endianess(&mut self);
     fn to_big_endian(&mut self);
@@ -73,5 +75,31 @@ impl EndianessTransform for [u64] {
 
     fn to_little_endian(&mut self) {
         self.change_endianess();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::EndianessTransform;
+
+    #[test]
+    fn test_change_endianness_u8() {
+        let mut val = [0x11_u8, 0x22, 0x33, 0x44, 0x99, 0xaa, 0xbb, 0xcc];
+        val.change_endianess();
+        assert_eq!([0x44, 0x33, 0x22, 0x11, 0xcc, 0xbb, 0xaa, 0x99], val);
+    }
+
+    #[test]
+    fn test_change_endianness_u32() {
+        let mut val = [0x1122_3344_u32, 0x99aa_bbcc];
+        val.change_endianess();
+        assert_eq!([0x4433_2211, 0xccbb_aa99], val);
+    }
+
+    #[test]
+    fn test_change_endianness_u64() {
+        let mut val = [0x1122_3344_5566_7788_u64, 0x99aa_bbcc_ddee_ff00];
+        val.change_endianess();
+        assert_eq!([0x4433_2211_8877_6655, 0xccbb_aa99_00ff_eedd], val);
     }
 }
