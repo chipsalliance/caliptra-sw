0472318781f43c171de9b661f430ceb3b6c01935
Kor Nielsen <kor@google.com>
2023-03-13 23:23:55 -0700

    Add integration tests for drivers crate.
    This means that all the risc-v driver tests will execute as part of
    "cargo test".

---
+++ drivers/tests/integration_tests.rs
@@ -0,0 +1,76 @@
+// Licensed under the Apache-2.0 license
+
+use std::io::stdout;
+
+use caliptra_hw_model::{HwModel, InitParams};
+
+fn run_driver_test(test_bin_name: &str) {
+    let rom =
+        caliptra_builder::build_firmware_rom("caliptra-drivers-test-bin", test_bin_name).unwrap();
+    let mut model = caliptra_hw_model::create(InitParams {
+        rom: &rom,
+        ..Default::default()
+    })
+    .unwrap();
+    model.copy_output_until_exit_success(stdout()).unwrap();
+}
+
+#[test]
+fn test_doe() {
+    run_driver_test("doe");
+}
+
+#[test]
+fn test_ecc384() {
+    run_driver_test("ecc384");
+}
+
+#[test]
+fn test_error_reporter() {
+    run_driver_test("error_reporter");
+}
+
+#[test]
+fn test_hmac384() {
+    run_driver_test("hmac384");
+}
+
+#[test]
+fn test_keyvault() {
+    run_driver_test("keyvault");
+}
+
+#[test]
+fn test_mailbox() {
+    run_driver_test("mailbox");
+}
+
+#[test]
+fn test_pcrbank() {
+    run_driver_test("pcrbank");
+}
+
+#[test]
+fn test_sha1() {
+    run_driver_test("sha1");
+}
+
+#[test]
+fn test_sha256() {
+    run_driver_test("sha256");
+}
+
+#[test]
+fn test_sha384() {
+    run_driver_test("sha384");
+}
+
+#[test]
+fn test_sha384acc() {
+    run_driver_test("sha384acc");
+}
+
+#[test]
+fn test_status_reporter() {
+    run_driver_test("status_reporter");
+}
