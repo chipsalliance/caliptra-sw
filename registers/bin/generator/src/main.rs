e0cb4ff9461dda33b52294c8d08f042ccb2092a1
Royce Rajan <roycerajan@google.com>
2023-03-20 10:28:15 -0700

    Add TRNG and entropy_src RDL files

--- registers/bin/generator/src/main.rs
+++ registers/bin/generator/src/main.rs
@@ -29,6 +29,8 @@ static CALIPTRA_RDL_FILES: &[&str] = &[
     "src/doe/rtl/doe_reg.rdl",
     "src/ecc/rtl/ecc_reg.rdl",
     "src/hmac/rtl/hmac_reg.rdl",
+    "src/csrng/data/csrng.rdl",
+    "src/entropy_src/data/entropy_src.rdl",
     "src/sha256/rtl/sha256_reg.rdl",
     "src/sha512/rtl/sha512_reg.rdl",
     "src/soc_ifc/rtl/mbox_csr.rdl",
