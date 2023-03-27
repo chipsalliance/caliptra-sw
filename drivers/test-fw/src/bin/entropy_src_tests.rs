21262ff298db626c54c63ee4f9488234cd5ed23b
Kor Nielsen <kor@google.com>
2023-03-27 15:41:52 -0700

    Cleanup test case

--- drivers/test-fw/src/bin/entropy_src_tests.rs
+++ drivers/test-fw/src/bin/entropy_src_tests.rs
@@ -6,21 +6,20 @@ mod harness;
 use caliptra_registers::entropy_src;
 
 fn test_success() {
-    println!("Hello.");
-
     let esrc = entropy_src::RegisterBlock::entropy_src_reg();
 
     const FALSE: u32 = 9;
     const TRUE: u32 = 6;
 
-    // esrc.conf().write(|_| 0x909099.into());
-    // esrc.module_enable().write(|_| 6.into());
-
-    esrc.entropy_control().write(|w| w.es_route(TRUE));
-
+    esrc.conf().write(
+        |w| w.entropy_data_reg_enable(TRUE)
+            .fips_enable(FALSE)
+            .rng_bit_enable(FALSE)
+            .rng_bit_sel(0)
+            .threshold_scope(TRUE));
+    esrc.entropy_control().write(|w| w.es_route(TRUE)
+                                      .es_type(TRUE));
     esrc.module_enable().write(|w| w.module_enable(TRUE));
-
-    println!("L");
 
     for _ in 0..100 {
         core::hint::black_box(esrc);
