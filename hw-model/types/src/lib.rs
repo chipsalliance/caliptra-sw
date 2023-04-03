3f28e579069b67980e88ef03767d49ec0f2b9526
Kor Nielsen <kor@google.com>
2023-04-02 22:23:54 -0700

    Add Fuses struct to hw-model-types
    This will be used to represent the complete set of fuses in the run
    parameters.

--- hw-model/types/src/lib.rs
+++ hw-model/types/src/lib.rs
@@ -1,8 +1,9 @@
 // Licensed under the Apache-2.0 license
 
 // Based on device_lifecycle_e from RTL
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
 pub enum DeviceLifecycle {
+    #[default]
     Unprovisioned = 0b00,
     Manufacturing = 0b01,
     Reserved2 = 0b10,
@@ -19,6 +20,11 @@ impl TryFrom<u32> for DeviceLifecycle {
             0b11 => Ok(Self::Production),
             _ => Err(()),
         }
+    }
+}
+impl From<DeviceLifecycle> for u32 {
+    fn from(value: DeviceLifecycle) -> Self {
+        value as u32
     }
 }
 
@@ -55,6 +61,65 @@ impl SecurityState {
         self.0 |= (val as u32) & 0x3;
         self
     }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Default)]
+pub enum U4 {
+    #[default]
+    X0 = 0x0,
+    X1 = 0x1,
+    X2 = 0x2,
+    X3 = 0x3,
+    X4 = 0x4,
+    X5 = 0x5,
+    X6 = 0x6,
+    X7 = 0x7,
+    X8 = 0x8,
+    X9 = 0x9,
+    Xa = 0xa,
+    Xb = 0xb,
+    Xc = 0xc,
+    Xd = 0xd,
+    Xe = 0xe,
+    Xf = 0xf,
+}
+impl U4 {
+    pub const B0000: Self = Self::X0;
+    pub const B0001: Self = Self::X1;
+    pub const B0010: Self = Self::X2;
+    pub const B0011: Self = Self::X3;
+    pub const B0100: Self = Self::X4;
+    pub const B0101: Self = Self::X5;
+    pub const B0110: Self = Self::X6;
+    pub const B0111: Self = Self::X7;
+    pub const B1000: Self = Self::X8;
+    pub const B1001: Self = Self::X9;
+    pub const B1010: Self = Self::Xa;
+    pub const B1011: Self = Self::Xb;
+    pub const B1100: Self = Self::Xc;
+    pub const B1101: Self = Self::Xd;
+    pub const B1110: Self = Self::Xe;
+    pub const B1111: Self = Self::Xf;
+}
+impl From<U4> for u32 {
+    fn from(value: U4) -> Self {
+        value as u32
+    }
+}
+
+#[derive(Default)]
+pub struct Fuses {
+    pub uds_seed: [u32; 12],
+    pub field_entropy: [u32; 8],
+    pub key_manifest_pk_hash: [u32; 12],
+    pub key_manifest_pk_hash_mask: U4,
+    pub owner_pk_hash: [u32; 12],
+    pub fmc_key_manifest_svn: u32,
+    pub runtime_svn: [u32; 4],
+    pub anti_rollback_disable: bool,
+    pub idevid_cert_attr: [u32; 24],
+    pub idevid_manuf_hsm_id: [u32; 4],
+    pub life_cycle: DeviceLifecycle,
 }
 
 #[cfg(test)]
