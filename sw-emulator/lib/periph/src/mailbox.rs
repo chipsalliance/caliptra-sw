chipsalliance/caliptra-sw

commit 1e1748b257d5bce247f91f9177b1faa351330424

Fix mailbox incoming message notification. #129 (#132)
Don't use mbox.status.status to detect incoming messages, and stop using
it in test cases to signal an incoming message, as this is incorrect
according to the integration spec. See issue #129 for more details.

sw-emulator/lib/periph/src/mailbox.rs
@@ -19,8 +19,8 @@ use caliptra_emu_derive::Bus;
 use caliptra_emu_types::{RvAddr, RvData, RvSize};
 use std::{cell::RefCell, rc::Rc};
 use tock_registers::interfaces::{ReadWriteable, Readable, Writeable};
-use tock_registers::register_bitfields;
 use tock_registers::registers::InMemoryRegister;
+use tock_registers::{register_bitfields, LocalRegisterCopy};
 
 /// Maximum mailbox capacity.
 const MAX_MAILBOX_CAPACITY_BYTES: usize = 128 << 10;
@@ -358,13 +358,26 @@ impl MailboxRegs {
 
     // Todo: Implement write status callback fn
     pub fn write_status(&mut self, _size: RvSize, val: RvData) -> Result<(), BusError> {
-        self.state_machine.context.status = val;
+        let val = LocalRegisterCopy::<u32, Status::Register>::new(val);
+        self.state_machine
+            .context
+            .status
+            .write(Status::STATUS.val(val.read(Status::STATUS)));
         Ok(())
     }
 
     // Todo: Implement read status callback fn
     pub fn read_status(&self, _size: RvSize) -> Result<u32, BusError> {
-        Ok(self.state_machine.context.status)
+        let mut result = self.state_machine.context.status;
+        result.modify(match self.state_machine.state {
+            // TODO: What about MBOX_EXECUTE_SOC?
+            States::Exec => Status::MBOX_FSM_PS::MBOX_EXECUTE_UC,
+            States::Idle => Status::MBOX_FSM_PS::MBOX_IDLE,
+            States::RdyForCmd => Status::MBOX_FSM_PS::MBOX_RDY_FOR_CMD,
+            States::RdyForData => Status::MBOX_FSM_PS::MBOX_RDY_FOR_DATA,
+            States::RdyForDlen => Status::MBOX_FSM_PS::MBOX_RDY_FOR_DLEN,
+        });
+        Ok(result.get())
     }
 }
 
@@ -410,7 +423,7 @@ pub struct Context {
     /// Fifo storage
     pub ring_buffer: RingBuffer,
     /// Mailbox Status
-    status: u32,
+    status: LocalRegisterCopy<u32, Status::Register>,
     /// Command
     pub cmd: u32,
     // data_out
@@ -424,7 +437,7 @@ impl Context {
             user: 0,
             exec: false,
             dlen: 0,
-            status: 0,
+            status: LocalRegisterCopy::new(0),
             ring_buffer: RingBuffer::new(ram),
             cmd: 0,
             data_out: 0,
@@ -590,7 +603,10 @@ mod tests {
         ));
 
         let status = mb.read(RvSize::Word, Mailbox::OFFSET_STATUS).unwrap();
-        assert_eq!(status, Status::STATUS::DATA_READY.value);
+        assert_eq!(
+            status,
+            (Status::STATUS::DATA_READY + Status::MBOX_FSM_PS::MBOX_EXECUTE_UC).value
+        );
 
         let cmd = mb.read(RvSize::Word, Mailbox::OFFSET_CMD).unwrap();
         assert_eq!(cmd, 0x55);
@@ -764,7 +780,10 @@ mod tests {
         ));
 
         let status = mb.read(RvSize::Word, Mailbox::OFFSET_STATUS).unwrap();
-        assert_eq!(status, Status::STATUS::DATA_READY.value);
+        assert_eq!(
+            status,
+            (Status::STATUS::DATA_READY + Status::MBOX_FSM_PS::MBOX_EXECUTE_UC).value
+        );
 
         let cmd = mb.read(RvSize::Word, Mailbox::OFFSET_CMD).unwrap();
         assert_eq!(cmd, 0x55);

drivers/src/mailbox.rs
@@ -13,6 +13,7 @@ Abstract:
 --*/
 
 use crate::{caliptra_err_def, CaliptraResult};
+use caliptra_registers::mbox::enums::MboxFsmE;
 use caliptra_registers::mbox::{self, enums::MboxStatusE};
 use core::cmp::min;
 use core::mem::size_of;
@@ -69,8 +70,8 @@ impl Mailbox {
     /// * 'MailboxRecvTxn' - Object representing a receive operation
     pub fn try_start_recv_txn(&self) -> Option<MailboxRecvTxn> {
         let mbox = mbox::RegisterBlock::mbox_csr();
-        match mbox.status().read().status() {
-            MboxStatusE::DataReady => Some(MailboxRecvTxn::default()),
+        match mbox.status().read().mbox_fsm_ps() {
+            MboxFsmE::MboxExecuteUc => Some(MailboxRecvTxn::default()),
             _ => None,
         }
     }

hw-model/src/lib.rs
@@ -210,11 +210,6 @@ pub trait HwModel {
             self.soc_mbox().datain().write(|_| last_word);
         }
 
-        // Set the status as DATA_READY.
-        self.soc_mbox()
-            .status()
-            .write(|w| w.status(|w| w.data_ready()));
-
         // Set Execute Bit
         self.soc_mbox().execute().write(|w| w.execute(true));
 
@@ -354,7 +349,6 @@ mod tests {
             caliptra_hw_model::FW_LOAD_CMD_OPCODE
         );
         assert_eq!(model.soc_mbox().dlen().read(), firmware.len() as u32);
-        assert!(model.soc_mbox().status().read().status().data_ready());
 
         // Read the data out of the mailbox.
         let mut temp: Vec<u32> = Vec::new();

sw-emulator/app/src/main.rs
@@ -298,9 +298,6 @@ fn upload_fw_to_mailbox(mailbox: &mut Mailbox, firmware_buffer: Rc<Vec<u8>>) {
         let _ = mailbox.write_datain(last_word);
     }
 
-    // Set the status as DATA_READY.
-    let _ = mailbox.set_status_data_ready();
-
     // Set the execute register.
     let _ = mailbox.write_execute(1);
 }

commit 090a72e51603541463968232c8215fb1339b59ff

emu: Make CSR hacks in soc_ifc compatible with fix for #129
When flowstatus.[il]devid_csr_ready fields are set, the boot ROM enters
a special mode that signs a ldevid/idevid CSR and places it in the
mailbox. The emulator's soc_ifc peripheral has some hacks in the
implementation that check for this, and download the CSR from the
mailbox into a file "/tmp/caliptra_?devid_cert.der". Unfortunately, this
logic was broken by #132, as I didn't realize this hack exited at the
time. To fix:

  * The hack should look at mbox_fsm_ps field instead of status field to
    detect an incoming message from the Caliptra microcontroller.

  * The hack should set the status field to signal the microcontroller
    that it has read the incoming message

sw-emulator/lib/periph/src/mailbox.rs
@@ -172,6 +172,10 @@ impl Mailbox {
         Ok(())
     }
 
+    pub fn is_command_exec_requested(&self) -> bool {
+        matches!(self.regs.borrow_mut().state_machine.state, States::Exec)
+    }
+
     pub fn is_status_data_ready(&mut self) -> bool {
         self.match_status(Status::STATUS::DATA_READY.value)
     }

sw-emulator/lib/periph/src/soc_reg.rs
@@ -897,7 +897,7 @@ impl SocRegistersImpl {
     }
 
     fn download_idev_id_csr(&mut self) {
-        if !self.mailbox.is_status_data_ready() {
+        if !self.mailbox.is_command_exec_requested() {
             return;
         }
 
@@ -909,7 +909,7 @@ impl SocRegistersImpl {
     }
 
     fn download_ldev_id_cert(&mut self) {
-        if !self.mailbox.is_status_data_ready() {
+        if !self.mailbox.is_command_exec_requested() {
             return;
         }
 
@@ -941,6 +941,7 @@ impl SocRegistersImpl {
                 file.write_all(&[byte]).unwrap();
             }
         }
+        self.mailbox.set_status_cmd_complete().unwrap();
     }
 
     fn reset_common(&mut self) {