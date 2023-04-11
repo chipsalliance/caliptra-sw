a38b928c8d954e74e3140d9d4e82a787c67570a0
Kor Nielsen <kor@google.com>
2023-04-11 10:48:51 -0700

    Test that the HwModel handles ICCM NMIs correctly.
    This ensures that both the RTL and sw-emulator behave the same after a
    non-word or unaligned write to ICCM.

---
+++ hw-model/test-fw/test_iccm_unaligned_write.rs
@@ -0,0 +1,30 @@
+// Licensed under the Apache-2.0 license
+
+#![no_main]
+#![no_std]
+
+use ::core::arch::global_asm;
+
+// Needed to bring in startup code
+#[allow(unused)]
+use caliptra_test_harness;
+
+#[panic_handler]
+pub fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+global_asm!(
+    r#"
+main:
+    lui t0, 0x40000
+    sw x0, 1(t0)
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    ret
+"#
+);
