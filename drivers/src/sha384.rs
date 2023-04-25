6aa1bd877a62c0f8b41642b55b42c63485065d38
Kor Nielsen <kor@google.com>
2023-04-25 14:46:29 -0700

    Fix #197: intermittent corruption in sha384 driver. (#198)

--- drivers/src/sha384.rs
+++ drivers/src/sha384.rs
@@ -134,11 +134,11 @@ impl Sha384 {
     ///
     /// * `buf` - Digest buffer
     fn copy_digest_to_buf(&self, buf: &mut Array4x12) -> CaliptraResult<()> {
-        *buf = Array4x12::read_from_reg(
-            sha512::RegisterBlock::sha512_reg()
-                .digest()
-                .truncate::<12>(),
-        );
+        let sha = sha512::RegisterBlock::sha512_reg();
+        // digest_block() only waits until the peripheral is ready for the next
+        // command; the result register may not be valid yet
+        wait::until(|| sha.status().read().valid());
+        *buf = Array4x12::read_from_reg(sha.digest().truncate::<12>());
         Ok(())
     }
 
