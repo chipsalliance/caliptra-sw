47d2aa7144b03f45f88499be1a01d3384d342d8b
Anthony Rocha <anthony.rocha@amd.com>
2023-05-14 21:46:07 -0700

    try to restore the correct folder

---
+++ hw-latest/verilated/caliptra_verilated.cpp
@@ -0,0 +1,85 @@
+/*++
+Licensed under the Apache-2.0 license.
+--*/
+#include "caliptra_verilated.h"
+
+#include "Vcaliptra_verilated.h"
+#include "verilated_vcd_c.h"
+
+struct caliptra_verilated {
+  Vcaliptra_verilated v;
+  std::unique_ptr<VerilatedVcdC> tfp;
+  uint64_t sim_time = 0;
+};
+
+struct caliptra_verilated* caliptra_verilated_new(struct caliptra_verilated_init_args* init_args) {
+  auto result = new caliptra_verilated();
+  result->v.security_state = init_args->security_state;
+  return result;
+}
+void caliptra_verilated_destroy(struct caliptra_verilated* model) {
+  if (model->tfp.get()) {
+    model->tfp->close();
+  }
+  delete model;
+}
+
+void caliptra_verilated_trace(struct caliptra_verilated* model,
+                              const char* vcd_out_path, int depth) {
+  Verilated::traceEverOn(vcd_out_path ? true : false);
+  if (model->tfp.get()) {
+    model->tfp->close();
+  }
+  model->tfp.reset(NULL);
+
+  if (vcd_out_path) {
+    model->tfp.reset(new VerilatedVcdC());
+
+    model->v.trace(model->tfp.get(), depth);
+    model->tfp->open(vcd_out_path);
+  }
+}
+
+void caliptra_verilated_eval(struct caliptra_verilated* model,
+                             const struct caliptra_verilated_sig_in* in,
+                             struct caliptra_verilated_sig_out* out) {
+  Vcaliptra_verilated* v = &model->v;
+
+  v->eval();
+
+  v->core_clk = in->core_clk;
+
+  v->cptra_pwrgood = in->cptra_pwrgood;
+  v->cptra_rst_b = in->cptra_rst_b;
+
+  v->paddr = in->paddr;
+  v->pprot = in->pprot;
+  v->psel = in->psel;
+  v->penable = in->penable;
+  v->pwrite = in->pwrite;
+  v->pwdata = in->pwdata;
+  v->pauser = in->pauser;
+
+  v->ext_imem_we = in->imem_we;
+  v->ext_imem_addr = in->imem_addr;
+  v->ext_imem_wdata = in->imem_wdata;
+
+  v->itrng_data = in->itrng_data;
+  v->itrng_valid = in->itrng_valid;
+
+  if (model->tfp.get()) {
+    model->tfp->dump(model->sim_time++);
+  }
+
+  out->ready_for_fuses = v->ready_for_fuses;
+  out->ready_for_fw_push = v->ready_for_fw_push;
+
+  out->pready = v->pready;
+  out->pslverr = v->pslverr;
+  out->prdata = v->prdata;
+
+  out->generic_load_en = v->generic_load_en;
+  out->generic_load_data = v->generic_load_data;
+
+  out->etrng_req = v->etrng_req;
+}
---
+++ hw-latest/verilated/src/lib.rs
@@ -0,0 +1,203 @@
+/*++
+Licensed under the Apache-2.0 license.
+--*/
+mod bindings;
+use std::ffi::CString;
+use std::ffi::NulError;
+use std::ptr::null;
+
+pub use bindings::caliptra_verilated_init_args as InitArgs;
+pub use bindings::caliptra_verilated_sig_in as SigIn;
+pub use bindings::caliptra_verilated_sig_out as SigOut;
+
+pub type GenericLoadCallbackFn = dyn Fn(&CaliptraVerilated, u8);
+
+pub struct CaliptraVerilated {
+    v: *mut bindings::caliptra_verilated,
+    pub input: SigIn,
+    pub output: SigOut,
+    generic_load_cb: Box<GenericLoadCallbackFn>,
+    total_cycles: u64,
+}
+
+impl CaliptraVerilated {
+    /// Constructs a new model.
+    pub fn new(args: InitArgs) -> Self {
+        Self::with_generic_load_cb(args, Box::new(|_, _| {}))
+    }
+
+    /// Creates a model that calls `generic_load_cb` whenever the
+    /// microcontroller CPU does a load to the generic wires.
+    #[allow(clippy::type_complexity)]
+    pub fn with_generic_load_cb(
+        mut args: InitArgs,
+        generic_load_cb: Box<GenericLoadCallbackFn>,
+    ) -> Self {
+        unsafe {
+            Self {
+                v: bindings::caliptra_verilated_new(&mut args),
+                input: Default::default(),
+                output: Default::default(),
+                generic_load_cb,
+                total_cycles: 0,
+            }
+        }
+    }
+
+    /// Returns the total number of cycles since simulation start
+    pub fn total_cycles(&self) -> u64 {
+        self.total_cycles
+    }
+
+    /// Starts tracing to VCD file `path`, with SystemVerilog module depth
+    /// `depth`. If tracing was previously started to another file, that file
+    /// will be closed and all new traces will be written to this file.
+    pub fn start_tracing(&mut self, path: &str, depth: i32) -> Result<(), NulError> {
+        unsafe {
+            bindings::caliptra_verilated_trace(self.v, CString::new(path)?.as_ptr(), depth);
+        }
+        Ok(())
+    }
+
+    /// Stop any tracing that might have been previously started with `start_tracing()`.
+    pub fn stop_tracing(&mut self) {
+        unsafe {
+            bindings::caliptra_verilated_trace(self.v, null(), 0);
+        }
+    }
+
+    /// Evaluates the model into self.output, then copies all `self.input`
+    /// signals into psuedo flip-flops that will be visible to always_ff blocks
+    /// in subsequent evaluations. Typically `next_cycle_high` is used instead.
+    pub fn eval(&mut self) {
+        unsafe { bindings::caliptra_verilated_eval(self.v, &self.input, &mut self.output) }
+        if self.output.generic_load_en && self.input.core_clk {
+            (self.generic_load_cb)(self, self.output.generic_load_data as u8);
+        }
+    }
+
+    /// Toggles core_clk until there have been `n_cycles` rising edges.
+    pub fn next_cycle_high(&mut self, n_cycles: u32) {
+        for _ in 0..n_cycles {
+            self.total_cycles += 1;
+            loop {
+                self.input.core_clk = !self.input.core_clk;
+                self.eval();
+                if self.input.core_clk {
+                    break;
+                }
+            }
+        }
+    }
+
+    /// Writes a ROM image to the RAM backing the "fake ROM". Typically this should be
+    /// done before asserting cptra_pwrgood and cptra_rst_b.
+    pub fn write_rom_image(&mut self, image: &[u8]) {
+        // TODO: bounds check length against ROM size?
+        for (addr, data) in image.chunks_exact(8).enumerate() {
+            // panic is impossible because an 8-byte slice-ref will always
+            // convert into an 8-byte array-ref.
+            let data = u64::from_le_bytes(data.try_into().unwrap());
+            self.write_rom_u64(addr as u32, data);
+        }
+    }
+
+    /// Initiates a read transaction on the SoC->Caliptra APB bus with user
+    /// `pauser` and `addr`, and returns the word read from the bus.
+    pub fn apb_read_u32(&mut self, pauser: u32, addr: u32) -> u32 {
+        self.input.paddr = addr;
+        self.input.psel = true;
+        self.input.penable = false;
+        self.input.pwrite = false;
+        self.input.pauser = pauser;
+
+        self.next_cycle_high(1);
+
+        self.input.penable = true;
+
+        loop {
+            self.next_cycle_high(1);
+            if self.output.pready {
+                break;
+            }
+        }
+
+        self.input.psel = false;
+        self.input.penable = false;
+
+        self.output.prdata
+    }
+
+    /// Initiates a write transaction on the SoC->Caliptra APB bus with user
+    /// `pauser`, `addr` and `data`.
+    pub fn apb_write_u32(&mut self, pauser: u32, addr: u32, data: u32) {
+        self.input.paddr = addr;
+        self.input.psel = true;
+        self.input.penable = false;
+        self.input.pwrite = true;
+        self.input.pwdata = data;
+        self.input.pauser = pauser;
+
+        self.next_cycle_high(1);
+
+        self.input.penable = true;
+
+        loop {
+            self.next_cycle_high(1);
+            if self.output.pready {
+                break;
+            }
+        }
+
+        self.input.psel = false;
+        self.input.penable = false;
+        self.input.pwrite = false;
+        self.input.pwdata = 0;
+    }
+
+    fn write_rom_u64(&mut self, addr: u32, data: u64) {
+        self.input.imem_addr = addr;
+        self.input.imem_wdata = data;
+        self.input.imem_we = true;
+        self.next_cycle_high(1);
+        self.input.imem_we = false;
+    }
+}
+impl Drop for CaliptraVerilated {
+    fn drop(&mut self) {
+        unsafe { bindings::caliptra_verilated_destroy(self.v) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::*;
+
+    // Additional tests in hw-model/src/model_verilated.rs
+
+    #[test]
+    fn test_tracing() {
+        if !cfg!(feature = "verilator") {
+            return;
+        }
+        let mut v = CaliptraVerilated::new(InitArgs { security_state: 0 });
+
+        std::fs::remove_file("/tmp/caliptra_verilated_test.vcd").ok();
+        std::fs::remove_file("/tmp/caliptra_verilated_test2.vcd").ok();
+        assert!(!Path::new("/tmp/caliptra_verilated_test.vcd").exists());
+        assert!(!Path::new("/tmp/caliptra_verilated_test2.vcd").exists());
+
+        v.start_tracing("/tmp/caliptra_verilated_test.vcd", 99)
+            .unwrap();
+        v.next_cycle_high(2);
+        v.start_tracing("/tmp/caliptra_verilated_test2.vcd", 99)
+            .unwrap();
+        v.next_cycle_high(2);
+        assert!(Path::new("/tmp/caliptra_verilated_test.vcd").exists());
+        assert!(Path::new("/tmp/caliptra_verilated_test2.vcd").exists());
+        v.stop_tracing();
+        v.next_cycle_high(2);
+    }
+}
