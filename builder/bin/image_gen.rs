62e9af18909456d621b7c54ec2b2066f543ac2ca
fdamato <83837719+fdamato@users.noreply.github.com>
2023-05-25 12:29:27 -0700

    Add support for image_builder and C smoke test to take rom and fw binary image from command line (#303)
    Co-authored-by: Kor Nielsen <kor@google.com>

--- builder/bin/image_gen.rs
+++ builder/bin/image_gen.rs
@@ -1,9 +1,19 @@
 // Licensed under the Apache-2.0 license
 
 use caliptra_builder::{ImageOptions, APP_WITH_UART, FMC_WITH_UART, ROM_WITH_UART};
+use clap::{arg, value_parser, Command};
 use std::io::Write;
+use std::path::PathBuf;
 
 fn main() {
+    let args = Command::new("caliptra-emu")
+        .about("Caliptra emulator")
+        .arg(arg!(--"rom" <FILE> "ROM binary image").value_parser(value_parser!(PathBuf)))
+        .arg(arg!(--"fw" <FILE> "FW bundle image").value_parser(value_parser!(PathBuf)))
+        .get_matches();
+    let rom_path = args.get_one::<PathBuf>("rom").unwrap();
+    let fw_path = args.get_one::<PathBuf>("fw").unwrap();
+
     // Generate ROM Image
     let rom = caliptra_builder::build_firmware_rom(&ROM_WITH_UART).unwrap();
 
@@ -15,18 +25,11 @@ fn main() {
     )
     .unwrap();
 
-    // Create "out" folder if doesn`t exist
-    if !std::path::Path::new("out").exists() {
-        std::fs::create_dir("out").unwrap();
-    }
-
     // Generate ROM Binary
-    let _ = std::fs::File::create("out/caliptra-rom.bin")
-        .unwrap()
-        .write_all(&rom);
+    let _ = std::fs::File::create(rom_path).unwrap().write_all(&rom);
 
     // Generate Image Bundle Binary
-    let _ = std::fs::File::create("out/image-bundle.bin")
+    let _ = std::fs::File::create(fw_path)
         .unwrap()
         .write_all(&image.to_bytes().unwrap());
 }
