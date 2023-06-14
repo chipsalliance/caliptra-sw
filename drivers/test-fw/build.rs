ab8647a49d18800a9677cf689aa6ea8a1c5be55d
Kor Nielsen <kor@google.com>
2023-06-14 00:03:12 -0700

    Improve DOE driver tests with strict key assertions.
    As part of generating test vectors, I've included a standalone
    implementation (DoeTestVectors::generate()) of the cryptographic
    operations necessary to generate the expected DOE test's HMAC results
    from fuse values and silicon secrets, using only openssl.

--- drivers/test-fw/build.rs
+++ drivers/test-fw/build.rs
@@ -13,6 +13,8 @@ Abstract:
 --*/
 
 fn main() {
-    println!("cargo:rerun-if-changed=../../test-harness/scripts/rom.ld");
-    println!("cargo:rustc-link-arg=-Ttest-harness/scripts/rom.ld");
+    if cfg!(feature = "riscv") {
+        println!("cargo:rerun-if-changed=../../test-harness/scripts/rom.ld");
+        println!("cargo:rustc-link-arg=-Ttest-harness/scripts/rom.ld");
+    }
 }
--- drivers/tests/integration_tests.rs
+++ drivers/tests/integration_tests.rs
@@ -3,17 +3,26 @@
 use std::error::Error;
 
 use caliptra_builder::FwId;
-use caliptra_hw_model::{BootParams, DefaultHwModel, HwModel, InitParams, ModelError};
+use caliptra_drivers_test_bin::DoeTestResults;
+use caliptra_hw_model::{
+    BootParams, DefaultHwModel, DeviceLifecycle, HwModel, InitParams, ModelError, SecurityState,
+};
 use caliptra_registers::mbox::enums::MboxStatusE;
+use openssl::{hash::MessageDigest, pkey::PKey};
+use zerocopy::{transmute, AsBytes, FromBytes};
 
-fn start_driver_test(test_bin_name: &'static str) -> Result<DefaultHwModel, Box<dyn Error>> {
-    let rom = caliptra_builder::build_firmware_rom(&FwId {
+fn build_test_rom(test_bin_name: &'static str) -> Vec<u8> {
+    caliptra_builder::build_firmware_rom(&FwId {
         crate_name: "caliptra-drivers-test-bin",
         bin_name: test_bin_name,
         features: &["emu"],
         ..Default::default()
     })
-    .unwrap();
+    .unwrap()
+}
+
+fn start_driver_test(test_bin_name: &'static str) -> Result<DefaultHwModel, Box<dyn Error>> {
+    let rom = build_test_rom(test_bin_name);
     caliptra_hw_model::new(BootParams {
         init_params: InitParams {
             rom: &rom,
@@ -29,9 +38,271 @@ fn run_driver_test(test_bin_name: &'static str) {
     model.step_until_exit_success().unwrap();
 }
 
+struct DoeInput {
+    // The DOE obfuscation key, as wired to caliptra_top
+    doe_obf_key: [u32; 8],
+
+    // The DOE initialization vector, as given to the DOE_IV register by the
+    // firmware.
+    doe_iv: [u8; 16],
+
+    // The UDS seed, as stored in the fuses
+    uds_seed: [u32; 12],
+
+    // The field entropy, as stored in the fuses
+    field_entropy_seed: [u32; 8],
+
+    // The initial value of key-vault entry words at startup
+    keyvault_initial_word_value: u32,
+}
+
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+struct DoeTestVectors {
+    // The decrypted UDS as stored in the key vault
+    uds: [u32; 12],
+
+    // The decrypted field entropy as stored in the key vault (with padding)
+    field_entropy: [u32; 12],
+
+    // The expected results of the HMAC operations performed by the test.
+    expected_test_results: DoeTestResults,
+}
+impl DoeTestVectors {
+    /// A standalone implementation of the cryptographic operations necessary to
+    /// generate the expected DOE test's HMAC results from fuse values and
+    /// silicon secrets, using only openssl. This independent implementation is
+    /// used to validate that the test-vector constants are correct.
+    fn generate(input: &DoeInput) -> Self {
+        use openssl::{cipher::Cipher, cipher_ctx::CipherCtx, sign::Signer};
+
+        fn swap_word_bytes(words: &[u32]) -> Vec<u32> {
+            words.iter().map(|word| word.swap_bytes()).collect()
+        }
+        fn swap_word_bytes_inplace(words: &mut [u32]) {
+            for word in words.iter_mut() {
+                *word = word.swap_bytes()
+            }
+        }
+        fn hmac384(key: &[u8], data: &[u8]) -> [u8; 48] {
+            let pkey = PKey::hmac(key).unwrap();
+            let mut signer = Signer::new(MessageDigest::sha384(), &pkey).unwrap();
+            signer.update(data).unwrap();
+            let mut result = [0u8; 48];
+            signer.sign(&mut result).unwrap();
+            result
+        }
+        fn aes256_decrypt_blocks(key: &[u8], iv: &[u8], input: &[u8]) -> Vec<u8> {
+            let cipher = Cipher::aes_256_cbc();
+            let mut ctx = CipherCtx::new().unwrap();
+            ctx.decrypt_init(Some(cipher), Some(key), Some(iv)).unwrap();
+            ctx.set_padding(false);
+            let mut result = vec![];
+            ctx.cipher_update_vec(input, &mut result).unwrap();
+            ctx.cipher_final_vec(&mut result).unwrap();
+            result
+        }
+
+        let mut result = Self::default();
+
+        result
+            .uds
+            .as_bytes_mut()
+            .copy_from_slice(&aes256_decrypt_blocks(
+                swap_word_bytes(&input.doe_obf_key).as_bytes(),
+                &input.doe_iv,
+                swap_word_bytes(&input.uds_seed).as_bytes(),
+            ));
+        swap_word_bytes_inplace(&mut result.uds);
+
+        result.expected_test_results.hmac_uds_as_key = transmute!(hmac384(
+            swap_word_bytes(&result.uds).as_bytes(),
+            "Hello world!".as_bytes()
+        ));
+        swap_word_bytes_inplace(&mut result.expected_test_results.hmac_uds_as_key);
+
+        result.expected_test_results.hmac_uds_as_data = transmute!(hmac384(
+            swap_word_bytes(&caliptra_drivers_test_bin::DOE_TEST_HMAC_KEY).as_bytes(),
+            swap_word_bytes(&result.uds).as_bytes()
+        ));
+        swap_word_bytes_inplace(&mut result.expected_test_results.hmac_uds_as_data);
+
+        // After reset, the key-vault registers are filled with a particular
+        // word, depending on the debug-locked mode.  The field entropy only
+        // takes up 8 of the 12 words, so the 4 remaining words keep their
+        // original value (their contents are used when the key-vault entry is
+        // used as a HMAC key, but not when used as HMAC
+        // data).
+        result.field_entropy = [input.keyvault_initial_word_value; 12];
+        result.field_entropy[0..8]
+            .as_bytes_mut()
+            .copy_from_slice(&aes256_decrypt_blocks(
+                swap_word_bytes(&input.doe_obf_key).as_bytes(),
+                &input.doe_iv,
+                swap_word_bytes(&input.field_entropy_seed).as_bytes(),
+            ));
+        swap_word_bytes_inplace(&mut result.field_entropy);
+
+        result.expected_test_results.hmac_field_entropy_as_key = transmute!(hmac384(
+            swap_word_bytes(&result.field_entropy).as_bytes(),
+            "Hello world!".as_bytes()
+        ));
+        swap_word_bytes_inplace(&mut result.expected_test_results.hmac_field_entropy_as_key);
+
+        result.expected_test_results.hmac_field_entropy_as_data = transmute!(hmac384(
+            swap_word_bytes(&caliptra_drivers_test_bin::DOE_TEST_HMAC_KEY).as_bytes(),
+            swap_word_bytes(&result.field_entropy[0..8]).as_bytes()
+        ));
+        swap_word_bytes_inplace(&mut result.expected_test_results.hmac_field_entropy_as_data);
+        result
+    }
+}
+
+const DOE_TEST_VECTORS_DEBUG_MODE: DoeTestVectors = DoeTestVectors {
+    // The decrypted UDS as stored in the key vault
+    uds: [
+        0x34aa667c, 0x0a52c71f, 0x977a1de2, 0x701ef611, 0x0de19e21, 0x24b49b9d, 0xdf205ff6,
+        0xa9c04303, 0x0de19e21, 0x24b49b9d, 0xdf205ff6, 0xa9c04303,
+    ],
+
+    // The decrypted field entropy as stored in the key vault (with padding)
+    field_entropy: [
+        0x34aa667c,
+        0x0a52c71f,
+        0x977a1de2,
+        0x701ef611,
+        0x0de19e21,
+        0x24b49b9d,
+        0xdf205ff6,
+        0xa9c04303,
+        0xaaaa_aaaa,
+        0xaaaa_aaaa,
+        0xaaaa_aaaa,
+        0xaaaa_aaaa,
+    ],
+
+    // The expected results of the HMAC operations performed by the test.
+    expected_test_results: DoeTestResults {
+        hmac_uds_as_key: [
+            0x4446d380, 0xd2cb5d96, 0xcf745d40, 0xbfe7dcdb, 0x58a8befe, 0x2ddc1eac, 0xbc93b36c,
+            0xccc277ab, 0xedc67ae7, 0x7e4e12a4, 0x106e0e34, 0xb065b021,
+        ],
+        hmac_uds_as_data: [
+            0xe507101b, 0xb5fc57e0, 0xa02d2cdf, 0xb5b4d5ba, 0x69535616, 0xcb9d3ab8, 0x5a571a66,
+            0xb5e76d47, 0x802e86ba, 0x2969e838, 0x36869873, 0xb6847c27,
+        ],
+        hmac_field_entropy_as_key: [
+            0x683285f1, 0x27d26fc8, 0xe9e716c2, 0x0dc9c7fb, 0x9cad8b4c, 0xaeb167c5, 0xb402cf3b,
+            0x2e2c1745, 0x560bb884, 0xf592628f, 0x66db5c8f, 0x883086eb,
+        ],
+        hmac_field_entropy_as_data: [
+            0x4d2aec76, 0x7c73efbe, 0xb50aa67c, 0x89a684e3, 0x823834c4, 0x3429dea2, 0xf35cfdb0,
+            0xbfef4e6a, 0xa40dc572, 0xea82be07, 0xc93ef76a, 0xf955f845,
+        ],
+    },
+};
+
 #[test]
-fn test_doe() {
-    run_driver_test("doe");
+fn test_generate_doe_vectors_when_debug_not_locked() {
+    // When microcontroller debugging is possible, all the secrets are set by the hardware to
+    // 0xffff_ffff words.
+    let vectors = DoeTestVectors::generate(&DoeInput {
+        doe_obf_key: [0xffff_ffff_u32; 8],
+        doe_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
+
+        uds_seed: [0xffff_ffff_u32; 12],
+        field_entropy_seed: [0xffff_ffff_u32; 8],
+
+        // In debug mode, this defaults to 0xaaaa_aaaa
+        keyvault_initial_word_value: 0xaaaa_aaaa,
+    });
+    assert_eq!(vectors, DOE_TEST_VECTORS_DEBUG_MODE);
+}
+
+#[test]
+fn test_doe_when_debug_not_locked() {
+    let rom = build_test_rom("doe");
+    let mut model = caliptra_hw_model::new(BootParams {
+        init_params: InitParams {
+            rom: &rom,
+            security_state: *SecurityState::from(0)
+                .set_debug_locked(false)
+                .set_device_lifecycle(DeviceLifecycle::Unprovisioned),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .unwrap();
+
+    let txn = model.wait_for_mailbox_receive().unwrap();
+    let test_results = DoeTestResults::read_from(txn.req.data.as_slice()).unwrap();
+    assert_eq!(
+        test_results,
+        DOE_TEST_VECTORS_DEBUG_MODE.expected_test_results
+    )
+}
+
+const DOE_TEST_VECTORS: DoeTestVectors = DoeTestVectors {
+    uds: [
+        0x0b21f10f, 0x6963005e, 0x4884d93f, 0x1f91037a, 0x2d37ffe0, 0x3727b5e8, 0xb78b9608,
+        0x7e0e58d2, 0x420ce5ae, 0x4b1f04f8, 0x33b7af81, 0x72156bd8,
+    ],
+    field_entropy: [
+        0x3d75d35e, 0xbc44a31e, 0xad27aee5, 0x75cdd170, 0xe51dcaf4, 0x09c096ae, 0xa70ff448,
+        0x64834722, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+    ],
+    expected_test_results: DoeTestResults {
+        hmac_uds_as_key: [
+            0xf1e6eebe, 0x17718892, 0x6b3482a4, 0x6ebdd31a, 0x1a64b1df, 0xf832d618, 0x5d209aeb,
+            0x3e22c6a5, 0xaf18b9da, 0x78767e58, 0x143b5932, 0xb94caa30,
+        ],
+        hmac_uds_as_data: [
+            0x255b90d3, 0xce58a455, 0x72ca9fbb, 0xb6f963b8, 0x8a9e809c, 0x101dadf8, 0x1e35d99c,
+            0x459e5648, 0x44ad895a, 0x6342b793, 0x73b5d82a, 0xa65a9e8a,
+        ],
+        hmac_field_entropy_as_key: [
+            0x4c904ff4, 0xe1b642b7, 0xdaf61d5c, 0x0ae649ad, 0x22411ddd, 0x288e0902, 0x2911effc,
+            0xd76b38f1, 0x0c6ea42e, 0xd1b53612, 0xf77d2515, 0x954d9088,
+        ],
+        hmac_field_entropy_as_data: [
+            0x9f6024ff, 0x68fd825a, 0xbad1ce52, 0x18ed486d, 0x4dd1edc2, 0xeacfeb0b, 0x8d5d8873,
+            0x896be4f5, 0x8f30e6fa, 0xcc1b11c3, 0x0df0bc6e, 0x8fa6b5ba,
+        ],
+    },
+};
+
+#[test]
+fn test_generate_doe_vectors_when_debug_locked() {
+    let vectors = DoeTestVectors::generate(&DoeInput {
+        doe_obf_key: caliptra_hw_model_types::DEFAULT_CPTRA_OBF_KEY,
+        doe_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
+
+        uds_seed: caliptra_hw_model_types::DEFAULT_UDS_SEED,
+        field_entropy_seed: caliptra_hw_model_types::DEFAULT_FIELD_ENTROPY,
+
+        // in debug-locked mode, this defaults to 0
+        keyvault_initial_word_value: 0x0000_0000,
+    });
+    assert_eq!(DOE_TEST_VECTORS, vectors);
+}
+
+#[test]
+fn test_doe_when_debug_locked() {
+    let rom = build_test_rom("doe");
+    let mut model = caliptra_hw_model::new(BootParams {
+        init_params: InitParams {
+            rom: &rom,
+            security_state: *SecurityState::from(0)
+                .set_debug_locked(true)
+                .set_device_lifecycle(DeviceLifecycle::Unprovisioned),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .unwrap();
+
+    let txn = model.wait_for_mailbox_receive().unwrap();
+    let test_results = DoeTestResults::read_from(txn.req.data.as_slice()).unwrap();
+    assert_eq!(test_results, DOE_TEST_VECTORS.expected_test_results)
 }
 
 #[test]
