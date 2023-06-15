e6227d57591a01a268628330ad71d074cb6c1eb7
Kor Nielsen <kor@google.com>
2023-06-15 13:07:34 -0700

    DOEOutput (for testing): Allow separate UDS/FE IVs.
    This is important for calculating DOE output of the ROM, which uses
    separate IVs.

--- drivers/tests/integration_tests.rs
+++ drivers/tests/integration_tests.rs
@@ -156,7 +156,9 @@ fn test_generate_doe_vectors_when_debug_not_locked() {
     // 0xffff_ffff words.
     let vectors = DoeTestVectors::generate(&DoeInput {
         doe_obf_key: [0xffff_ffff_u32; 8],
-        doe_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
+
+        doe_uds_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
+        doe_fe_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
 
         uds_seed: [0xffff_ffff_u32; 12],
         field_entropy_seed: [0xffff_ffff_u32; 8],
@@ -225,7 +227,9 @@ const DOE_TEST_VECTORS: DoeTestVectors = DoeTestVectors {
 fn test_generate_doe_vectors_when_debug_locked() {
     let vectors = DoeTestVectors::generate(&DoeInput {
         doe_obf_key: caliptra_hw_model_types::DEFAULT_CPTRA_OBF_KEY,
-        doe_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
+
+        doe_uds_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
+        doe_fe_iv: caliptra_drivers_test_bin::DOE_TEST_IV,
 
         uds_seed: caliptra_hw_model_types::DEFAULT_UDS_SEED,
         field_entropy_seed: caliptra_hw_model_types::DEFAULT_FIELD_ENTROPY,
