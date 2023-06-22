b51a6f4d07b1b66911174eb32214e62865132e5a
Kor Nielsen <kor@google.com>
2023-06-22 13:36:26 -0700

    caliptra-test: Derive IDevID/LDevId/Pcr0/FmcAlias
    This is an independent implementation of the caliptra key derivation
    logic, and will be used by the smoke_test to ensure that all the correct
    measurements are being mixed into the private keys.

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -2,3 +2,12 @@
 
 mod crypto;
 pub mod derive;
+
+pub fn swap_word_bytes(words: &[u32]) -> Vec<u32> {
+    words.iter().map(|word| word.swap_bytes()).collect()
+}
+pub fn swap_word_bytes_inplace(words: &mut [u32]) {
+    for word in words.iter_mut() {
+        *word = word.swap_bytes()
+    }
+}
