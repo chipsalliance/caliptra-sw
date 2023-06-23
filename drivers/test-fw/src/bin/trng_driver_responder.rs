2e0f058e4c2978a6458d992186920ce485c79a31
Kor Nielsen <kor@google.com>
2023-06-22 23:58:32 -0700

    Generic TRNG driver: Add test cases.
    These test cases test the driver in both external and internal TRNG
    modes, and can pass with the emulated and verilated hw-models.
    
    To run the itrng test in verilator, use --features=verilator,itrng,
    which forces the RTL to be compiled with the ITRNG enabled.

---
+++ drivers/test-fw/src/bin/trng_driver_responder.rs
@@ -0,0 +1,61 @@
+// Licensed under the Apache-2.0 license
+
+//! A very simple program that responds with TRNG data through the mailbox
+
+#![no_main]
+#![no_std]
+
+// Needed to bring in startup code
+#[allow(unused)]
+use caliptra_test_harness::{self, println};
+
+use caliptra_drivers::{self, Trng};
+use caliptra_registers::{
+    csrng::CsrngReg, entropy_src::EntropySrcReg, mbox::MboxCsr, soc_ifc::SocIfcReg,
+    soc_ifc_trng::SocIfcTrngReg,
+};
+use zerocopy::AsBytes;
+
+#[panic_handler]
+pub fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+extern "C" fn main() {
+    let csrng_reg = unsafe { CsrngReg::new() };
+    let entropy_src_reg = unsafe { EntropySrcReg::new() };
+    let soc_ifc_trng = unsafe { SocIfcTrngReg::new() };
+    let mut soc_ifc = unsafe { SocIfcReg::new() };
+
+    let mut mbox = unsafe { MboxCsr::new() };
+    let mut trng = Trng::new(csrng_reg, entropy_src_reg, soc_ifc_trng, &soc_ifc).unwrap();
+    loop {
+        if !mbox.regs().status().read().mbox_fsm_ps().mbox_execute_uc() {
+            continue;
+        }
+
+        match trng.generate() {
+            Ok(data) => {
+                mbox.regs_mut()
+                    .dlen()
+                    .write(|_| data.0.as_bytes().len() as u32);
+                for word in data.0 {
+                    mbox.regs_mut().datain().write(|_| word);
+                }
+                mbox.regs_mut()
+                    .status()
+                    .write(|w| w.status(|w| w.data_ready()));
+            }
+            Err(e) => {
+                soc_ifc
+                    .regs_mut()
+                    .cptra_fw_error_non_fatal()
+                    .write(|_| e.into());
+                mbox.regs_mut()
+                    .status()
+                    .write(|w| w.status(|w| w.cmd_failure()));
+            }
+        }
+    }
+}
