2ea6a1bcab745c6d24eda3130ca13c6a715e7efa
Isaac Asay <Isaac.Asay@amd.com>
2023-06-26 10:46:25 -0700

    Test

--- release/build_release.sh
+++ release/build_release.sh
@@ -1,10 +1,9 @@
 # Licensed under the Apache-2.0 license
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
-mkdir release_${{ steps.date.outputs.date }}
-cargo run --manifest-path=../builder/Cargo.toml --bin image -- --rom caliptra_rom.bin --fw image_bundle.bin
-objcopy -I binary -O verilog caliptra_rom.bin caliptra_rom.hex
-cp -a ../target/riscv32imc-unknown-none-elf/firmware/* release_${{ steps.date.outputs.date }}/
-cp caliptra_rom.hex release_${{ steps.date.outputs.date }}/
-zip -r release_${{ steps.date.outputs.date }}.zip release_${{ steps.date.outputs.date }}
+mkdir release/workspace
+cargo run --manifest-path=builder/Cargo.toml --bin image -- --rom caliptra_rom.bin --fw image_bundle.bin
+objcopy -I binary -O verilog caliptra_rom.bin release/workspace/caliptra_rom.hex
+cp -a target/riscv32imc-unknown-none-elf/firmware/* release/workspace/
+zip -r release/release.zip release/workspace
