34d23ad847d9dbe88b0e91e224a16c03076c627d
Luke Mahowald <Luke.Mahowald@amd.com>
2023-07-06 09:50:47 -0700

    Revert "Updated hw-latest/caliptra-rtl to 67563371b8208a8e6720fa6c33456c37914d858d"
    This reverts commit f077568c2def8bd8d25ef7d11221ed6fcad92849.

--- drivers/src/error_reporter.rs
+++ drivers/src/error_reporter.rs
@@ -40,10 +40,7 @@ pub fn report_fw_error_fatal(val: u32) {
 /// * `val` - H/W error code.
 pub fn report_hw_error_non_fatal(val: u32) {
     let mut soc_ifc = unsafe { SocIfcReg::new() };
-    soc_ifc
-        .regs_mut()
-        .cptra_hw_error_non_fatal()
-        .write(|_| val.into());
+    soc_ifc.regs_mut().cptra_hw_error_non_fatal().write(|_| val);
 }
 
 /// Report fatal H/W error
@@ -53,8 +50,5 @@ pub fn report_hw_error_non_fatal(val: u32) {
 /// * `val` - H/W error code.
 pub fn report_hw_error_fatal(val: u32) {
     let mut soc_ifc = unsafe { SocIfcReg::new() };
-    soc_ifc
-        .regs_mut()
-        .cptra_hw_error_fatal()
-        .write(|_| val.into());
+    soc_ifc.regs_mut().cptra_hw_error_fatal().write(|_| val);
 }
