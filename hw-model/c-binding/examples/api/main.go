7e57314d68c30f315577265680adcfa4b02a3c98
prems95 <kpremalatha95@yahoo.com>
2023-08-07 05:48:21 -0700

    test

--- hw-model/c-binding/examples/api/main.go
+++ hw-model/c-binding/examples/api/main.go
@@ -58,11 +58,11 @@ func main() {
 }
 
 // Helper function to copy Go []byte to C array
-func copySliceToCArray(dest *[12]C.uint32_t, src []byte) {
-	if len(src) != 12*4 {
+func copySliceToCArray(dest *[12]C.uint32_t, src []byte, size int) {
+	if len(src) != size*4 {
 		panic("Invalid source slice length")
 	}
-	for i := 0; i < 12; i++ {
+	for i := 0; i < size; i++ {
 		dest[i] = C.uint32_t(binary.LittleEndian.Uint32(src[i*4:]))
 	}
 }
@@ -74,21 +74,22 @@ func caliptraInitFuses(model *caliptraModel, fuses *caliptraBuffer) int {
 
 	// Call the C function
 	cFuses := C.struct_caliptra_fuses{}
-	copySliceToCArray(&cFuses.uds_seed, fuses.data)
-	copySliceToCArray(&cFuses.field_entropy, fuses.data[48:][:8]) // Adjust array size to 8
-	copySliceToCArray(&cFuses.key_manifest_pk_hash, fuses.data[80:][:12]) // Adjust array size to 12
-	copySliceToCArray(&cFuses.owner_pk_hash, fuses.data[112:][:12]) // Adjust array size to 12
+	copySliceToCArray(&cFuses.uds_seed, fuses.data, 12)
+	copySliceToCArray(&cFuses.field_entropy, fuses.data[48:], 8)
+	copySliceToCArray(&cFuses.key_manifest_pk_hash, fuses.data[80:], 12)
+	copySliceToCArray(&cFuses.owner_pk_hash, fuses.data[112:], 12)
 	cFuses.fmc_key_manifest_svn = C.uint32_t(binary.LittleEndian.Uint32(fuses.data[148:]))
-	copySliceToCArray(&cFuses.runtime_svn, fuses.data[152:][:4]) // Adjust array size to 4
+	copySliceToCArray(&cFuses.runtime_svn, fuses.data[152:], 4)
 	cFuses.anti_rollback_disable = C._Bool(binary.LittleEndian.Uint32(fuses.data[184:]) != 0)
-	copySliceToCArray(&cFuses.idevid_cert_attr, fuses.data[188:][:24]) // Adjust array size to 24
-	copySliceToCArray(&cFuses.idevid_manuf_hsm_id, fuses.data[376:][:4]) // Adjust array size to 4
+	copySliceToCArray(&cFuses.idevid_cert_attr, fuses.data[188:], 24)
+	copySliceToCArray(&cFuses.idevid_manuf_hsm_id, fuses.data[376:], 4)
 	cFuses.life_cycle = C.enum_DeviceLifecycle(binary.LittleEndian.Uint32(fuses.data[392:]))
 
 	ret := C.caliptra_init_fuses(cModel, &cFuses)
 
 	return int(ret)
 }
+
 
 func caliptraBootFsmGo(model *caliptraModel) int {
 	// Convert Go structs to C structs
