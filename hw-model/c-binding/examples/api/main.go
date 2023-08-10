05724189ab681fe9a350b0b33e93e4cb1691badb
prems95 <kpremalatha95@yahoo.com>
2023-08-09 22:51:32 -0700

    test

--- hw-model/c-binding/examples/api/main.go
+++ hw-model/c-binding/examples/api/main.go
@@ -6,7 +6,6 @@ package main
 // #include "caliptra_api.h"
 // #include "caliptra_fuses.h"
 // #include "caliptra_mbox.h"
-// extern int caliptra_mailbox_write_fifo(struct caliptra_model *model, struct caliptra_buffer *buffer);
 import "C"
 
 import (
@@ -24,15 +23,18 @@ func main() {
 	a := C.greet(&me)
 	fmt.Println(a)
 
-	// Create a caliptra_buffer struct
-	romData := "This is the ROM data."
+	// Create a caliptra_buffer struct from a []byte
+	romData := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
 	rom := C.caliptra_buffer{
-		data: (*C.uint8_t)(unsafe.Pointer(C.CString(romData))),
+		data: (*C.uint8_t)(unsafe.Pointer(&romData[0])),
 		len:  C.uintptr_t(len(romData)),
 	}
 
-	// Call the caliptra_mailbox_write_fifo function
+	// Create a caliptra_model struct
 	var model C.caliptra_model
+	model._unused = [1]C.uint8_t{} // Initialize as needed
+
+	// Call the caliptra_mailbox_write_fifo function
 	ret := C.caliptra_mailbox_write_fifo(&model, &rom)
 
 	fmt.Println(ret)
