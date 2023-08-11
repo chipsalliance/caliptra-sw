ebd4389e70e4f5b75a7ff608ce5b7f1080b90479
prems95 <kpremalatha95@yahoo.com>
2023-08-11 00:04:52 -0700

    test

--- hw-model/c-binding/examples/api/main.go
+++ hw-model/c-binding/examples/api/main.go
@@ -10,38 +10,85 @@ package main
 import "C"
 
 import (
-	"fmt"
+    "flag"
+    "os"
+    "unsafe"
 )
 
+func read_file_or_die(path *C.char) C.struct_caliptra_buffer {
+    // Open File in Read Only Mode
+    fp := C.fopen(path, C.CString("r"))
+    if fp == nil {
+        C.printf(C.CString("Cannot find file %s \n"), path)
+        os.Exit(int(C.ENOENT))
+    }
+
+    var buffer C.struct_caliptra_buffer
+
+    // Get File Size
+    C.fseek(fp, 0, C.SEEK_END)
+    buffer.len = C.ftell(fp)
+    C.fseek(fp, 0, C.SEEK_SET)
+
+    // Allocate Buffer Memory
+    buffer.data = C.malloc(C.size_t(buffer.len))
+    if buffer.data == nil {
+        C.printf(C.CString("Cannot allocate memory for buffer->data \n"))
+        os.Exit(int(C.ENOMEM))
+    }
+
+    // Read Data in Buffer
+    C.fread(unsafe.Pointer(buffer.data), C.size_t(buffer.len), 1, fp)
+
+    return buffer
+}
+
 func main() {
-	// Create a C struct
-	var me C.struct_person
-	me.name = C.CString("Tony")
-	me.age = 23
+    // Process Input Arguments
+    romPath := flag.String("r", "", "rom file path")
+    fwPath := flag.String("f", "", "fw image file path")
+    flag.Parse()
 
-	// Call the C function
-	a := C.greet(&me)
-	fmt.Println(a)
+    if *romPath == "" || *fwPath == "" {
+        flag.Usage()
+        os.Exit(int(C.EINVAL))
+    }
 
-	// Create a caliptra_buffer struct from a []byte
-	romData := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
-	romDataPtr := C.CBytes(romData)
-	defer C.free(romDataPtr) // Release the allocated memory
+    // Initialize Params
+    initParams := C.struct_caliptra_model_init_params{
+        rom: read_file_or_die(C.CString(*romPath)),
+        dccm: C.struct_caliptra_buffer{data: nil, len: 0},
+        iccm: C.struct_caliptra_buffer{data: nil, len: 0},
+    }
 
-	rom := C.caliptra_buffer{
-		data: (*C.uint8_t)(romDataPtr),
-		len:  C.uintptr_t(len(romData)),
-	}
-	// Create a caliptra_model struct
-	var model C.caliptra_model
-	//model._unused = [1]C.uint8_t{} // Initialize as needed
+    // Initialize Model
+    var model *C.struct_caliptra_model
+    C.caliptra_model_init_default(initParams, &model)
 
-	// Call the caliptra_mailbox_write_fifo function
-	//ret := C.caliptra_mailbox_write_fifo(&model, &rom)
+    // Initialize Fuses (Todo: Set real fuse values)
+    var fuses C.struct_caliptra_fuses
+    C.caliptra_init_fuses(model, &fuses)
 
-	value := C.uint32_t(0xe0002000)
+    // Initialize FSM GO
+    C.caliptra_bootfsm_go(model)
+    C.caliptra_model_step(model)
 
-	ret := C.caliptra_mailbox_execute(&model,value,&rom,&rom);
+    // Step until ready for FW
+    for C.caliptra_model_ready_for_fw(model) == 0 {
+        C.caliptra_model_step(model)
+    }
 
-	fmt.Println(ret)
+    // Load Image Bundle
+    imageBundle := read_file_or_die(C.CString(*fwPath))
+    C.caliptra_upload_fw(model, &imageBundle)
+
+    // Run Until RT is ready to receive commands
+    for {
+        C.caliptra_model_step(model)
+        buffer := C.caliptra_model_output_peek(model)
+        if C.strstr(buffer.data, C.CString("Caliptra RT listening for mailbox commands...")) != nil {
+            break
+        }
+    }
+    C.printf(C.CString("Caliptra C Smoke Test Passed \n"))
 }
