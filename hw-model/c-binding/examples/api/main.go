4aa57988871e402b550af4a5feed790fab15e8f1
prems95 <kpremalatha95@yahoo.com>
2023-08-14 00:15:36 -0700

    test

--- hw-model/c-binding/examples/api/main.go
+++ hw-model/c-binding/examples/api/main.go
@@ -94,7 +94,8 @@ func main() {
     C.caliptra_model_step(model)
         buffer := C.caliptra_model_output_peek(model)
         if C.strstr((*C.char)(unsafe.Pointer(buffer.data)), C.CString("Caliptra RT listening for mailbox commands...")) != nil {
-            C.caliptra_mailbox_execute(model,0x020446574C4404, &imageBundle, NULL)
+            const C.uint32_t Get_PROFILE_OPCODE = 0x020446574C4404
+            C.caliptra_mailbox_execute(model,Get_PROFILE_OPCODE, &imageBundle, nil)
             break
         }
     }
