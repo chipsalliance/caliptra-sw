7f2aebe5bfd8a92fad851c5927d8bda98275c48b
Nick Quarton <139178705+nquarton@users.noreply.github.com>
2023-08-18 14:10:03 -0400

    Reset flows for val-rom (#644)
    - Warm and update reset flows for val-rom
    - Introduces val-fmc to test FMC for ROMx

--- rom/dev/tests/test_val_rom.rs
+++ rom/dev/tests/test_val_rom.rs
@@ -2,10 +2,20 @@
 
 use caliptra_builder::ROM_VAL_WITH_UART;
 use caliptra_builder::{FwId, ImageOptions, APP_WITH_UART};
+use caliptra_common::RomBootStatus::*;
 use caliptra_drivers::CaliptraError;
 use caliptra_hw_model::{BootParams, DeviceLifecycle, Fuses, HwModel, InitParams, SecurityState};
 
 pub mod helpers;
+
+const TEST_FMC_CMD_RESET_FOR_UPDATE: u32 = 0x1000_0004;
+
+const VAL_TEST_FMC_WITH_UART: FwId = FwId {
+    crate_name: "caliptra-rom-test-fmc",
+    bin_name: "caliptra-rom-test-fmc",
+    features: &["emu", "val-fmc"],
+    workspace_dir: None,
+};
 
 #[test]
 fn test_skip_kats() {
@@ -54,13 +64,6 @@ fn test_val_rom_production_error() {
 
 #[test]
 fn test_val_rom_fw_load() {
-    pub const TEST_FMC_WITH_UART: FwId = FwId {
-        crate_name: "caliptra-rom-test-fmc",
-        bin_name: "caliptra-rom-test-fmc",
-        features: &["emu"],
-        workspace_dir: None,
-    };
-
     let fuses = Fuses::default();
     let rom = caliptra_builder::build_firmware_rom(&ROM_VAL_WITH_UART).unwrap();
     let mut hw = caliptra_hw_model::new(BootParams {
@@ -76,7 +79,7 @@ fn test_val_rom_fw_load() {
 
     // Build the image we are going to send to ROM to load
     let image_bundle = caliptra_builder::build_and_sign_image(
-        &TEST_FMC_WITH_UART,
+        &VAL_TEST_FMC_WITH_UART,
         &APP_WITH_UART,
         ImageOptions::default(),
     )
@@ -94,4 +97,48 @@ fn test_val_rom_fw_load() {
     // Make sure we actually get into FMC
     hw.step_until_output_contains("Running Caliptra FMC")
         .unwrap();
+}
+
+#[test]
+fn test_val_rom_update_reset() {
+    let fuses = Fuses::default();
+    let rom = caliptra_builder::build_firmware_rom(&ROM_VAL_WITH_UART).unwrap();
+    let mut hw = caliptra_hw_model::new(BootParams {
+        init_params: InitParams {
+            rom: &rom,
+            security_state: SecurityState::from(fuses.life_cycle as u32),
+            ..Default::default()
+        },
+        fuses,
+        ..Default::default()
+    })
+    .unwrap();
+
+    let image_bundle = caliptra_builder::build_and_sign_image(
+        &VAL_TEST_FMC_WITH_UART,
+        &APP_WITH_UART,
+        ImageOptions::default(),
+    )
+    .unwrap();
+
+    // Upload FW
+    hw.step_until(|m| m.soc_ifc().cptra_flow_status().read().ready_for_fw());
+    hw.upload_firmware(&image_bundle.to_bytes().unwrap())
+        .unwrap();
+
+    hw.step_until_boot_status(ColdResetComplete.into(), true);
+
+    // Send command to test FMC to initiate a reset for update
+    hw.mailbox_execute(TEST_FMC_CMD_RESET_FOR_UPDATE, &[])
+        .unwrap();
+
+    hw.step_until_boot_status(UpdateResetStarted.into(), true);
+
+    // Upload FW again
+    hw.step_until(|m| m.soc_ifc().cptra_flow_status().read().ready_for_fw());
+    hw.upload_firmware(&image_bundle.to_bytes().unwrap())
+        .unwrap();
+
+    hw.step_until_boot_status(UpdateResetComplete.into(), true);
+    hw.step_until_exit_success().unwrap();
 }
