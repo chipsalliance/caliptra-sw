d4f732404cc4561ba50359a5e082d15ec34616cb
Will Marone <Will.Marone@amd.com>
2023-08-18 10:59:21 -0700

    DPE Command Support
    This adds support for DPE commands as described in the runtime
    documentation. The values and fields contained in the headers are
    placeholders pending final documentation from TCG.

--- libcaliptra/inc/caliptra_enums.h
+++ libcaliptra/inc/caliptra_enums.h
@@ -31,3 +31,44 @@ enum toc_entry_id {
     MAX     = 0xFFFFFFFF,
 };
 
+// The below enums are placeholders to set up the baseline
+// required for communication of DPE commands to Caliptra
+// firmware.
+
+enum dpe_commands {
+    DPE_GET_PROFILE        = 0x1,
+    DPE_INITIALIZE_CONTEXT = 0x7,
+    DPE_DERIVE_CHILD       = 0x8,
+    DPE_CERTIFY_KEY        = 0x9,
+    DPE_SIGN               = 0xA,
+    DPE_ROTATE_CTX_HANDLE  = 0xE,
+    DPE_DESTROY_CTX        = 0xF,
+    DPE_GET_CERT_CHAIN     = 0x80,
+    DPE_EXTEND_TCI         = 0x81,
+    DPE_TAG_TCI            = 0x82,
+    DPE_GET_TAGGED_TCI     = 0x83,
+};
+
+enum dpe_error_codes {
+    DPE_NO_ERROR               = 0,
+    DPE_INTERNAL_ERROR         = 1,
+    DPE_INVALID_COMMAND        = 2,
+    DPE_INVALID_ARGUMENT       = 3,
+    DPE_ARGUMENT_NOT_SUPPORTED = 4,
+    DPE_INVALID_HANDLE         = 0x1000,
+    DPE_INVALID_LOCALITY       = 0x1001,
+    DPE_BADTAG                 = 0x1002,
+    DPE_MAXTCIS                = 0x1003,
+    DPE_PLATFORM_ERROR         = 0x1004,
+    DPE_CRYPTO_ERROR           = 0x1005,
+    DPE_HASH_ERROR             = 0x1006,
+    DPE_RAND_ERROR             = 0x1007,
+};
+
+#define DPE_PROFILE_256 1
+#define DPE_PROFILE_384 2
+
+enum dpe_profile {
+    P256Sha256 = DPE_PROFILE_256,
+    P384Sha384 = DPE_PROFILE_384,
+};
