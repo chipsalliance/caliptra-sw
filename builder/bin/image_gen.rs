eda87f921ad8dcffe5c4accb7283bdce54b87df0
Kor Nielsen <kor@google.com>
2023-09-09 11:24:51 -0700

    caliptra-builder: Make image_gen args optional.
    With this change, the user can choose to build only the ROM or only the
    mutable firmware bundle. And it makes room for new features.

--- builder/bin/image_gen.rs
+++ builder/bin/image_gen.rs
@@ -3,34 +3,29 @@
 use caliptra_builder::firmware;
 use caliptra_builder::ImageOptions;
 use clap::{arg, value_parser, Command};
-use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    let args = Command::new("caliptra-emu")
-        .about("Caliptra emulator")
-        .arg(arg!(--"rom" <FILE> "ROM binary image").value_parser(value_parser!(PathBuf)))
-        .arg(arg!(--"fw" <FILE> "FW bundle image").value_parser(value_parser!(PathBuf)))
+    let args = Command::new("image-gen")
+        .about("Caliptra firmware image builder")
+        .arg(arg!(--"rom" [FILE] "ROM binary image").value_parser(value_parser!(PathBuf)))
+        .arg(arg!(--"fw" [FILE] "FW bundle image").value_parser(value_parser!(PathBuf)))
         .get_matches();
-    let rom_path = args.get_one::<PathBuf>("rom").unwrap();
-    let fw_path = args.get_one::<PathBuf>("fw").unwrap();
 
-    // Generate ROM Image
-    let rom = caliptra_builder::build_firmware_rom(&firmware::ROM_WITH_UART).unwrap();
+    if let Some(rom_path) = args.get_one::<PathBuf>("rom") {
+        // Generate ROM Image
+        let rom = caliptra_builder::build_firmware_rom(&firmware::ROM_WITH_UART).unwrap();
+        std::fs::write(rom_path, rom).unwrap();
+    };
 
-    // Generate Image Bundle
-    let image = caliptra_builder::build_and_sign_image(
-        &firmware::FMC_WITH_UART,
-        &firmware::APP_WITH_UART,
-        ImageOptions::default(),
-    )
-    .unwrap();
-
-    // Generate ROM Binary
-    let _ = std::fs::File::create(rom_path).unwrap().write_all(&rom);
-
-    // Generate Image Bundle Binary
-    let _ = std::fs::File::create(fw_path)
-        .unwrap()
-        .write_all(&image.to_bytes().unwrap());
+    if let Some(fw_path) = args.get_one::<PathBuf>("fw") {
+        // Generate Image Bundle
+        let image = caliptra_builder::build_and_sign_image(
+            &firmware::FMC_WITH_UART,
+            &firmware::APP_WITH_UART,
+            ImageOptions::default(),
+        )
+        .unwrap();
+        std::fs::write(fw_path, image.to_bytes().unwrap()).unwrap();
+    }
 }
