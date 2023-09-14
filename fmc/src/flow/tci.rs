bf2d924bcca620843a5dc532168e7c5945bef059
Jeff Andersen <jeffandersen@google.com>
2023-09-14 15:43:43 -0700

    Use persistent FHT throughout FMC.
    Rather than maintain a copy.

--- fmc/src/flow/tci.rs
+++ fmc/src/flow/tci.rs
@@ -18,10 +18,8 @@ Environment:
 --*/
 use crate::flow::crypto::Crypto;
 use crate::fmc_env::FmcEnv;
-use crate::HandOff;
 use caliptra_drivers::{Array4x12, CaliptraResult};
-use caliptra_image_types::ImageManifest;
-use core::mem::size_of;
+use zerocopy::AsBytes;
 
 pub struct Tci {}
 
@@ -31,29 +29,8 @@ impl Tci {
     /// # Arguments
     ///
     /// * `env` - ROM Environment
-    /// * `hand_off` - HandOff
-    pub fn image_manifest_digest(
-        env: &mut FmcEnv,
-        hand_off: &HandOff,
-    ) -> CaliptraResult<Array4x12> {
-        // TODO: Should compare this address to env.
-        let image_manifest_bytes = unsafe {
-            core::slice::from_raw_parts_mut(
-                hand_off.image_manifest_address(env) as *mut u8,
-                size_of::<ImageManifest>(),
-            )
-        };
-
-        Crypto::sha384_digest(env, image_manifest_bytes)
-    }
-
-    ///  RtFw Digest
-    ///
-    /// # Arguments
-    ///
-    /// * `env` - ROM Environment
-    /// * `hand_off` - HandOff
-    pub fn rt_tci(env: &mut FmcEnv, hand_off: &HandOff) -> CaliptraResult<Array4x12> {
-        Ok(hand_off.rt_tci(env))
+    pub fn image_manifest_digest(env: &mut FmcEnv) -> CaliptraResult<Array4x12> {
+        let manifest = env.persistent_data.get().manifest1;
+        Crypto::sha384_digest(env, manifest.as_bytes())
     }
 }
