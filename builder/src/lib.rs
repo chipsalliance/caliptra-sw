chipsalliance/caliptra-sw

commit c8d51abde0e236133181df539178859b25c0e1f5

Fix race when building same binary with different features. (#815)
To prevent a race condition with concurrent calls to caliptra-builder
from other threads or processes, we now hold a lock until we've read the
output binary from the filesystem (it's possible that another concurrent
process will build the same binary with different features before we get
a chance to read it).

I believe this race condition is responsible for intermittent test
failures of the FMC's test_fht_info() and test_pcr_log() tests in the
GHA environment.

builder/src/lib.rs
@@ -2,9 +2,10 @@
 
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
-use std::fs;
+use std::fs::{self, File};
 use std::io::{self, ErrorKind};
 use std::mem::size_of;
+use std::os::fd::AsRawFd;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use std::sync::{Arc, Mutex};
@@ -16,6 +17,7 @@ use caliptra_image_gen::{
 use caliptra_image_openssl::OsslCrypto;
 use caliptra_image_types::{ImageBundle, ImageRevision, RomInfo};
 use elf::endian::LittleEndian;
+use nix::fcntl::FlockArg;
 use zerocopy::AsBytes;
 
 mod elf_symbols;
@@ -143,6 +145,14 @@ pub fn build_firmware_elf_uncached(id: &FwId) -> io::Result<Vec<u8>> {
     let workspace_dir = id
         .workspace_dir
         .unwrap_or_else(|| Path::new(THIS_WORKSPACE_DIR));
+
+    // To prevent a race condition with concurrent calls to caliptra-builder
+    // from other threads or processes, hold a lock until we've read the output
+    // binary from the filesystem (it's possible that another thread will build
+    // the same binary with different features before we get a chance to read it).
+    let lock = File::create(workspace_dir.join("target/.caliptra-builder.lock"))?;
+    nix::fcntl::flock(lock.as_raw_fd(), FlockArg::LockExclusive)?;
+
     let mut cmd = Command::new(env!("CARGO"));
     cmd.current_dir(workspace_dir);
     if option_env!("GITHUB_ACTIONS").is_some() {

builder/Cargo.toml
@@ -17,6 +17,7 @@ caliptra-image-types.workspace = true
 clap.workspace = true
 elf.workspace = true
 hex.workspace = true
+nix.workspace = true
 once_cell.workspace = true
 openssl.workspace = true
 zerocopy.workspace = true

commit 70fdd2e58087c2928b60c119e69e92faeaac3b93

caliptra-builder: Create target dir if it doesn't exist.
This fixes the "No such file or directory" size-history workflow
regression caused by #815.

builder/src/lib.rs
@@ -150,6 +150,7 @@ pub fn build_firmware_elf_uncached(id: &FwId) -> io::Result<Vec<u8>> {
     // from other threads or processes, hold a lock until we've read the output
     // binary from the filesystem (it's possible that another thread will build
     // the same binary with different features before we get a chance to read it).
+    let _ = fs::create_dir(workspace_dir.join("target"));
     let lock = File::create(workspace_dir.join("target/.caliptra-builder.lock"))?;
     nix::fcntl::flock(lock.as_raw_fd(), FlockArg::LockExclusive)?;
 

ci-tools/size-history/src/main.rs
@@ -22,7 +22,7 @@ use crate::{cache_gha::GithubActionCache, util::other_err};
 
 // Increment with non-backwards-compatible changes are made to the cache record
 // format
-const CACHE_FORMAT_VERSION: &str = "v1";
+const CACHE_FORMAT_VERSION: &str = "v2";
 
 #[derive(Clone, Copy, Default, Eq, PartialEq, Serialize, Deserialize)]
 struct Sizes {