2acd23c0ea3ff56d035fda45a943111039dc0054
Julian Laubstein <contact@julianlaubstein.de>
2023-09-28 10:17:11 +0200

    Moved CLI into separate module

---
+++ sw-emulator/app/src/cli.rs
@@ -0,0 +1,78 @@
+use clap::{Parser, ValueEnum};
+use std::path::PathBuf;
+
+use crate::EXPECTED_CALIPTRA_BOOT_TIME_IN_CYCLES;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+pub struct Args {
+    /// ROM binary path
+    pub rom: PathBuf,
+
+    /// Gdb Debugger
+    #[arg(long)]
+    pub gdb_port: Option<u16>,
+
+    /// Current Firmware image file
+    #[arg(long)]
+    pub firmware: Option<PathBuf>,
+
+    /// Update Firmware image file
+    #[arg(long)]
+    pub update_firmware: Option<PathBuf>,
+
+    /// Trace instructions to a file in log-dir
+    #[arg(long)]
+    pub trace_instr: bool,
+
+    /// 64-bit Unique Endpoint Id
+    #[arg(long, default_value_t = u64::MAX)]
+    pub ueid: u64,
+
+    /// idevid certificate key id algorithm
+    #[arg(long, value_enum, default_value_t = ArgsIdevidAlgo::Sha1)]
+    pub idevid_key_id_algo: ArgsIdevidAlgo,
+
+    /// Request IDevID CSR. Downloaded CSR is stored in log-dir
+    #[arg(long)]
+    pub req_idevid_csr: bool,
+
+    /// Request LDevID Cert. Downloaded cert is stored in log-dir
+    #[arg(long)]
+    pub req_ldevid_csr: bool,
+
+    /// Directory to log execution artifacts
+    #[arg(long, default_value = "/tmp")]
+    pub log_dir: PathBuf,
+
+    /// Hash of the four Manufacturer Public Keys
+    #[arg(long, default_value = "")]
+    pub mfg_pk_hash: String,
+
+    /// Owner Public Key Hash
+    #[arg(long, default_value = "")]
+    pub owner_pk_hash: String,
+
+    /// Device Lifecycle State
+    #[arg(long, value_enum, default_value_t = ArgsDeviceLifecycle::Unprovisioned)]
+    pub device_lifecycle: ArgsDeviceLifecycle,
+
+    /// Watchdog Timer Timeout in CPU Clock Cycles
+    #[arg(long, default_value_t = EXPECTED_CALIPTRA_BOOT_TIME_IN_CYCLES)]
+    pub wdt_timeout: u64,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum ArgsIdevidAlgo {
+    Sha1,
+    Sha256,
+    Sha384,
+    Fuse,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum ArgsDeviceLifecycle {
+    Unprovisioned,
+    Manufacturing,
+    Production,
+}
