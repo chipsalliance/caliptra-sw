1f6fcb16163e11fc25b1a560ef43c4717d1554d5
Sree Revoori <srrevoori@google.com>
2023-11-07 03:51:06 +0000

    Add POPULATE_IDEV_CERT mailbox command to RT
    This command will allow the SoC to add the idev id cert
    to the start of the certificate chain.

--- runtime/src/info.rs
+++ runtime/src/info.rs
@@ -1,8 +1,10 @@
 // Licensed under the Apache-2.0 license
 
-use crate::{handoff::RtHandoff, Drivers};
+use crate::{handoff::RtHandoff, Drivers, MAX_CERT_CHAIN_SIZE, PL0_PAUSER_FLAG};
+use arrayvec::ArrayVec;
 use caliptra_common::mailbox_api::{
     FwInfoResp, GetIdevCertReq, GetIdevCertResp, GetIdevInfoResp, MailboxResp, MailboxRespHeader,
+    PopulateIdevCertReq,
 };
 use caliptra_drivers::{CaliptraError, CaliptraResult};
 use caliptra_x509::{Ecdsa384CertBuilder, Ecdsa384Signature};
@@ -75,6 +77,37 @@ impl IDevIdCertCmd {
                 cert_size: cert_size as u32,
                 cert,
             }))
+        } else {
+            Err(CaliptraError::RUNTIME_INSUFFICIENT_MEMORY)
+        }
+    }
+}
+
+pub struct PopulateIDevIdCertCmd;
+impl PopulateIDevIdCertCmd {
+    pub(crate) fn execute(drivers: &mut Drivers, cmd_args: &[u8]) -> CaliptraResult<MailboxResp> {
+        if let Some(cmd) = PopulateIdevCertReq::read_from(cmd_args) {
+            let cert_size = cmd.cert_size as usize;
+            if cert_size > cmd.cert.len() {
+                return Err(CaliptraError::RUNTIME_MAILBOX_INVALID_PARAMS);
+            }
+
+            let flags = drivers.persistent_data.get().manifest1.header.flags;
+            // PL1 cannot call this mailbox command
+            if flags & PL0_PAUSER_FLAG == 0 {
+                return Err(CaliptraError::RUNTIME_INCORRECT_PAUSER_PRIVILEGE_LEVEL);
+            }
+
+            let mut tmp_chain = ArrayVec::<u8, MAX_CERT_CHAIN_SIZE>::new();
+            tmp_chain
+                .try_extend_from_slice(&cmd.cert[..cert_size])
+                .map_err(|_| CaliptraError::RUNTIME_IDEV_CERT_POPULATION_FAILED)?;
+            tmp_chain
+                .try_extend_from_slice(drivers.cert_chain.as_slice())
+                .map_err(|_| CaliptraError::RUNTIME_IDEV_CERT_POPULATION_FAILED)?;
+            drivers.cert_chain = tmp_chain;
+
+            Ok(MailboxResp::default())
         } else {
             Err(CaliptraError::RUNTIME_INSUFFICIENT_MEMORY)
         }
