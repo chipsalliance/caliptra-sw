63a2cfa20196c58e37fa2c42d620f14c4479acae
Kor Nielsen <kor@google.com>
2023-11-10 00:21:45 -0800

    Use closures for cfi.

--- cfi/derive/src/lib.rs
+++ cfi/derive/src/lib.rs
@@ -17,11 +17,9 @@ References:
 --*/
 
 use proc_macro::TokenStream;
-use quote::{format_ident, quote, ToTokens};
-use syn::__private::TokenStream2;
+use quote::quote;
 use syn::parse_macro_input;
 use syn::parse_quote;
-use syn::FnArg;
 use syn::ItemFn;
 
 #[proc_macro_attribute]
@@ -34,63 +32,37 @@ pub fn cfi_impl_fn(_args: TokenStream, input: TokenStream) -> TokenStream {
     cfi_fn(false, input)
 }
 
-fn cfi_fn(mod_fn: bool, input: TokenStream) -> TokenStream {
-    let mut wrapper_fn: ItemFn = parse_macro_input!(input as ItemFn);
-    let mut orig_fn = wrapper_fn.clone();
-    orig_fn.sig.ident = format_ident!("__cfi_{}", wrapper_fn.sig.ident);
-    orig_fn.attrs.retain(|a| a.path.is_ident("inline"));
-    orig_fn.vis = syn::Visibility::Inherited;
+fn cfi_fn(_mod_fn: bool, input: TokenStream) -> TokenStream {
+    let mut f: ItemFn = parse_macro_input!(input as ItemFn);
 
-    wrapper_fn.attrs.retain(|a| !a.path.is_ident("inline"));
+    let inline_attr = f.attrs.iter().find(|a| a.path.is_ident("inline")).cloned();
 
-    let fn_name = format_ident!("{}", orig_fn.sig.ident);
+    // Remove the inline attribute from the wrapper function; we'll put this on
+    // the closure instead.
+    f.attrs.retain(|a| !a.path.is_ident("inline"));
 
-    let param_names: Vec<TokenStream2> = orig_fn
-        .sig
-        .inputs
-        .iter()
-        .map(|input| match input {
-            FnArg::Receiver(r) => r.self_token.to_token_stream(),
-            FnArg::Typed(p) => p.pat.to_token_stream(),
-        })
-        .collect();
-
-    let fn_call = if mod_fn {
-        quote!(#fn_name( #(#param_names,)* ))
-    } else {
-        quote!(Self::#fn_name( #(#param_names,)* ))
-    };
-
-    wrapper_fn.block.stmts.clear();
-    wrapper_fn.block.stmts = parse_quote!(
-        let saved_ctr = caliptra_cfi_lib::CfiCounter::read();
+    let orig_block = std::mem::replace(&mut f.block, parse_quote!({}));
+    f.block.stmts = parse_quote!(
+        // This is necessary to allow the inline attribute to be placed on the
+        // closure
+        fn __cfi_move<R>(r: R) -> R {
+            r
+        }
+        let __cfi_saved_ctr = caliptra_cfi_lib::CfiCounter::read();
         caliptra_cfi_lib::CfiCounter::delay();
-        let ret = #fn_call;
-        caliptra_cfi_lib::CfiCounter::delay();
-        let new_ctr = caliptra_cfi_lib::CfiCounter::decrement();
-        caliptra_cfi_lib::CfiCounter::assert_eq(saved_ctr, new_ctr);
-        ret
-    );
-
-    // Add inline attribute to the wrapper function.
-    let inline_attr = parse_quote! {
-    #[inline(always)]
-     };
-
-    wrapper_fn.attrs.insert(wrapper_fn.attrs.len(), inline_attr);
-
-    // Add CFI counter increment statement to the beginning of the original function.
-    orig_fn.block.stmts.insert(
-        0,
-        parse_quote!(
+        let __cfi_ret = __cfi_move(#inline_attr move || {
             caliptra_cfi_lib::CfiCounter::increment();
-        ),
+            #orig_block
+        })();
+        caliptra_cfi_lib::CfiCounter::delay();
+        let __cfi_new_ctr = caliptra_cfi_lib::CfiCounter::decrement();
+        caliptra_cfi_lib::CfiCounter::assert_eq(__cfi_saved_ctr, __cfi_new_ctr);
+        __cfi_ret
     );
 
-    let code = quote! {
-            #wrapper_fn
-            #orig_fn
-    };
-
-    code.into()
+    quote! {
+        #[inline(always)]
+        #f
+    }
+    .into()
 }
