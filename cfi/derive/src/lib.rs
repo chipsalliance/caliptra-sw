ccee65568cd1f6f82c48e2f7b570f94a3694322a
Kor Nielsen <kor@google.com>
2023-11-10 02:02:34 -0800

    CFI derive: Pass parameters to closure rather than capturing them.
    This makes the optimizer a lot smarter.

--- cfi/derive/src/lib.rs
+++ cfi/derive/src/lib.rs
@@ -20,6 +20,9 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse_macro_input;
 use syn::parse_quote;
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
+use syn::FnArg;
 use syn::ItemFn;
 
 #[proc_macro_attribute]
@@ -41,6 +44,23 @@ fn cfi_fn(_mod_fn: bool, input: TokenStream) -> TokenStream {
     // the closure instead.
     f.attrs.retain(|a| !a.path.is_ident("inline"));
 
+    // For some silly reason, LLVM doesn't optimize well if let the closure
+    // capture the arguments. It seems to be fine to let it capture self,
+    // however.
+    let params: Punctuated<_, Comma> = f
+        .sig
+        .inputs
+        .iter()
+        .filter(|f| matches!(f, FnArg::Typed(_)))
+        .collect();
+    let args: Punctuated<_, Comma> = params
+        .iter()
+        .map(|f| match f {
+            FnArg::Typed(a) => &a.pat,
+            _ => unreachable!(),
+        })
+        .collect();
+
     let orig_block = std::mem::replace(&mut f.block, parse_quote!({}));
     f.block.stmts = parse_quote!(
         // This is necessary to allow the inline attribute to be placed on the
@@ -50,10 +70,10 @@ fn cfi_fn(_mod_fn: bool, input: TokenStream) -> TokenStream {
         }
         let __cfi_saved_ctr = caliptra_cfi_lib::CfiCounter::read();
         caliptra_cfi_lib::CfiCounter::delay();
-        let __cfi_ret = __cfi_move(#inline_attr move || {
+        let __cfi_ret = __cfi_move(#inline_attr move |#params| {
             caliptra_cfi_lib::CfiCounter::increment();
             #orig_block
-        })();
+        })(#args);
         caliptra_cfi_lib::CfiCounter::delay();
         let __cfi_new_ctr = caliptra_cfi_lib::CfiCounter::decrement();
         caliptra_cfi_lib::CfiCounter::assert_eq(__cfi_saved_ctr, __cfi_new_ctr);
