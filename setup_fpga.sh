06bd56490574e4c0d1d08844ad4a2c0577ff9c63
Leon Gross <leon.gross@9elements.com>
2023-11-21 09:55:22 +0100

    fix io_module, add sanity checks

--- setup_fpga.sh
+++ setup_fpga.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
+
 CALIPTRA_ROOT=$(pwd)
+
+function usage() {
+    echo " usage: $0 [binfile]"
+}
 
 function disable_cpu_idle() {
     for i in $(seq 1 3); do
         cpu_sysfs=/sys/devices/system/cpu/cpu"$i"/cpuidle/state1/disable
-        echo 1  > "$cpu_sysfs"
+        echo 1 >"$cpu_sysfs"
         echo "    |- cpu[$i]"
 
         # verify options were set
         while IFS= read -r line; do
-            if [[ "$line" -ne "1" ]];then
+            if [[ "$line" -ne "1" ]]; then
                 echo "[-] error setting cpu[$i] into idle state"
+                exit 1
             fi
-        done < "$cpu_sysfs"
+        done <"$cpu_sysfs"
     done
 }
 
 function reduce_fan_speed() {
-    echo 321    > /sys/class/gpio/export
-    echo out    > /sys/class/gpio/gpio321/direction
+    echo 321 >/sys/class/gpio/export
+    echo out >/sys/class/gpio/gpio321/direction
 }
 
 function build_and_install_kernel_modules() {
@@ -27,31 +34,38 @@ function build_and_install_kernel_modules() {
     cd "$CALIPTRA_ROOT"/hw-latest/fpga/rom_backdoor || exit 2
     make
 
-    if [[ -f "$CALIPTRA_ROOT/hw-latest/fpga/rom_backdoor/rom_backdoor.ko" ]];then
-        insmod "$CALIPTRA_ROOT/hw-latest/fpga/rom_backdoor/rom_backdoor.ko"  
+    if [[ -f "$CALIPTRA_ROOT/hw-latest/fpga/rom_backdoor/rom_backdoor.ko" ]]; then
+        insmod "$CALIPTRA_ROOT/hw-latest/fpga/rom_backdoor/rom_backdoor.ko"
+    else
+        echo "[-] error inserting rom bakdoor. module not found"
+        exit 2
     fi
 
-    # io_module
+    # io_module.ko
     cd "$CALIPTRA_ROOT"/hw-latest/fpga/io_module || exit 2
     make
 
-    if [[ -f "$CALIPTRA_ROOT/hw-latest/fpga/rom_backdoor/rom_backdoor.ko" ]];then
-        insmod "$CALIPTRA_ROOT/hw-latest/fpga/rom_backdoor/rom_backdoor.ko"
+    if [[ -f "$CALIPTRA_ROOT/hw-latest/fpga/io_module/io_module.ko" ]]; then
+        insmod "$CALIPTRA_ROOT/hw-latest/fpga/io_module/io_module.ko"
         chmod 666 /dev/uio4
+    else
+        echo "[-] error inserting io module. module not found"
+        exit 2
     fi
 }
 
 function set_fpga_pll_freq() {
-    echo 20000000   > /sys/bus/platform/drivers/xilinx_fclk/fclk0/set_rate
+    echo 20000000 >/sys/bus/platform/drivers/xilinx_fclk/fclk0/set_rate
 }
 
 function install_fpga_image() {
-    if [[ $# -ne 1 ]];then
+    if [[ $# -ne 1 ]]; then
+        echo "[-] no fpga image provided"
         exit 3
     fi
-    
+
     fpga_image="$1"
-    if [[ -z "$fpga_image" ]];then
+    if [[ -z "$fpga_image" ]]; then
         echo "[-] image $fpga_image does not exist. exiting."
         exit 3
     fi
@@ -60,14 +74,15 @@ function install_fpga_image() {
 }
 
 # entryppoint
-if [[ $EUID -ne 0 ]];then
+if [[ $EUID -ne 0 ]]; then
     echo "[-] you must run this script as root"
+    usage "$(basename "$0")"
     exit 1
 fi
 
 # check parameters
-if [[ $# -ne 1 ]];then
-    echo "[-] not enough aruments"
+if [[ $# -ne 1 ]]; then
+    usage "$(basename "$0")"
     exit 1
 fi
 
