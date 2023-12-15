2bfd248995d5e9634d01eedefafaafe6b4a72d82
SRINATH <srinathshiv94@gmail.com>
2023-12-15 23:48:56 +0530

    stress test impactless firmware update (#1002)

--- runtime/tests/runtime_integration_tests/test_boot.rs
+++ runtime/tests/runtime_integration_tests/test_boot.rs
@@ -89,6 +89,59 @@ fn test_update() {
     assert_eq!(fw_rev[1], 0xaabbccdd);
 }
 
+///This test will be run for 500 times if feature "slow_tests" is enabled and just once if the feature is absent
+#[test]
+fn test_stress_update() {
+    let app_versions = [0xaaabbbbc, 0xaaabbbbd];
+    let image_options_0 = ImageOptions {
+        app_version: app_versions[0],
+        ..Default::default()
+    };
+    let image_options_1 = ImageOptions {
+        app_version: app_versions[1],
+        ..Default::default()
+    };
+
+    let image = [
+        caliptra_builder::build_and_sign_image(&FMC_WITH_UART, &APP_WITH_UART, image_options_0)
+            .unwrap()
+            .to_bytes()
+            .unwrap(),
+        caliptra_builder::build_and_sign_image(&FMC_WITH_UART, &APP_WITH_UART, image_options_1)
+            .unwrap()
+            .to_bytes()
+            .unwrap(),
+    ];
+
+    let mut model = run_rt_test(None, None, None);
+
+    let stress_num = if cfg!(feature = "slow_tests") { 500 } else { 1 };
+    let mut image_select = 0;
+
+    model.step_until(|m| m.soc_mbox().status().read().mbox_fsm_ps().mbox_idle());
+
+    for _ in 0..stress_num {
+        if image_select == 0 {
+            image_select = 1;
+        } else {
+            image_select = 0;
+        };
+
+        model
+            .mailbox_execute(u32::from(CommandId::FIRMWARE_LOAD), &image[image_select])
+            .unwrap();
+
+        model
+            .step_until_output_contains("Caliptra RT listening for mailbox commands...")
+            .unwrap();
+
+        //Check if the new firmware is actually the one we built
+        let fw_rev = model.soc_ifc().cptra_fw_rev_id().read();
+        assert_eq!(fw_rev[0], 0xaaaaaaaa);
+        assert_eq!(fw_rev[1], app_versions[image_select]);
+    }
+}
+
 #[test]
 fn test_boot_tci_data() {
     let mut model = run_rt_test(Some(&firmware::runtime_tests::MBOX), None, None);
