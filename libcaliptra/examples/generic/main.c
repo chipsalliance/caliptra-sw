3ca7e4259517989cd93d3e30e021ac9ed904e696
Nick Quarton <139178705+nquarton@users.noreply.github.com>
2023-12-19 14:15:40 -0800

    libcaliptra: adding tests to exercise all commands

--- libcaliptra/examples/generic/main.c
+++ libcaliptra/examples/generic/main.c
@@ -20,6 +20,9 @@ static const uint16_t itrng_entropy_repetition_count = 0xFFFF;
 // Arbitrary example only - values must be customized/tuned for the SoC
 static const uint32_t apb_pauser = 0x1;
 
+// Exists for testbench only - not part of interface for actual implementation
+extern void testbench_reinit(void);
+
 struct caliptra_buffer image_bundle;
 struct caliptra_fuses fuses = {0};
 
@@ -57,7 +60,13 @@ static int set_fuses()
     return status;
 }
 
-int main(int argc, char *argv[])
+void dump_caliptra_error_codes()
+{
+    printf("Caliptra FW error non-fatal code is 0x%x\n", caliptra_read_fw_non_fatal_error());
+    printf("Caliptra FW error fatal code is 0x%x\n", caliptra_read_fw_fatal_error());
+}
+
+int boot_to_ready_for_fw()
 {
     int status;
 
@@ -74,18 +83,32 @@ int main(int argc, char *argv[])
     status = caliptra_mbox_pauser_set_and_lock(apb_pauser);
     if (status) {
         printf("Set MBOX pauser Failed: 0x%x\n", status);
+        return status;
     }
 
     // Set up our PAUSER value for the fuse regs
     status = caliptra_fuse_pauser_set_and_lock(apb_pauser);
     if (status) {
         printf("Set FUSE pauser Failed: 0x%x\n", status);
+        return status;
     }
 
     set_fuses();
 
     // Wait until ready for FW
     caliptra_ready_for_firmware();
+
+    return status;
+}
+
+int legacy_boot_test()
+{
+    int failure = 0;
+    int status = boot_to_ready_for_fw();
+
+    if (status){
+        failure = 1;
+    }
 
     // Load Image Bundle
     // FW_PATH is defined on the compiler command line
@@ -94,68 +117,442 @@ int main(int argc, char *argv[])
     if (status)
     {
         printf("FW Load Failed: 0x%x\n", status);
-    }
-    else
-    {
+        failure = 1;
+    } else {
         printf("FW Load: OK\n");
     }
 
     // Send a FIPS version command in async mode
-    if (!status) {
-        struct caliptra_fips_version_resp version;
-        // Send async
-        status = caliptra_fips_version(&version, true);
+    struct caliptra_fips_version_resp version;
+    // Send async
+    status = caliptra_fips_version(&version, true);
 
-        if (status) {
-            printf("Get FIPS Version send failed: 0x%x\n", status);
-        } else {
-            // Wait indefinitely for completion
-            while (!caliptra_test_for_completion()){
-                caliptra_wait();
-            }
-
-            status = caliptra_complete();
+    if (status) {
+        printf("Get FIPS Version send failed: 0x%x\n", status);
+        failure = 1;
+    } else {
+        // Wait indefinitely for completion
+        while (!caliptra_test_for_completion()){
+            caliptra_wait();
         }
 
-        if (status)
-        {
-            printf("Get FIPS Version failed: 0x%x\n", status);
-        }
-        else
-        {
-            printf("FIPS_VERSION = mode: 0x%x, fips_rev (0x%x, 0x%x, 0x%x), name %s \n", version.mode,
-                version.fips_rev[0], version.fips_rev[1], version.fips_rev[2], version.name);
-        }
-    }
-
-    // Send a stash measurement command with async off
-    if (!status) {
-        // Need some representative values for these, see below.
-        struct caliptra_stash_measurement_req r = {0};
-        struct caliptra_stash_measurement_resp c = {0};
-
-        status = caliptra_stash_measurement(&r, &c, false);
-
-        if (status)
-        {
-            printf("Stash measurement failed: 0x%x\n", status);
-        }
-        else
-        {
-            printf("Stash measurement: OK\n");
-        }
+        status = caliptra_complete();
     }
 
     if (status)
     {
-        printf("Caliptra C API Integration Test Failed: 0x%x\n", status);
-        printf("Caliptra FW error non-fatal code is 0x%x\n", caliptra_read_fw_non_fatal_error());
-        printf("Caliptra FW error fatal code is 0x%x\n", caliptra_read_fw_fatal_error());
+        printf("Get FIPS Version failed: 0x%x\n", status);
+        failure = 1;
     }
     else
     {
-        printf("Caliptra C API Integration Test Passed!\n");
+        printf("FIPS_VERSION = mode: 0x%x, fips_rev (0x%x, 0x%x, 0x%x), name %s \n", version.mode,
+            version.fips_rev[0], version.fips_rev[1], version.fips_rev[2], version.name);
     }
 
-    return status;
+    // Send a stash measurement command with async off
+    // Need some representative values for these, see below.
+    struct caliptra_stash_measurement_req r = {0};
+    struct caliptra_stash_measurement_resp c = {0};
+
+    status = caliptra_stash_measurement(&r, &c, false);
+
+    if (status) {
+        printf("Stash measurement failed: 0x%x\n", status);
+        failure = 1;
+    } else {
+        printf("Stash measurement: OK\n");
+    }
+
+    return failure;
+}
+
+// Issue every ROM command
+// Intent is just to make sure ROM accepts the command ID and the payload
+// Not attempting to actually test command functionality
+int rom_test_all_commands()
+{
+    int failure = 0;
+    int status = boot_to_ready_for_fw();
+
+    if (status){
+        dump_caliptra_error_codes();
+        failure = 1;
+    }
+
+    // STASH_MEASUREMENT
+    struct caliptra_stash_measurement_req stash_req = {};
+    struct caliptra_stash_measurement_resp stash_resp = {};
+
+    status = caliptra_stash_measurement(&stash_req, &stash_resp, false);
+
+    if (status) {
+        printf("Stash Measurement failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Stash Measurement: OK\n");
+    }
+
+    // CAPABILITIES
+    struct caliptra_capabilities_resp cap_resp = {};
+
+    status = caliptra_capabilities(&cap_resp, false);
+
+    if (status) {
+        printf("Capabilities failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Capabilities: OK\n");
+    }
+
+    // FIPS_VERSION
+    struct caliptra_fips_version_resp version_resp = {};
+
+    status = caliptra_fips_version(&version_resp, false);
+
+    if (status) {
+        printf("FIPS Version failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Version: OK\n");
+    }
+
+    // SELF_TEST_START
+    status = caliptra_self_test_start(false);
+
+    if (status) {
+        printf("FIPS Self Test Start failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Self Test Start: OK\n");
+    }
+
+    // SELF_TEST_GET_RESULTS
+    status = caliptra_self_test_get_results(false);
+
+    if (status) {
+        printf("FIPS Self Test Get Results failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Self Test Get Results: OK\n");
+    }
+    
+    // SHUTDOWN
+    status = caliptra_shutdown(false);
+
+    if (status) {
+        printf("FIPS Shutdown failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Shutdown: OK\n");
+    }
+
+    return failure;
+}
+
+// Issue every runtime command
+// Intent is just to make sure FW accepts the command ID and the payload
+// Not attempting to actually test command functionality
+int rt_test_all_commands()
+{
+    int failure = 0;
+    uint32_t non_fatal_error;
+    int status = boot_to_ready_for_fw();
+
+    if (status){
+        failure = 1;
+    }
+
+    // Load Image Bundle
+    // FW_PATH is defined on the compiler command line
+    status = caliptra_upload_fw(&image_bundle, false);
+
+    if (status)
+    {
+        printf("FW Load Failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FW Load: OK\n");
+    }
+
+    // GET_IDEV_CERT
+    struct caliptra_get_idev_cert_req idev_cert_req = {};
+    struct caliptra_get_idev_cert_resp idev_cert_resp = {};
+
+    status = caliptra_get_idev_cert(&idev_cert_req, &idev_cert_resp, false);
+
+    if (status) {
+        printf("Get IDEV Cert failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Get IDEV Cert: OK\n");
+    }
+
+    // GET_IDEV_INFO
+    struct caliptra_get_idev_info_resp idev_info_resp = {};
+
+    status = caliptra_get_idev_info(&idev_info_resp, false);
+
+    if (status) {
+        printf("Get IDEV Info: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Get IDEV Info: OK\n");
+    }
+
+    // POPULATE_IDEV_CERT
+    struct caliptra_populate_idev_cert_req populate_idev_req = {};
+
+    status = caliptra_populate_idev_cert(&populate_idev_req, false);
+
+    if (status) {
+        printf("Populate IDEV Cert failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Populate IDEV Cert: OK\n");
+    }
+
+    // GET_LDEV_CERT
+    struct caliptra_get_ldev_cert_resp ldev_cert_resp = {};
+
+    status = caliptra_get_ldev_cert(&ldev_cert_resp, false);
+
+    if (status) {
+        printf("Get LDEV Cert failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Get LDEV Cert: OK\n");
+    }
+
+    // GET_FMC_ALIAS_CERT
+    struct caliptra_get_fmc_alias_cert_resp fmc_alias_cert_resp = {};
+
+    status = caliptra_get_fmc_alias_cert(&fmc_alias_cert_resp, false);
+
+    if (status) {
+        printf("Get FMC Alias Cert failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Get FMC Alias Cert: OK\n");
+    }
+
+    // GET_RT_ALIAS_CERT
+    struct caliptra_get_rt_alias_cert_resp rt_alias_cert_resp = {};
+
+    status = caliptra_get_rt_alias_cert(&rt_alias_cert_resp, false);
+
+    if (status) {
+        printf("Get Runtime Alias Cert failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Get Runtime Alias Cert: OK\n");
+    }
+
+    // ECDSA384_VERIFY
+    struct caliptra_ecdsa_verify_req ecdsa_req = {};
+
+    status = caliptra_ecdsa384_verify(&ecdsa_req, false);
+
+    // Not testing for full success
+    // Instead, just want to see it give the right ECC-specific error
+    // This still proves the FW recognizes the message and request data and got to the right ECC code
+    uint32_t DRIVER_ECC384_KEYGEN_BAD_USAGE = 0x5000f;
+    non_fatal_error = caliptra_read_fw_non_fatal_error();
+    if (status != MBX_STATUS_FAILED || non_fatal_error != DRIVER_ECC384_KEYGEN_BAD_USAGE) {
+        printf("ECDSA384 Verify unexpected result/failure: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("ECDSA384 Verify: OK\n");
+    }
+
+    // STASH_MEASUREMENT
+    struct caliptra_stash_measurement_req stash_req = {};
+    struct caliptra_stash_measurement_resp stash_resp = {};
+
+    status = caliptra_stash_measurement(&stash_req, &stash_resp, false);
+
+    if (status) {
+        printf("Stash Measurement failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Stash Measurement: OK\n");
+    }
+
+    // INVOKE_DPE_COMMAND
+    struct caliptra_invoke_dpe_req dpe_req = {};
+    struct caliptra_invoke_dpe_resp dpe_resp = {};
+
+    status = caliptra_invoke_dpe_command(&dpe_req, &dpe_resp, false);
+
+    // Not testing for full success
+    // Instead, just want to see it give the right DPE-specific error
+    // This still proves the FW recognizes the message and request data and got to the right DPE code
+    uint32_t RUNTIME_DPE_COMMAND_DESERIALIZATION_FAILED = 0xe002b;
+    non_fatal_error = caliptra_read_fw_non_fatal_error();
+    if (status != MBX_STATUS_FAILED || non_fatal_error != RUNTIME_DPE_COMMAND_DESERIALIZATION_FAILED) {
+        printf("DPE Command unexpected result/failure: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("DPE Command: OK\n");
+    }
+
+
+    // FW_INFO
+    struct caliptra_fw_info_resp fw_info_resp = {};
+
+    status = caliptra_fw_info(&fw_info_resp, false);
+
+    if (status) {
+        printf("FW Info failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FW Info: OK\n");
+    }
+
+    // DPE_TAG_TCI
+    struct caliptra_dpe_tag_tci_req tag_tci_req = {};
+
+    status = caliptra_dpe_tag_tci(&tag_tci_req, false);
+
+    if (status) {
+        printf("DPE Tag TCI failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("DPE Tag TCI: OK\n");
+    }
+
+    // DPE_GET_TAGGED_TCI
+    struct caliptra_get_tagged_tci_req get_tagged_tci_req = {};
+    struct caliptra_get_tagged_tci_resp get_tagged_tci_resp = {};
+
+    status = caliptra_dpe_get_tagged_tci(&get_tagged_tci_req, &get_tagged_tci_resp, false);
+
+    if (status) {
+        printf("DPE Get Tagged TCI failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("DPE Get Tagged TCI: OK\n");
+    }
+
+    // FIPS_VERSION
+    struct caliptra_fips_version_resp version_resp = {};
+
+    status = caliptra_fips_version(&version_resp, false);
+
+    if (status) {
+        printf("FIPS Version failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Version: OK\n");
+    }
+
+    // SELF_TEST_START
+    status = caliptra_self_test_start(false);
+
+    if (status) {
+        printf("FIPS Self Test Start failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Self Test Start: OK\n");
+    }
+
+    // Give self test time to run
+    for (int i = 0; i < 4000000; i++){
+        caliptra_wait();
+    }
+
+    // SELF_TEST_GET_RESULTS
+    status = caliptra_self_test_get_results(false);
+
+    if (status) {
+        printf("FIPS Self Test Get Results failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Self Test Get Results: OK\n");
+    }
+    
+    // These commands are last since they cause lasting affects on the runtime
+    // DISABLE_ATTESTATION
+    status = caliptra_disable_attestation(false);
+
+    if (status) {
+        printf("Disable Attestation failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("Disable Attestation: OK\n");
+    }
+
+    // SHUTDOWN
+    status = caliptra_shutdown(false);
+
+    if (status) {
+        printf("FIPS Shutdown failed: 0x%x\n", status);
+        dump_caliptra_error_codes();
+        failure = 1;
+    } else {
+        printf("FIPS Shutdown: OK\n");
+    }
+
+    return failure;
+}
+
+
+// Test infrastructure
+
+int global_test_result = 0;
+
+void run_test( int func(void), char* test_name)
+{
+    testbench_reinit();
+    int result = func();
+
+    if (result) {
+        printf("\n\n\t%s FAILED\n", test_name);
+        dump_caliptra_error_codes();
+        printf("\n\n");
+    } else {
+        printf("\n\n\t%s PASSED\n\n\n", test_name);
+    }
+
+    global_test_result |= result;
+}
+
+int main(int argc, char *argv[])
+{
+    global_test_result = 0;
+
+    run_test(legacy_boot_test, "Legacy boot test");
+    run_test(rom_test_all_commands, "Test all ROM commands");
+    run_test(rt_test_all_commands, "Test all Runtime commmands");
+
+    if (global_test_result) {
+        printf("libcaliptra test failures reported");
+    } else {
+        printf("All libcaliptra tests passed");
+    }
+
+    return global_test_result;
 }
