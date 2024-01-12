e03961e2223a1ae2df22b8a0e44c04f894839505
Nick Quarton <139178705+nquarton@users.noreply.github.com>
2024-01-12 09:21:44 -0800

    Adding revision and digest for FW, FMC, and ROM to FW_INFO cmd

--- runtime/tests/runtime_integration_tests/test_info.rs
+++ runtime/tests/runtime_integration_tests/test_info.rs
@@ -2,16 +2,36 @@
 
 use crate::common::run_rt_test;
 use caliptra_builder::{
-    firmware::{APP_WITH_UART, FMC_WITH_UART},
+    firmware::{self, APP_WITH_UART, FMC_WITH_UART},
     ImageOptions,
 };
 use caliptra_common::mailbox_api::{
     CommandId, FwInfoResp, GetIdevInfoResp, MailboxReqHeader, MailboxRespHeader,
 };
-use caliptra_hw_model::{DefaultHwModel, HwModel};
+use caliptra_hw_model::{BootParams, DefaultHwModel, HwModel, InitParams};
+use caliptra_image_types::RomInfo;
+use core::mem::size_of;
 use zerocopy::{AsBytes, FromBytes};
 
 const RT_READY_FOR_COMMANDS: u32 = 0x600;
+
+fn find_rom_info(rom: Vec<u8>) -> Option<RomInfo> {
+    // RomInfo is 64-byte aligned and the last data in the ROM bin
+    // Iterate backwards by 64-byte increments (assumes rom size will always be 64 byte aligned)
+    for i in (0..rom.len() - 63).rev().step_by(64) {
+        let chunk = &rom[i..i + 64];
+
+        // Check if the chunk contains non-zero data
+        if chunk.iter().any(|&byte| byte != 0) {
+            // Found non-zero data, return RomInfo constructed from the data
+            let rom_info = RomInfo::read_from(&rom[i..i + size_of::<RomInfo>()])?;
+            return Some(rom_info);
+        }
+    }
+
+    // No non-zero data found
+    None
+}
 
 #[test]
 fn test_fw_info() {
@@ -24,7 +44,25 @@ fn test_fw_info() {
     let mut image_opts10 = image_opts.clone();
     image_opts10.app_svn = 10;
 
-    let mut model = run_rt_test(None, Some(image_opts10), None);
+    // Cannot use run_rt_test since we need the rom and image to verify info
+    let rom = caliptra_builder::build_firmware_rom(firmware::rom_from_env()).unwrap();
+    let init_params = InitParams {
+        rom: &rom,
+        ..Default::default()
+    };
+
+    let image =
+        caliptra_builder::build_and_sign_image(&FMC_WITH_UART, &APP_WITH_UART, image_opts10)
+            .unwrap();
+
+    let mut model = caliptra_hw_model::new(BootParams {
+        init_params,
+        fw_image: Some(&image.to_bytes().unwrap()),
+        ..Default::default()
+    })
+    .unwrap();
+
+    let rom_info = find_rom_info(rom).unwrap();
 
     let get_fwinfo = |model: &mut DefaultHwModel| {
         let payload = MailboxReqHeader {
@@ -66,6 +104,13 @@ fn test_fw_info() {
     assert_eq!(info.fmc_manifest_svn, 5);
     assert_eq!(info.runtime_svn, 10);
     assert_eq!(info.min_runtime_svn, 10);
+    // Verify revision (Commit ID) and digest of each component
+    assert_eq!(info.rom_revision, rom_info.revision);
+    assert_eq!(info.fmc_revision, image.manifest.fmc.revision);
+    assert_eq!(info.runtime_revision, image.manifest.runtime.revision);
+    assert_eq!(info.rom_sha256_digest, rom_info.sha256_digest);
+    assert_eq!(info.fmc_sha384_digest, image.manifest.fmc.digest);
+    assert_eq!(info.runtime_sha384_digest, image.manifest.runtime.digest);
 
     // Make image with newer SVN.
     let mut image_opts20 = image_opts.clone();
