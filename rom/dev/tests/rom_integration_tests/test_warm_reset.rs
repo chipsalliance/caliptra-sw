0db1b66cbdbc65728471e79ae5882a88a5b2ac07
srinathshiv <srinaths@ami.com>
2024-02-02 23:05:32 +0530

    Removed the config line instead of commenting it out

--- rom/dev/tests/rom_integration_tests/test_warm_reset.rs
+++ rom/dev/tests/rom_integration_tests/test_warm_reset.rs
@@ -1,7 +1,5 @@
 // Licensed under the Apache-2.0 license
 
-//#![cfg(any(feature = "verilator", feature = "fpga_realtime"))]
-
 use caliptra_builder::firmware::FMC_WITH_UART;
 use caliptra_builder::firmware::{APP_WITH_UART, ROM_WITH_UART};
 use caliptra_builder::ImageOptions;
