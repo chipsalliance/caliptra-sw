9230429e4b3ca090a49a907bc147530eccd38de9
Kor Nielsen <kor@google.com>
2024-02-22 15:35:08 -0800

    Move hw-latest/{fpga,verilated} to hw/
    Also, move hw-latest/caliptra-rtl to hw/latest/rtl.
    
    This is in preparation of moving the current caliptra-rtl submodule from
    hw/latest/rtl to hw/1.0/rtl, and replacing hw/latest/rtl with the 1.1
    caliptra-rtl branch.

--- registers/update.sh
+++ registers/update.sh
@@ -2,10 +2,10 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [[ ! -f "../hw-latest/caliptra-rtl/.git" ]]; then
+if [[ ! -f "../hw/latest/rtl/.git" ]]; then
     echo "caliptra-rtl submodule is not populated"
     echo "Please run 'git submodule update --init'"
     exit 1
 fi
 
-cargo run --manifest-path bin/generator/Cargo.toml -- ../hw-latest/caliptra-rtl bin/extra-rdl/ src/
+cargo run --manifest-path bin/generator/Cargo.toml -- ../hw/latest/rtl bin/extra-rdl/ src/
