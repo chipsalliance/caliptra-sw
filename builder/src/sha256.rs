350f1c563f98795057a1d997fa20aaec6d67f9e3
Sree Revoori <srrevoori@google.com>
2024-04-04 21:53:19 +0000

    Use sha2 instead of openssl to hash in builder

--- builder/src/sha256.rs
+++ builder/src/sha256.rs
@@ -1,11 +1,12 @@
 // Licensed under the Apache-2.0 license
+use sha2::{Digest, Sha256};
 
 pub fn sha256_word_reversed(bytes: &[u8]) -> [u32; 8] {
-    let mut sha = openssl::sha::Sha256::new();
+    let mut sha = Sha256::new();
     for i in 0..bytes.len() / 4 {
         let word = u32::from_le_bytes(bytes[i * 4..][..4].try_into().unwrap());
-        sha.update(&word.swap_bytes().to_le_bytes());
+        sha.update(word.swap_bytes().to_le_bytes());
     }
-    let result_bytes = sha.finish();
+    let result_bytes = sha.finalize();
     core::array::from_fn(|i| u32::from_be_bytes(result_bytes[i * 4..][..4].try_into().unwrap()))
 }
