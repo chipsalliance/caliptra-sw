332cbf592f67054f0ef8074ee75edfea58a93459
Nick Quarton <139178705+nquarton@users.noreply.github.com>
2024-05-16 17:12:57 -0700

    Updating ROM version to 1.0.2

--- builder/src/version.rs
+++ builder/src/version.rs
@@ -2,7 +2,7 @@
 
 pub const ROM_VERSION_MAJOR: u16 = 1;
 pub const ROM_VERSION_MINOR: u16 = 0;
-pub const ROM_VERSION_PATCH: u16 = 1;
+pub const ROM_VERSION_PATCH: u16 = 2;
 
 // ROM Version - 16 bits
 // Major - 5 bits [15:11]
