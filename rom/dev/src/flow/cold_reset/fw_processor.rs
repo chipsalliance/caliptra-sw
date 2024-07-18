ce77388a30abba5691a0a103d216b067d4a69f04
Vishal Mhatre <38512878+mhatrevi@users.noreply.github.com>
2024-07-18 11:18:33 -0700

    [fix] Raise a fatal error in the ROM if max. limit of measurements is crossed (#1596)
    * [feat] Raise a fatal error in the ROM if max. limit of measurements is crossed.
    
    This change updates the logic when a stash measurement is requested beyond the max. supported.
    The new code raises a fatal error instead of continuing the boot sequence.

--- rom/dev/src/flow/cold_reset/fw_processor.rs
+++ rom/dev/src/flow/cold_reset/fw_processor.rs
@@ -278,10 +278,14 @@ impl FirmwareProcessor {
                     CommandId::STASH_MEASUREMENT => {
                         if persistent_data.fht.meas_log_index == MEASUREMENT_MAX_COUNT as u32 {
                             cprintln!(
-                                "[fwproc] Maximum supported number of measurements already received, ignoring."
+                                "[fwproc] Maximum supported number of measurements already received."
                             );
                             txn.complete(false)?;
-                            continue;
+
+                            // Raise a fatal error on hitting the max. limit.
+                            // This ensures that any SOC ROM/FW couldn't send a stash measurement
+                            // that wasn't properly stored within Caliptra.
+                            return Err(CaliptraError::FW_PROC_MAILBOX_STASH_MEASUREMENT_MAX_LIMIT);
                         }
 
                         Self::stash_measurement(pcr_bank, env.sha384, persistent_data, &mut txn)?;
