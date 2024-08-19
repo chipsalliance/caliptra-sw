c0b77565a2d956397a77005c8748597c62987fe3
Vishal Mhatre <vishal.mhatre@gmail.com>
2024-08-19 16:27:15 -0700

    [feat] Manifest Based Image Authorization - Manifest creation tool
    This change contains the manifest creation tool for the Manifest Based Image Authorization feature.

---
+++ auth-manifest/gen/src/generator.rs
@@ -0,0 +1,166 @@
+/*++
+
+Licensed under the Apache-2.0 license.
+
+File Name:
+
+   generator.rs
+
+Abstract:
+
+    Caliptra Authorization Manifest generator
+
+--*/
+
+use caliptra_image_gen::ImageGeneratorCrypto;
+use zerocopy::AsBytes;
+
+use crate::*;
+use core::mem::size_of;
+
+/// Authorization Manifest generator
+pub struct AuthManifestGenerator<Crypto: ImageGeneratorCrypto> {
+    crypto: Crypto,
+}
+
+impl<Crypto: ImageGeneratorCrypto> AuthManifestGenerator<Crypto> {
+    /// Create an instance `AuthManifestGenerator`
+    pub fn new(crypto: Crypto) -> Self {
+        Self { crypto }
+    }
+
+    pub fn generate(
+        &self,
+        config: &AuthManifestGeneratorConfig,
+    ) -> anyhow::Result<AuthorizationManifest> {
+        let mut auth_manifest = AuthorizationManifest::default();
+
+        if config.image_metadata_list.len() > AUTH_MANIFEST_IMAGE_METADATA_MAX_COUNT {
+            eprintln!(
+                "Encountered an error converting image_metadata_list, only {} entries supported.",
+                AUTH_MANIFEST_IMAGE_METADATA_MAX_COUNT
+            );
+            return Err(anyhow::anyhow!("Error converting image_metadata_list"));
+        }
+
+        // Generate the Image Metadata List.
+        let slice = config.image_metadata_list.as_slice();
+        auth_manifest.image_metadata_col.image_metadata_array[..slice.len()].copy_from_slice(slice);
+
+        auth_manifest.image_metadata_col.header.entry_count =
+            config.image_metadata_list.len() as u32;
+        auth_manifest.image_metadata_col.header.revision = 0; // [TODO] Need to update this.
+
+        // Generate the preamble.
+        auth_manifest.preamble.marker = AUTH_MANIFEST_MARKER;
+        auth_manifest.preamble.size = size_of::<AuthManifestPreamble>() as u32;
+        auth_manifest.preamble.version = config.version;
+        auth_manifest.preamble.flags = config.flags;
+
+        // Sign the vendor manifest keys.
+        auth_manifest.preamble.vendor_pub_keys = config.vendor_man_key_info.pub_keys;
+
+        let range = AuthManifestPreamble::vendor_signed_data_range();
+
+        let data = auth_manifest
+            .preamble
+            .as_bytes()
+            .get(range.start as usize..)
+            .ok_or_else(|| anyhow::anyhow!("Failed to get vendor signed data range start"))?
+            .get(..range.len())
+            .ok_or(anyhow::anyhow!(
+                "Failed to get vendor signed data range length"
+            ))?;
+
+        let digest = self.crypto.sha384_digest(data)?;
+
+        if let Some(priv_keys) = config.vendor_fw_key_info.priv_keys {
+            let sig = self.crypto.ecdsa384_sign(
+                &digest,
+                &priv_keys.ecc_priv_key,
+                &config.vendor_fw_key_info.pub_keys.ecc_pub_key,
+            )?;
+            auth_manifest.preamble.vendor_pub_keys_signatures.ecc_sig = sig;
+
+            let lms_sig = self.crypto.lms_sign(&digest, &priv_keys.lms_priv_key)?;
+            auth_manifest.preamble.vendor_pub_keys_signatures.lms_sig = lms_sig;
+        }
+
+        // Sign the owner manifest keys.
+        if let Some(owner_fw_config) = &config.owner_fw_key_info {
+            if let Some(owner_man_config) = &config.owner_man_key_info {
+                auth_manifest.preamble.owner_pub_keys = owner_man_config.pub_keys;
+
+                let digest = self
+                    .crypto
+                    .sha384_digest(auth_manifest.preamble.owner_pub_keys.as_bytes())?;
+
+                if let Some(owner_fw_priv_keys) = owner_fw_config.priv_keys {
+                    let sig = self.crypto.ecdsa384_sign(
+                        &digest,
+                        &owner_fw_priv_keys.ecc_priv_key,
+                        &owner_fw_config.pub_keys.ecc_pub_key,
+                    )?;
+                    auth_manifest.preamble.owner_pub_keys_signatures.ecc_sig = sig;
+                    let lms_sig = self
+                        .crypto
+                        .lms_sign(&digest, &owner_fw_priv_keys.lms_priv_key)?;
+                    auth_manifest.preamble.owner_pub_keys_signatures.lms_sig = lms_sig;
+                }
+            }
+        }
+
+        // Sign the Image Metadata List.
+        let digest = self
+            .crypto
+            .sha384_digest(auth_manifest.image_metadata_col.as_bytes())?;
+
+        // Sign with the vendor manifest public keys if indicated in the flags.
+        if auth_manifest.preamble.flags & AUTH_MANIFEST_VENDOR_SIGNATURE_REQURIED_FLAG != 0 {
+            if let Some(vendor_man_priv_keys) = config.vendor_man_key_info.priv_keys {
+                let sig = self.crypto.ecdsa384_sign(
+                    &digest,
+                    &vendor_man_priv_keys.ecc_priv_key,
+                    &config.vendor_man_key_info.pub_keys.ecc_pub_key,
+                )?;
+                auth_manifest
+                    .preamble
+                    .vendor_image_metdata_signatures
+                    .ecc_sig = sig;
+
+                let lms_sig = self
+                    .crypto
+                    .lms_sign(&digest, &vendor_man_priv_keys.lms_priv_key)?;
+                auth_manifest
+                    .preamble
+                    .vendor_image_metdata_signatures
+                    .lms_sig = lms_sig;
+            }
+        }
+
+        // Sign with the owner manifest public keys.
+        if let Some(owner_man_config) = &config.owner_man_key_info {
+            if let Some(owner_man_priv_keys) = &owner_man_config.priv_keys {
+                let sig = self.crypto.ecdsa384_sign(
+                    &digest,
+                    &owner_man_priv_keys.ecc_priv_key,
+                    &owner_man_config.pub_keys.ecc_pub_key,
+                )?;
+                auth_manifest
+                    .preamble
+                    .owner_image_metdata_signatures
+                    .ecc_sig = sig;
+
+                let lms_sig = self
+                    .crypto
+                    .lms_sign(&digest, &owner_man_priv_keys.lms_priv_key)?;
+                auth_manifest
+                    .preamble
+                    .owner_image_metdata_signatures
+                    .lms_sig = lms_sig;
+            }
+        }
+
+        Ok(auth_manifest)
+    }
+}
---
+++ auth-manifest/types/src/lib.rs
@@ -0,0 +1,168 @@
+/*++
+
+Licensed under the Apache-2.0 license.
+
+File Name:
+
+   lib.rs
+
+Abstract:
+
+    File contains data structures for the image authorization manifest bundle.
+
+--*/
+
+#![no_std]
+
+use core::ops::Range;
+
+use caliptra_image_types::*;
+use core::default::Default;
+use memoffset::span_of;
+use zerocopy::{AsBytes, FromBytes};
+use zeroize::Zeroize;
+
+pub const AUTH_MANIFEST_MARKER: u32 = 0x4154_4D4E;
+pub const AUTH_MANIFEST_IMAGE_METADATA_MAX_COUNT: usize = 16;
+pub const AUTH_MANIFEST_VENDOR_SIGNATURE_REQURIED_FLAG: u32 = 0x1;
+
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Default, Debug, Clone, Copy, Zeroize)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthManifestPubKeys {
+    pub ecc_pub_key: ImageEccPubKey,
+    #[zeroize(skip)]
+    pub lms_pub_key: ImageLmsPublicKey,
+}
+
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Default, Debug, Clone, Copy, Zeroize)]
+pub struct AuthManifestPrivKeys {
+    pub ecc_priv_key: ImageEccPrivKey,
+    #[zeroize(skip)]
+    pub lms_priv_key: ImageLmsPrivKey,
+}
+
+#[repr(C)]
+#[derive(AsBytes, Clone, Copy, FromBytes, Default, Debug, Zeroize)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthManifestSignatures {
+    pub ecc_sig: ImageEccSignature,
+    #[zeroize(skip)]
+    pub lms_sig: ImageLmsSignature,
+}
+
+/// Caliptra Authorization Image Manifest Preamble
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Clone, Copy, Debug, Zeroize, Default)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthManifestPreamble {
+    pub marker: u32,
+
+    pub size: u32,
+
+    pub version: u32,
+
+    pub flags: u32,
+
+    pub vendor_pub_keys: AuthManifestPubKeys,
+
+    pub vendor_pub_keys_signatures: AuthManifestSignatures,
+
+    pub owner_pub_keys: AuthManifestPubKeys,
+
+    pub owner_pub_keys_signatures: AuthManifestSignatures,
+
+    pub vendor_image_metdata_signatures: AuthManifestSignatures,
+
+    pub owner_image_metdata_signatures: AuthManifestSignatures,
+}
+
+impl AuthManifestPreamble {
+    /// Returns `Range<u32>` containing the version, flags and vendor manifest pub keys.
+    pub fn vendor_signed_data_range() -> Range<u32> {
+        let span = span_of!(AuthManifestPreamble, version..=vendor_pub_keys);
+        span.start as u32..span.end as u32
+    }
+
+    /// Returns `Range<u32>` containing the vendor_pub_keys_signatures
+    pub fn vendor_pub_keys_signatures_range() -> Range<u32> {
+        let span = span_of!(AuthManifestPreamble, vendor_pub_keys_signatures);
+        span.start as u32..span.end as u32
+    }
+
+    /// Returns `Range<u32>` containing the owner_pub_keys
+    pub fn owner_pub_keys_range() -> Range<u32> {
+        let span = span_of!(AuthManifestPreamble, owner_pub_keys);
+        span.start as u32..span.end as u32
+    }
+
+    /// Returns `Range<u32>` containing the owner_pub_keys_signatures
+    pub fn owner_pub_keys_signatures_range() -> Range<u32> {
+        let span = span_of!(AuthManifestPreamble, owner_pub_keys_signatures);
+        span.start as u32..span.end as u32
+    }
+
+    /// Returns `Range<u32>` containing the vendor_image_metdata_signatures
+    pub fn vendor_image_metdata_signatures_range() -> Range<u32> {
+        let span = span_of!(AuthManifestPreamble, vendor_image_metdata_signatures);
+        span.start as u32..span.end as u32
+    }
+
+    /// Returns `Range<u32>` containing the owner_image_metdata_signatures
+    pub fn owner_image_metdata_signatures_range() -> Range<u32> {
+        let span = span_of!(AuthManifestPreamble, owner_image_metdata_signatures);
+        span.start as u32..span.end as u32
+    }
+}
+
+/// Caliptra Authorization Manifest Image Metadata
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Clone, Copy, Debug, Zeroize)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthManifestImageMetadata {
+    pub digest: [u8; 48],
+
+    pub image_source: u32,
+}
+
+/// Caliptra Authorization Manifest Image Metadata Collection Header
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Clone, Copy, Debug, Zeroize, Default)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthManifestImageMetadataCollectionHeader {
+    pub revision: u32,
+
+    pub reserved: [u8; 12],
+
+    pub entry_count: u32,
+}
+
+impl Default for AuthManifestImageMetadata {
+    fn default() -> Self {
+        AuthManifestImageMetadata {
+            digest: [0; 48],
+            image_source: 0,
+        }
+    }
+}
+
+/// Caliptra Authorization Manifest Image Metadata Collection
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Clone, Copy, Debug, Zeroize, Default)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthManifestImageMetadataCollection {
+    pub header: AuthManifestImageMetadataCollectionHeader,
+
+    pub image_metadata_array: [AuthManifestImageMetadata; AUTH_MANIFEST_IMAGE_METADATA_MAX_COUNT],
+}
+
+/// Caliptra Image Authorization Manifest
+#[repr(C)]
+#[derive(AsBytes, FromBytes, Clone, Copy, Debug, Zeroize, Default)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct AuthorizationManifest {
+    pub preamble: AuthManifestPreamble,
+
+    pub image_metadata_col: AuthManifestImageMetadataCollection,
+}
