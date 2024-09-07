e41a13f3597ee2a55fbafe4e1e53f9a0442d2b83
Anthony Rocha <anthony.rocha@amd.com>
2024-09-06 18:34:36 -0700

    - Modify HwModel  trait so it boots with a set of  five valid pausers for all supported model types. - Add a negative test to ascertain that , when FpgaRealTime is selected, a  SIGBUS is raised if an invalid   PAUSER tries to access the mailbox.

--- hw-model/src/lib.rs
+++ hw-model/src/lib.rs
@@ -81,6 +81,15 @@ pub type DefaultHwModel = ModelVerilated;
 #[cfg(feature = "fpga_realtime")]
 pub type DefaultHwModel = ModelFpgaRealtime;
 
+pub enum ValidPaUsers {
+    Pl0 = 0x01,
+    Pl1 = 0x02,
+    Pl2 = 0x03,
+    Pl3 = 0x04,
+    Pl4 = 0x05,
+}
+pub const DEFAULT_APB_PAUSER: u32 = ValidPaUsers::Pl0 as u32;
+
 /// Constructs an HwModel based on the cargo features and environment
 /// variables. Most test cases that need to construct a HwModel should use this
 /// function over HwModel::new_unbooted().
@@ -252,7 +261,7 @@ pub struct BootParams<'a> {
     pub initial_dbg_manuf_service_reg: u32,
     pub initial_repcnt_thresh_reg: Option<CptraItrngEntropyConfig1WriteVal>,
     pub initial_adaptp_thresh_reg: Option<CptraItrngEntropyConfig0WriteVal>,
-    pub valid_pauser: u32,
+    pub valid_pauser: [u32; 5],
     pub wdt_timeout_cycles: u64,
 }
 
@@ -264,7 +273,13 @@ impl<'a> Default for BootParams<'a> {
             initial_dbg_manuf_service_reg: Default::default(),
             initial_repcnt_thresh_reg: Default::default(),
             initial_adaptp_thresh_reg: Default::default(),
-            valid_pauser: 0x1,
+            valid_pauser: [
+                ValidPaUsers::Pl0 as u32,
+                ValidPaUsers::Pl1 as u32,
+                ValidPaUsers::Pl2 as u32,
+                ValidPaUsers::Pl3 as u32,
+                ValidPaUsers::Pl4 as u32,
+            ],
             wdt_timeout_cycles: EXPECTED_CALIPTRA_BOOT_TIME_IN_CYCLES,
         }
     }
@@ -514,15 +529,18 @@ pub trait HwModel: SocManager {
                 .write(|_| reg);
         }
 
-        // Set up the PAUSER as valid for the mailbox (using index 0)
-        self.soc_ifc()
-            .cptra_mbox_valid_pauser()
-            .at(0)
-            .write(|_| boot_params.valid_pauser);
-        self.soc_ifc()
-            .cptra_mbox_pauser_lock()
-            .at(0)
-            .write(|w| w.lock(true));
+        {
+            for idx in 0..5 {
+                self.soc_ifc()
+                    .cptra_mbox_valid_pauser()
+                    .at(idx)
+                    .write(|_| boot_params.valid_pauser[idx]);
+                self.soc_ifc()
+                    .cptra_mbox_pauser_lock()
+                    .at(idx)
+                    .write(|w| w.lock(true));
+            }
+        }
 
         writeln!(self.output().logger(), "writing to cptra_bootfsm_go")?;
         self.soc_ifc().cptra_bootfsm_go().write(|w| w.go(true));
