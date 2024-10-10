97821fca933a0247083335b7e426f9b4ba78f998
Vishal Mhatre <vishal.mhatre@gmail.com>
2024-10-09 22:39:37 -0700

    Disabling CFI for ROM

--- rom/dev/src/flow/cold_reset/fmc_alias.rs
+++ rom/dev/src/flow/cold_reset/fmc_alias.rs
@@ -21,6 +21,7 @@ use crate::cprintln;
 use crate::flow::cold_reset::{copy_tbs, TbsType};
 use crate::print::HexBytes;
 use crate::rom_env::RomEnv;
+#[cfg(not(feature = "no-cfi"))]
 use caliptra_cfi_derive::cfi_impl_fn;
 use caliptra_cfi_lib::{cfi_assert, cfi_assert_eq, cfi_launder};
 use caliptra_common::dice;
