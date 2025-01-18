c34bc0f70f61e166ec461bc19749bdc8629a41b2
Max Timkovich <max@timkovi.ch>
2025-01-17 16:40:20 -0800

    rebase

--- image/verify/src/verifier.rs
+++ image/verify/src/verifier.rs
@@ -202,7 +202,7 @@ impl<Env: ImageVerificationEnv> ImageVerifier<Env> {
         pqc_key_type: FwVerificationPqcKeyType,
     ) -> CaliptraResult<HeaderInfo<'a>> {
         // Verify Vendor Public Key Info Digest
-        self.verify_vendor_pub_key_info_digest(&preamble, pqc_key_type)?;
+        self.verify_vendor_pub_key_info_digest(preamble, pqc_key_type)?;
 
         // Verify Owner Public Key Info Digest
         let (owner_pub_keys_digest, owner_pub_keys_digest_in_fuses) =
