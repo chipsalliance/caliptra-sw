8a95ea2a96c24296824057913aa1dc599c4ac97b
Anthony Rocha <anthony.rocha@amd.com>
2025-02-03 12:56:47 -0800

    Bump up FMC_SIZE.

--- fmc/build.sh
+++ fmc/build.sh
@@ -10,3 +10,12 @@ cargo build \
   --profile=firmware \
   --no-default-features \
   --bin=caliptra-fmc
+
+cargo build \
+  --locked \
+  --target riscv32imc-unknown-none-elf \
+  --profile=firmware \
+  --no-default-features \
+  --features="emu hw-1.0" \
+  --bin=caliptra-fmc
+
