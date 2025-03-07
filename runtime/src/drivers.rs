a78056734472e845c0be27f9062afaec4f12e5b2
Christopher Swenson <chswenson@microsoft.com>
2025-03-07 09:21:23 -0800

    runtime: Add CM_STATUS, CM_IMPORT commands
    The first two cryptographic mailbox commands, which show the status of
    the system (for now, usage counters for AES keys) and the ability to
    import keys and return them as wrapped output.
    
    (For now, this is more of a no-op, since we don't have the AES driver
    yet to encrypt the CMKs.)
    
    We add some of the additional structures we will need to track AES keys
    (in the form of usage counters), and start adding in the necessary
    mailbox requests, responses, and errors.

--- runtime/src/drivers.rs
+++ runtime/src/drivers.rs
@@ -14,6 +14,7 @@ Abstract:
 
 #![cfg_attr(not(feature = "fips_self_test"), allow(unused))]
 
+use crate::cryptographic_mailbox::KeyUsage;
 #[cfg(feature = "fips_self_test")]
 pub use crate::fips::fips_self_test_cmd::SelfTestStatus;
 use crate::recovery_flow::RecoveryFlow;
@@ -108,6 +109,8 @@ pub struct Drivers {
     pub dmtf_device_info: Option<ArrayVec<u8, { AddSubjectAltNameReq::MAX_DEVICE_INFO_LEN }>>,
     pub exported_cdi_slots: ExportedCdiHandles,
     pub dma: Dma,
+
+    pub cryptographic_usage_data: KeyUsage,
 }
 
 impl Drivers {
@@ -148,6 +151,7 @@ impl Drivers {
             dmtf_device_info: None,
             exported_cdi_slots: [None; EXPORTED_HANDLES_NUM],
             dma: Dma::default(),
+            cryptographic_usage_data: KeyUsage::default(),
         })
     }
 
