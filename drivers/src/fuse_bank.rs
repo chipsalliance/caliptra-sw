e0e715762b8345f8e5c5b05012d943b42fb6a59a
Vishal Mhatre <vishal.mhatre@gmail.com>
2025-03-20 13:03:29 -0700

    [update] RTL update to 2bd44094bfd68740f2e5e6d1a5b6e9c22cd3cc3a

--- drivers/src/fuse_bank.rs
+++ drivers/src/fuse_bank.rs
@@ -12,7 +12,7 @@ Abstract:
 
 --*/
 
-use crate::{Array4x12, Array4x4};
+use crate::{Array4x12, Array4x16};
 #[cfg(not(feature = "no-cfi"))]
 use caliptra_cfi_derive::Launder;
 use caliptra_registers::soc_ifc::SocIfcReg;
@@ -301,11 +301,11 @@ impl FuseBank<'_> {
     /// * None
     ///
     /// # Returns
-    ///     manufactoring debug unlock token
+    ///     manufacturing debug unlock token
     ///
-    pub fn manuf_dbg_unlock_token(&self) -> Array4x4 {
+    pub fn manuf_dbg_unlock_token(&self) -> Array4x16 {
         let soc_ifc_regs = self.soc_ifc.regs();
-        Array4x4::read_from_reg(soc_ifc_regs.fuse_manuf_dbg_unlock_token())
+        Array4x16::read_from_reg(soc_ifc_regs.fuse_manuf_dbg_unlock_token())
     }
 }
 
