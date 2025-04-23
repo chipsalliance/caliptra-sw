1fa6dec37a8a512c31a194f5321554d81d8f17ab
Carl Lundin <clundin@google.com>
2025-04-23 09:15:01 -0700

    try again

--- ci-tools/fpga-image/build.sh
+++ ci-tools/fpga-image/build.sh
@@ -62,13 +62,10 @@ if [[ -z "${SKIP_DEBOOTSTRAP}" ]]; then
   (rm -rf out/xilinx-rootfs || true)
   mkdir -p out/xilinx-rootfs
   tar xvzf out/rootfs.tar.gz -C out/xilinx-rootfs
-  mkdir -p out/rootfs/lib/modules
-  scp -r out/xilinx-rootfs/lib/modules out/rootfs/lib/modules
-  mkdir -p out/rootfs/lib/firmware
-  scp -r out/xilinx-rootfs/lib/firmware out/rootfs/lib/firmware
 
   mkdir -p out/rootfs/boot
   scp -r out/xilinx-rootfs/boot out/rootfs/boot
+  rm -r out/rootfs/boot/firmware
 
   # Comment this line out if you don't trust folks with physical access to the
   # uart
