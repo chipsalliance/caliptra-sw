5bca2ad9c8cf5e1d687608e3337e15e982b7ac29
Max Timkovich <max@timkovi.ch>
2025-04-23 13:01:42 -0700

    add todos

--- api/src/mailbox.rs
+++ api/src/mailbox.rs
@@ -583,6 +583,7 @@ pub struct GetIdevMldsa87CertReq {
     pub tbs: [u8; GetIdevMldsa87CertReq::DATA_MAX_SIZE], // variable length
 }
 impl GetIdevMldsa87CertReq {
+    // TODO: This number is incorrect.
     pub const DATA_MAX_SIZE: usize = 916; // Req max size = Resp max size - MAX_MLDSA87_SIG_LEN
 
     pub fn as_bytes_partial(&self) -> CaliptraResult<&[u8]> {
