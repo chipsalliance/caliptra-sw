819d85f855cdc3e4ccd2104879d8c29316b32611
Arthur Heymans <arthur.heymans@9elements.com>
2025-04-30 15:48:13 +0200

    fmc: Use streaming mldsa methods
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- fmc/src/flow/crypto.rs
+++ fmc/src/flow/crypto.rs
@@ -15,7 +15,7 @@ use caliptra_common::{
 use caliptra_drivers::{
     okmutref, okref, Array4x12, CaliptraResult, Ecc384PrivKeyIn, Ecc384PrivKeyOut, Ecc384PubKey,
     Ecc384Result, Ecc384Signature, HmacMode, KeyId, KeyReadArgs, KeyUsage, KeyWriteArgs,
-    LEArray4x16, Mldsa87PubKey, Mldsa87Result, Mldsa87Seed, Mldsa87SignRnd, Mldsa87Signature,
+    Mldsa87PubKey, Mldsa87Result, Mldsa87Seed, Mldsa87SignRnd, Mldsa87Signature,
 };
 use caliptra_x509::Ecdsa384Signature;
 use zeroize::Zeroize;
@@ -222,8 +222,6 @@ impl Crypto {
 
     /// Sign data using MLDSA Private Key
     ///
-    /// This routine calculates the digest of the `data` and signs the hash
-    ///
     /// # Arguments
     ///
     /// * `env` - ROM Environment
@@ -239,21 +237,16 @@ impl Crypto {
         pub_key: &Mldsa87PubKey,
         data: &[u8],
     ) -> CaliptraResult<Mldsa87Signature> {
-        let mut digest = env.sha2_512_384.sha512_digest(data);
-        let digest = okmutref(&mut digest)?;
-        let digest: LEArray4x16 = (*digest).into();
-        env.mldsa.sign(
+        env.mldsa.sign_var(
             &Mldsa87Seed::Key(KeyReadArgs::new(key_pair_seed)),
             pub_key,
-            &digest,
+            data,
             &Mldsa87SignRnd::default(),
             &mut env.trng,
         )
     }
 
     /// Verify the MLDSA Signature
-    ///
-    /// This routine calculates the digest and verifies the signature
     ///
     /// # Arguments
     ///
@@ -272,9 +265,6 @@ impl Crypto {
         data: &[u8],
         sig: &Mldsa87Signature,
     ) -> CaliptraResult<Mldsa87Result> {
-        let digest = env.sha2_512_384.sha512_digest(data);
-        let digest = okref(&digest)?;
-        let digest: LEArray4x16 = (*digest).into();
-        env.mldsa.verify(pub_key, &digest, sig)
+        env.mldsa.verify_var(pub_key, data, sig)
     }
 }
