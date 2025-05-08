08227d0c45ffd75b210c6eebe4d7ceb321b35b3b
leongross <leon.gross@9elements.com>
2025-05-08 11:48:54 +0200

    ci/fpga-boss: add SDMux implementation for usbsdmux
    Signed-off-by: leongross <leon.gross@9elements.com>

--- ci-tools/fpga-boss/src/main.rs
+++ ci-tools/fpga-boss/src/main.rs
@@ -9,7 +9,6 @@ mod tty;
 mod usb_port_path;
 
 use anyhow::{anyhow, Context};
-use find_usb_block_device::find_usb_block_device;
 
 use clap::{arg, value_parser};
 use libftdi1_sys::ftdi_interface;
@@ -24,15 +23,18 @@ use std::{
 
 pub(crate) use fpga_jtag::{FpgaJtag, FpgaReset};
 pub(crate) use ftdi::FtdiCtx;
-pub(crate) use sd_mux::{SdMux, SdMuxTarget};
+use sd_mux::{SDWire, SdMux, SdMuxModel, SdMuxTarget, UsbsdMux};
 pub(crate) use usb_port_path::UsbPortPath;
 
 fn cli() -> clap::Command<'static> {
     clap::Command::new("fpga-boss")
         .about("FPGA boss tool")
         .arg(
-            arg!(--"sdwire" [PORT_PATH] "USB port path to the hub chip on the SDWire (ex: 3-1.2)")
-                .value_parser(value_parser!(UsbPortPath))
+            arg!(--"sdmux_model" [SDMUXMODEL] "Type of SD mux to use (sdwire, usbsdmux)")
+                .value_parser(value_parser!(SdMuxModel))
+        )
+        .arg(arg!(--"sdmux_id" [PORT_PATH] "ID of the SD mux to use (sdwire: e.g. 3-1.2, usbsdmux: e.g. 00048.00643)")
+            .value_parser(value_parser!(String))
         )
         .arg(
             arg!(--"zcu104" [PORT_PATH] "USB port path to the FTDI chip on the ZCU104 dev board (ex: 3-1.2)")
@@ -93,7 +95,7 @@ fn open_block_dev(path: &Path) -> std::io::Result<File> {
     }
 }
 
-/// Returns true if the device alread contains the image.
+/// Returns true if the device already contains the image.
 fn verify_image(dev: &mut File, image: &mut File) -> std::io::Result<bool> {
     dev.seek(SeekFrom::Start(0))?;
     let file_len = image.metadata()?.len();
@@ -162,7 +164,10 @@ fn active_runner_error_checks(input: &str) -> std::io::Result<()> {
 /// to recover the FPGA.
 fn check_for_github_runner_exception(input: &str) -> std::io::Result<()> {
     if input.contains("Unhandled exception") {
-        Err(Error::new(ErrorKind::BrokenPipe, "Github runner had an unhandled exception"))?;
+        Err(Error::new(
+            ErrorKind::BrokenPipe,
+            "Github runner had an unhandled exception",
+        ))?;
     }
     Ok(())
 }
@@ -232,7 +237,11 @@ fn log_uart_until_helper<R: BufRead>(
 
 fn main_impl() -> anyhow::Result<()> {
     let matches = cli().get_matches();
-    let sdwire_hub_path = matches.get_one::<UsbPortPath>("sdwire");
+    let sdmux_model = matches.get_one::<SdMuxModel>("sdmux_model");
+    let sdmux_id = matches
+        .get_one::<String>("sdmux_id")
+        .ok_or(anyhow!("--sdmux_id flag required"))?;
+
     let zcu104_path = matches.get_one::<UsbPortPath>("zcu104");
     let boss_ftdi_path = matches.get_one::<UsbPortPath>("boss_ftdi");
 
@@ -251,28 +260,17 @@ fn main_impl() -> anyhow::Result<()> {
             .ok_or(anyhow!("--zcu104 flag required"))
             .cloned()
     };
-    let get_sd_mux = || {
-        SdMux::open(
-            sdwire_hub_path
-                .ok_or(anyhow!("--sdwire flag required"))?
-                .child(2),
-        )
-    };
-    let get_fpga_ftdi = || FpgaJtag::open(get_zcu104_path()?);
-    let get_sd_dev_path = || {
-        let sdwire_hub_path = sdwire_hub_path.ok_or(anyhow!("--sdwire flag required"))?;
-        find_usb_block_device(&sdwire_hub_path.child(1)).with_context(|| {
-            format!(
-                "Could not find block device associated with {}",
-                sdwire_hub_path.child(1)
-            )
-        })
+
+    let mut sd_mux: Box<dyn SdMux> = match sdmux_model {
+        Some(SdMuxModel::SDWire) => Box::new(SDWire::open(sdmux_id.clone())?),
+        Some(SdMuxModel::UsbSDMux) => Box::new(UsbsdMux::open(sdmux_id.clone())?),
+        None => return Err(anyhow!("--sdmux_model flag required")),
     };
 
+    let get_fpga_ftdi = || FpgaJtag::open(get_zcu104_path()?);
     match matches.subcommand() {
         Some(("mode", sub_matches)) => {
             let mut fpga = get_fpga_ftdi();
-            let mut sd_mux = get_sd_mux()?;
             match sub_matches.get_one::<SdMuxTarget>("MODE").unwrap() {
                 SdMuxTarget::Dut => {
                     if let Ok(fpga) = &mut fpga {
@@ -335,8 +333,7 @@ fn main_impl() -> anyhow::Result<()> {
         }
         Some(("flash", sub_matches)) => {
             let mut fpga = get_fpga_ftdi();
-            let mut sd_mux = get_sd_mux()?;
-            let sd_dev_path = get_sd_dev_path()?;
+            let sd_dev_path = sd_mux.get_sd_dev_path()?;
             if let Ok(fpga) = &mut fpga {
                 fpga.set_reset(FpgaReset::Reset)?;
             }
@@ -362,14 +359,14 @@ fn main_impl() -> anyhow::Result<()> {
                 fpga.set_reset(FpgaReset::Run)?
             }
         }
+
         Some(("serve", sub_matches)) => {
             // Reuse the previous token if we never connect to GitHub.
             // This avoids creating a bunch of offline runners if the FPGA fails to establish a
             // connection.
             let mut cached_token: Option<Vec<u8>> = None;
             let mut fpga = get_fpga_ftdi()?;
-            let mut sd_mux = get_sd_mux()?;
-            let sd_dev_path = get_sd_dev_path()?;
+            let sd_dev_path = sd_mux.get_sd_dev_path()?;
             'outer: loop {
                 println!("Putting FPGA into reset");
                 fpga.set_reset(FpgaReset::Reset)?;
--- ci-tools/fpga-boss/src/sd_mux.rs
+++ ci-tools/fpga-boss/src/sd_mux.rs
@@ -1,17 +1,21 @@
 // Licensed under the Apache-2.0 license
 
+use anyhow::Context;
+use std::fs;
+use std::path::PathBuf;
 use std::str::FromStr;
 
 use clap::PossibleValue;
 use libftdi1_sys::ftdi_interface;
 
-use crate::{ftdi, FtdiCtx, UsbPortPath};
+use crate::{find_usb_block_device::find_usb_block_device, ftdi, FtdiCtx, UsbPortPath};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum SdMuxTarget {
     Host,
     Dut,
 }
+
 impl clap::ValueEnum for SdMuxTarget {
     fn value_variants<'a>() -> &'a [Self] {
         &[Self::Host, Self::Dut]
@@ -38,24 +42,239 @@ impl FromStr for SdMuxTarget {
     }
 }
 
-pub struct SdMux {
-    ftdi: FtdiCtx,
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SdMuxModel {
+    SDWire,
+    UsbSDMux,
 }
-impl SdMux {
-    pub fn open(port_path: UsbPortPath) -> anyhow::Result<Self> {
+
+impl FromStr for SdMuxModel {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "sdwire" => Ok(Self::SDWire),
+            "usbsdmux" => Ok(Self::UsbSDMux),
+            _ => Err(()),
+        }
+    }
+}
+
+impl clap::ValueEnum for SdMuxModel {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::SDWire, Self::UsbSDMux]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<clap::PossibleValue<'a>> {
+        match self {
+            Self::SDWire => Some(PossibleValue::new("sdwire").help("SDWire mux")),
+            Self::UsbSDMux => Some(PossibleValue::new("usbsdmux").help("USBSDMUX mux")),
+        }
+    }
+}
+
+// SdMux is a trait for SD muxes. It provides a way to set the target (DUT or host) and open the mux.
+pub trait SdMux {
+    fn set_target(&mut self, target: SdMuxTarget) -> anyhow::Result<()>;
+
+    /// Opens the mux and returns a new instance of the mux.
+    /// For SDWire, this is the FTDI device. It will be converted into UsbPortPath.
+    /// For UsbSDMux, this is UID for the USBSDMUX device.
+    fn open(port_path: String) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+
+    /// Returns the path to the block device associated with the SD mux.
+    fn get_sd_dev_path(&mut self) -> anyhow::Result<PathBuf>;
+}
+
+// Implementation specific to SDWire (idVendor=04e8, idProduct=6001)
+pub struct SDWire {
+    // The FTDI device used to communicate with the SDWire mux.
+    ftdi: FtdiCtx,
+
+    // The USB port path of the SDWire mux.
+    port_path: UsbPortPath,
+}
+
+impl SdMux for SDWire {
+    fn open(port_path: String) -> anyhow::Result<Self> {
+        let port_path: UsbPortPath = UsbPortPath::from_str(&port_path)
+            .map_err(|_| anyhow::anyhow!("Failed to parse port path"))?;
+
         let mut result = Self {
-            ftdi: FtdiCtx::open(port_path, ftdi_interface::INTERFACE_A)?,
+            ftdi: FtdiCtx::open(port_path.clone(), ftdi_interface::INTERFACE_A)?,
+            port_path,
         };
+
         result.ftdi.set_bitmode(0xc0, ftdi::BitMode::BitBang)?;
         Ok(result)
     }
 
-    pub fn set_target(&mut self, target: SdMuxTarget) -> anyhow::Result<()> {
+    fn set_target(&mut self, target: SdMuxTarget) -> anyhow::Result<()> {
         let pin_state = match target {
             SdMuxTarget::Dut => 0xf0,
             SdMuxTarget::Host => 0xf1,
         };
         self.ftdi.set_bitmode(pin_state, ftdi::BitMode::CBus)?;
         Ok(())
+    }
+
+    fn get_sd_dev_path(&mut self) -> anyhow::Result<PathBuf> {
+        let sdwire_hub_path = self.port_path.child(0);
+        find_usb_block_device(&sdwire_hub_path.child(1)).with_context(|| {
+            format!(
+                "Could not find block device associated with {}",
+                sdwire_hub_path.child(1)
+            )
+        })
+    }
+}
+
+// The UsbSDMux does expose the following things:
+// 1. A python cli interface to set the target.
+// 1.1 change target
+// 1.2 Shut the mux off
+// 1.3 manipulate GPIO pins on the MUX
+// 2. A SCSI generic device interface to set the target.
+// 2.1 A block device that can be used to access the SD card.
+// NO FTDI interface is exposed.
+// The SCSI generic device is usually /dev/sg0 but can be different (check sg_map -i).
+// https://github.com/linux-automation/usbsdmux/
+pub struct UsbsdMux {
+    // SCSI generic device name, e.g. sg0.
+    scsi_generic_name: String,
+}
+
+impl SdMux for UsbsdMux {
+    /// USBSDMUX exposes all connected adapters as linux devices under dev/usb-sd-mux/id-xxxxx.xxxxx.
+    /// The ID used to construct an UsbdsdMux instance is the symlink name without the leading "id-".
+    /// Resolving these symlinks by ID resolves them to the according /dev/sgX device.
+    fn open(sdmux_id: String) -> anyhow::Result<Self> {
+        for entry in fs::read_dir("/dev/usb-sd-mux/")? {
+            let entry = entry?;
+            let entry_id = format!("id-{}", sdmux_id);
+            if entry.file_name() == std::ffi::OsStr::new(&entry_id) {
+                // this will be of format ../sgx
+                let had_dev = entry
+                    .path()
+                    .read_link()
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string();
+
+                return Ok(Self {
+                    scsi_generic_name: had_dev.strip_prefix("../").unwrap().to_string(),
+                });
+            }
+        }
+        Err(anyhow::anyhow!(
+            "Failed to find SCSI generic device for USBSDMUX with ID {}",
+            sdmux_id
+        ))
+    }
+
+    // For now we use the python cli tool implementation provided by the vendor.
+    // Further, we assume there is only one usbsdmux device connected, exposed
+    // via /dev/sg0.
+    fn set_target(&mut self, target: SdMuxTarget) -> anyhow::Result<()> {
+        // UsbSDMux has three targets: DUT, HOST and CLIENT.
+        // TODO: handle client
+
+        let target = match target {
+            SdMuxTarget::Dut => "dut",
+            SdMuxTarget::Host => "host",
+        };
+
+        if std::process::Command::new("usbsdmux")
+            .arg("--help")
+            .output()
+            .is_err()
+        {
+            return Err(anyhow::anyhow!("usbsdmux tool not found"));
+        }
+
+        let out = std::process::Command::new("usbsdmux")
+            .arg(format!("/dev/{}", self.scsi_generic_name.clone()))
+            .arg(target)
+            .output()
+            .map_err(|e| anyhow::anyhow!("usbsdmux: {}", e))?;
+
+        if !out.status.success() {
+            return Err(anyhow::anyhow!(
+                "Failed to set target: {}",
+                String::from_utf8_lossy(&out.stderr)
+            ));
+        }
+        if !out.stdout.is_empty() {
+            println!("usbsdmux output: {}", String::from_utf8_lossy(&out.stdout));
+        }
+        Ok(())
+    }
+
+    // Resolve the SCSI generic device name to the block device name.
+    fn get_sd_dev_path(&mut self) -> anyhow::Result<PathBuf> {
+        let scsi_block_dev = format!(
+            "/sys/class/scsi_generic/{}/device/block/",
+            self.scsi_generic_name
+        );
+        if let Some(entry) = fs::read_dir(&scsi_block_dev)?.next() {
+            let entry = entry?;
+            let block_dev_name = entry.file_name();
+            let block_dev_path = format!("/dev/{}", block_dev_name.to_string_lossy());
+            Ok(PathBuf::from(block_dev_path))
+        } else {
+            Err(anyhow::anyhow!(
+                "Failed to find block device for SCSI generic device {}. Ensure the device is connected and the path {} exists.",
+                self.scsi_generic_name,
+                &scsi_block_dev
+            ))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sd_mux_target() {
+        assert_eq!(SdMuxTarget::from_str("host").unwrap(), SdMuxTarget::Host);
+        assert_eq!(SdMuxTarget::from_str("dut").unwrap(), SdMuxTarget::Dut);
+        assert!(SdMuxTarget::from_str("invalid").is_err());
+    }
+
+    #[test]
+    fn test_sd_mux_model() {
+        assert_eq!(SdMuxModel::from_str("sdwire").unwrap(), SdMuxModel::SDWire);
+        assert_eq!(
+            SdMuxModel::from_str("usbsdmux").unwrap(),
+            SdMuxModel::UsbSDMux
+        );
+        assert!(SdMuxModel::from_str("invalid").is_err());
+    }
+
+    #[test]
+    #[cfg(feature = "sdmux_usbsdmux")]
+    fn test_usbsdmux() {
+        /// This test assumes that the usbsdmux is connected and the SCSI generic device is /dev/sg0.
+        /// It also assumes that the block device is /dev/sda and that the USBSDMUX ID is 00048.00643.
+        const USBSDMUX_ID: &str = "00048.00643";
+        const USBSDMUX_SCSI_GENERIC_NAME: &str = "sg0";
+
+        let usbsdmux = UsbsdMux::open(USBSDMUX_ID.to_string());
+        assert!(usbsdmux.is_ok());
+
+        let mut usbsdmux = usbsdmux.unwrap();
+        assert_eq!(usbsdmux.scsi_generic_name, USBSDMUX_SCSI_GENERIC_NAME);
+
+        assert_eq!(
+            usbsdmux
+                .get_sd_dev_path()
+                .unwrap()
+                .to_string_lossy()
+                .to_string(),
+            "/dev/sda".to_string()
+        );
     }
 }
