864b0055a83a8c29f1305b9a56924c661661c1ac
Christopher Swenson <chswenson@microsoft.com>
2025-06-13 17:07:36 -0700

    Add CMAC KDF counter mode (#2244)
    And KAT.
    
    This will be used by the Device Ownership Transfer flows in another PR
    soon.

---
+++ kat/src/cmackdf_kat.rs
@@ -0,0 +1,69 @@
+/*++
+
+Licensed under the Apache-2.0 license.
+
+File Name:
+
+    hmac_kdf_kat.rs
+
+Abstract:
+
+    File contains the Known Answer Tests (KAT) for Counter Mode KDF using CMAC.
+
+--*/
+
+// From ACVP test vector:
+// "tgId": 263,
+// "keyOutLength": 1024,
+// "kdfMode": "counter",
+// "macMode": "CMAC-AES256",
+// "counterLength": 32,
+// "counterLocation": "before fixed data",
+// "testType": "AFT",
+// "tests": [
+// {
+//     "tcId": 525,
+//     "keyIn": "BFB99C6AAB859A9873ACC9880BD875BB83A8B24A9307576A054216908756BE5B"
+// },
+// "fixedData": "CD9B9791F5EEE211918BA1E2B01B4E29",
+// "keyOut": "C303887FB0ACA8E78DEBB8A008E75C88C26E927F0FA8A1DF1614C97E1B6F78B35C8F8A1CB9CD9F18DC30D06C73B75FDEA5A636ACB92F690FC6CB060F0A3DB66E759E30097C297E56C59DB8E17FF2656A8520D7309307B8E161B091FDDAF375B34E2EB8084D2832621C37BB67F09AAB29F3E467F422270B237D9B5AEBAD2D1F05"
+
+use caliptra_drivers::{cmac_kdf, Aes, CaliptraError, CaliptraResult};
+
+const KEY_IN: [u8; 32] = [
+    0xbf, 0xb9, 0x9c, 0x6a, 0xab, 0x85, 0x9a, 0x98, 0x73, 0xac, 0xc9, 0x88, 0x0b, 0xd8, 0x75, 0xbb,
+    0x83, 0xa8, 0xb2, 0x4a, 0x93, 0x07, 0x57, 0x6a, 0x05, 0x42, 0x16, 0x90, 0x87, 0x56, 0xbe, 0x5b,
+];
+
+const FIXED_DATA: [u8; 16] = [
+    0xcd, 0x9b, 0x97, 0x91, 0xf5, 0xee, 0xe2, 0x11, 0x91, 0x8b, 0xa1, 0xe2, 0xb0, 0x1b, 0x4e, 0x29,
+];
+
+const KEY_OUT: [u8; 64] = [
+    0xc3, 0x03, 0x88, 0x7f, 0xb0, 0xac, 0xa8, 0xe7, 0x8d, 0xeb, 0xb8, 0xa0, 0x08, 0xe7, 0x5c, 0x88,
+    0xc2, 0x6e, 0x92, 0x7f, 0x0f, 0xa8, 0xa1, 0xdf, 0x16, 0x14, 0xc9, 0x7e, 0x1b, 0x6f, 0x78, 0xb3,
+    0x5c, 0x8f, 0x8a, 0x1c, 0xb9, 0xcd, 0x9f, 0x18, 0xdc, 0x30, 0xd0, 0x6c, 0x73, 0xb7, 0x5f, 0xde,
+    0xa5, 0xa6, 0x36, 0xac, 0xb9, 0x2f, 0x69, 0x0f, 0xc6, 0xcb, 0x06, 0x0f, 0x0a, 0x3d, 0xb6, 0x6e,
+];
+
+#[derive(Default, Debug)]
+pub struct CmacKdfKat {}
+
+impl CmacKdfKat {
+    /// This function executes the Known Answer Tests (aka KAT) for CMAC KDF.
+    pub fn execute(&self, aes: &mut Aes) -> CaliptraResult<()> {
+        let output = cmac_kdf(
+            aes,
+            caliptra_drivers::AesKey::Array(&KEY_IN),
+            &FIXED_DATA,
+            None,
+            4,
+        )?;
+
+        if KEY_OUT != output {
+            Err(CaliptraError::KAT_CMAC_KDF_OUTPUT_MISMATCH)?;
+        }
+
+        Ok(())
+    }
+}
