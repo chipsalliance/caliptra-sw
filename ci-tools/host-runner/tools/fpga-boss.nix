d7a28839bb4068a066b95f341f9935e9860f93ef
leongross <leon.gross@9elements.com>
2025-09-09 14:39:04 +0200

    ci: update fpga-boss.nix to support usbsdmux
    Signed-off-by: leongross <leon.gross@9elements.com>

--- ci-tools/host-runner/tools/fpga-boss.nix
+++ ci-tools/host-runner/tools/fpga-boss.nix
@@ -11,8 +11,8 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "chipsalliance";
     repo = "caliptra-sw";
-    rev = "10e4ed31a10b2c6d60642d7894412e2fc8b2b9fb";
-    sha256 = "sha256-LIU6GDESwoYTL7KrGYZabopC8QRR8x70qQaU7XAVc5o=";
+    rev = "b5f9a6bed0e0291a0179df8af250a51a90f29c5b";
+    sha256 = "sha256-nzGj4kwvAJy9eULBzp1SUH0+A/MNEXi93BB5MHRO4Pc=";
   };
   sourceRoot = "${src.name}/ci-tools/fpga-boss";
   cargoHash = "sha256-cx+uHWFzeZHnX92G3UzfpAiT0SSWkEPP+5pxmZ/nbfY=";
