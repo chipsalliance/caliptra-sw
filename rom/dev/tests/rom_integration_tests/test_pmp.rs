0f6d893006c683ce2567a96eb8ac3887275e84f8
Vishal Mhatre <38512878+mhatrevi@users.noreply.github.com>
2025-09-09 07:57:57 -0700

    Fixing PMP tests for FPGA (#2468)

--- rom/dev/tests/rom_integration_tests/test_pmp.rs
+++ rom/dev/tests/rom_integration_tests/test_pmp.rs
@@ -1,6 +1,5 @@
 // Licensed under the Apache-2.0 license
 
-use crate::helpers;
 use caliptra_api::SocManager;
 use caliptra_builder::{
     firmware::{self, rom_tests::TEST_FMC_INTERACTIVE, APP_WITH_UART},
@@ -8,6 +7,7 @@ use caliptra_builder::{
 };
 use caliptra_common::RomBootStatus::*;
 use caliptra_hw_model::{BootParams, Fuses, HwModel, InitParams};
+use caliptra_image_types::FwVerificationPqcKeyType;
 // From RISC-V_VeeR_EL2_PRM.pdf
 // Exception causes
 pub const EXCEPTION_CAUSE_STORE_ACCESS_FAULT: u32 = 0x0000_0007;
@@ -27,68 +27,87 @@ fn test_pmp_enforced() {
 }
 
 #[test]
-fn test_datavault_pmp_enforcement() {
-    for subsystem_mode in [false, true] {
-        for pqc_key_type in helpers::PQC_KEY_TYPE.iter() {
-            let image_options = ImageOptions {
-                pqc_key_type: *pqc_key_type,
-                ..Default::default()
-            };
-            let fuses = Fuses {
-                fuse_pqc_key_type: *pqc_key_type as u32,
-                ..Default::default()
-            };
-            let rom = caliptra_builder::build_firmware_rom(firmware::rom_from_env()).unwrap();
-            let image_bundle = caliptra_builder::build_and_sign_image(
-                &TEST_FMC_INTERACTIVE,
-                &APP_WITH_UART,
-                image_options,
-            )
-            .unwrap();
+fn test_datavault_pmp_enforcement_region_start() {
+    let image_options = ImageOptions {
+        pqc_key_type: FwVerificationPqcKeyType::MLDSA,
+        ..Default::default()
+    };
+    let fuses = Fuses {
+        fuse_pqc_key_type: 1,
+        ..Default::default()
+    };
+    let rom = caliptra_builder::build_firmware_rom(firmware::rom_from_env()).unwrap();
+    let image_bundle = caliptra_builder::build_and_sign_image(
+        &TEST_FMC_INTERACTIVE,
+        &APP_WITH_UART,
+        image_options,
+    )
+    .unwrap();
 
-            let mut hw = caliptra_hw_model::new(
-                InitParams {
-                    rom: &rom,
-                    subsystem_mode: false,
-                    ..Default::default()
-                },
-                BootParams {
-                    fw_image: Some(&image_bundle.to_bytes().unwrap()),
-                    fuses: fuses.clone(),
-                    ..Default::default()
-                },
-            )
-            .unwrap();
+    let mut hw = caliptra_hw_model::new(
+        InitParams {
+            rom: &rom,
+            subsystem_mode: false,
+            ..Default::default()
+        },
+        BootParams {
+            fw_image: Some(&image_bundle.to_bytes().unwrap()),
+            fuses: fuses.clone(),
+            ..Default::default()
+        },
+    )
+    .unwrap();
 
-            hw.step_until_boot_status(u32::from(ColdResetComplete), true);
+    hw.step_until_boot_status(u32::from(ColdResetComplete), true);
 
-            // Test PMP enforcement by writing to start of Data Vault.
-            let result = hw.mailbox_execute(0x1000_0010, &[]);
-            assert!(result.is_err());
-            let mcause = hw.soc_ifc().cptra_fw_extended_error_info().at(0).read();
-            assert_eq!(mcause, EXCEPTION_CAUSE_STORE_ACCESS_FAULT);
+    // Test PMP enforcement by writing to start of Data Vault.
+    let result = hw.mailbox_execute(0x1000_0010, &[]);
+    assert!(result.is_ok());
 
-            // Test PMP enforcement by writing to end of Data Vault.
-            let mut hw = caliptra_hw_model::new(
-                InitParams {
-                    rom: &rom,
-                    subsystem_mode,
-                    ..Default::default()
-                },
-                BootParams {
-                    fw_image: Some(&image_bundle.to_bytes().unwrap()),
-                    fuses,
-                    ..Default::default()
-                },
-            )
-            .unwrap();
+    hw.step_until(|m| m.soc_ifc().cptra_fw_extended_error_info().at(0).read() != 0);
+    let mcause = hw.soc_ifc().cptra_fw_extended_error_info().at(0).read();
+    assert_eq!(mcause, EXCEPTION_CAUSE_STORE_ACCESS_FAULT);
+}
 
-            hw.step_until_boot_status(u32::from(ColdResetComplete), true);
+#[test]
+fn test_datavault_pmp_enforcement_region_end() {
+    let image_options = ImageOptions {
+        pqc_key_type: FwVerificationPqcKeyType::MLDSA,
+        ..Default::default()
+    };
+    let fuses = Fuses {
+        fuse_pqc_key_type: 1,
+        ..Default::default()
+    };
+    let rom = caliptra_builder::build_firmware_rom(firmware::rom_from_env()).unwrap();
+    let image_bundle = caliptra_builder::build_and_sign_image(
+        &TEST_FMC_INTERACTIVE,
+        &APP_WITH_UART,
+        image_options,
+    )
+    .unwrap();
 
-            let result = hw.mailbox_execute(0x1000_0011, &[]);
-            assert!(result.is_err());
-            let mcause = hw.soc_ifc().cptra_fw_extended_error_info().at(0).read();
-            assert_eq!(mcause, EXCEPTION_CAUSE_STORE_ACCESS_FAULT);
-        }
-    }
+    let mut hw = caliptra_hw_model::new(
+        InitParams {
+            rom: &rom,
+            subsystem_mode: false,
+            ..Default::default()
+        },
+        BootParams {
+            fw_image: Some(&image_bundle.to_bytes().unwrap()),
+            fuses: fuses.clone(),
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    hw.step_until_boot_status(u32::from(ColdResetComplete), true);
+
+    // Test PMP enforcement by writing to end of Data Vault.
+    let result = hw.mailbox_execute(0x1000_0011, &[]);
+    assert!(result.is_ok());
+
+    hw.step_until(|m| m.soc_ifc().cptra_fw_extended_error_info().at(0).read() != 0);
+    let mcause = hw.soc_ifc().cptra_fw_extended_error_info().at(0).read();
+    assert_eq!(mcause, EXCEPTION_CAUSE_STORE_ACCESS_FAULT);
 }
