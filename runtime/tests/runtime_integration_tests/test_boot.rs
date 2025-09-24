59261306775baa573bee069117f02ee251b4d237
Vishal Mhatre <38512878+mhatrevi@users.noreply.github.com>
2025-09-23 17:33:59 -0700

    [Fix] Restore FMC & RT Firmware Versions on Warm Reset (#2528)
    During a warm reset, the cptra_fw_rev_id registers are cleared, causing RT commands such as VERSION to return a firmware version of 0. This update re-applies the FMC and RT firmware versions during RT initialization by reading them from the manifest stored in DCCM.

--- runtime/tests/runtime_integration_tests/test_boot.rs
+++ runtime/tests/runtime_integration_tests/test_boot.rs
@@ -71,6 +71,7 @@ fn test_fw_version() {
 #[test]
 fn test_update() {
     let image_options = ImageOptions {
+        fmc_version: DEFAULT_FMC_VERSION,
         app_version: 0xaabbccdd,
         ..Default::default()
     };
