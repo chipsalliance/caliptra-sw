1cf120bf9b06350f3748bd5343f67bb47cddd631
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2025-10-06 10:38:10 -0700

    Increase boot stress test update timeout for EMU nightly (#2586)

--- runtime/tests/runtime_integration_tests/test_boot.rs
+++ runtime/tests/runtime_integration_tests/test_boot.rs
@@ -134,7 +134,7 @@ fn test_stress_update() {
 
     let mut model = run_rt_test(RuntimeTestArgs::default());
 
-    let stress_num = if cfg!(feature = "slow_tests") { 500 } else { 1 };
+    let stress_num = if cfg!(feature = "slow_tests") { 250 } else { 1 };
     let mut image_select = 0;
 
     model.step_until(|m| m.soc_mbox().status().read().mbox_fsm_ps().mbox_idle());
