ea40363408a903b3c160f917cb69c84b1711c5b0
Arthur Heymans <arthur.heymans@9elements.com>
2025-10-08 11:57:37 +0200

    runtime: Wait for ready_for_runtime before sending mbox commands
    This is needed in subsystem mode as the mbox sram is utilised to stage
    the SOC manifest and MCU image from the recovery register interface.
    
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- runtime/test-fw/src/boot_tests.rs
+++ runtime/test-fw/src/boot_tests.rs
@@ -15,10 +15,19 @@ Abstract:
 #![no_std]
 #![no_main]
 
+use caliptra_common::handle_fatal_error;
 use caliptra_registers::soc_ifc::SocIfcReg;
+use caliptra_runtime::Drivers;
 use caliptra_test_harness::{runtime_handlers, test_suite};
 
-fn test_boot() {}
+fn test_boot() {
+    let mut drivers = unsafe {
+        Drivers::new_from_registers().unwrap_or_else(|e| {
+            handle_fatal_error(e.into());
+        })
+    };
+    drivers.soc_ifc.assert_ready_for_runtime();
+}
 
 test_suite! {
     test_boot,
