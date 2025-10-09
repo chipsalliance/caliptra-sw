b4c095a8a5e3a272d1868581a6318ea02fe2fe85
Marco Visaya <marcovisaya@microsoft.com>
2025-10-09 16:37:52 -0700

    s

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -295,13 +295,7 @@ fn handle_command(drivers: &mut Drivers) -> CaliptraResult<MboxStatusE> {
     let resp = &mut [0u8; MAX_RESP_SIZE][..];
 
     let len = match CommandId::from(cmd_id) {
-        CommandId::ACTIVATE_FIRMWARE => {
-            activate_firmware::ActivateFirmwareCmd::execute(drivers, cmd_bytes, resp)
-        }
-        CommandId::FIRMWARE_LOAD => Err(CaliptraError::RUNTIME_UNIMPLEMENTED_COMMAND),
         CommandId::AUTHORIZE_AND_STASH => AuthorizeAndStashCmd::execute(drivers, cmd_bytes, resp),
-
-        
         CommandId::FE_PROG => FeProgrammingCmd::execute(drivers, cmd_bytes),
         _ => Err(CaliptraError::RUNTIME_UNIMPLEMENTED_COMMAND),
     }?;
