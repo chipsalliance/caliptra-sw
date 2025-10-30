8c09686f361b6f1e1598d28246a441e9202df192
Mei Lu <mei.lu@businessintegra.com>
2025-10-30 08:40:40 -0700

    add tests for GET_IDEV_ECC384_CSR, GET_IDEV_MLDSA87_CSR, GET_FMC_ALIAS_ECC384_CSR, GET_FMC_ALIAS_MLDSA87_CSR

--- runtime/tests/runtime_integration_tests/main.rs
+++ runtime/tests/runtime_integration_tests/main.rs
@@ -27,6 +27,7 @@ mod test_get_fmc_alias_csr;
 mod test_get_idev_csr;
 mod test_get_image_info;
 mod test_get_img_info_warmreset;
+
 mod test_idev_csr_warmreset;
 mod test_info;
 mod test_invoke_dpe;
