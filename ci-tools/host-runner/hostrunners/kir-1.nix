e6e2a3fc387ffc392aca6c52ce301a186fc1b918
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2025-11-03 08:46:51 -0800

    Hostrunner tweaks (#2691)
    * Run autoformatter
    
    * Split SSH keys into a separate config file
    
    * Add instructions for code formatting

--- ci-tools/host-runner/hostrunners/kir-1.nix
+++ ci-tools/host-runner/hostrunners/kir-1.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, user, fpga-boss-script, ... }:
+{
+  config,
+  pkgs,
+  user,
+  fpga-boss-script,
+  ...
+}:
 {
   systemd.user.services.vck-7 = {
     enable = true;
@@ -80,19 +86,23 @@
     };
   };
   environment.systemPackages = with pkgs; [
-      ((pkgs.writeShellScriptBin "vck-4-debug" ''
+    (
+      (pkgs.writeShellScriptBin "vck-4-debug" ''
         #!${pkgs.bash}/bin/bash
         export ZCU_FTDI="1-1.2"
         export ZCU_SDWIRE="1-1.1"
 
         caliptra-fpga-boss --zcu104 $ZCU_FTDI --sdwire $ZCU_SDWIRE "$@"
-     ''))
-      ((pkgs.writeShellScriptBin "vck-0-debug" ''
+      '')
+    )
+    (
+      (pkgs.writeShellScriptBin "vck-0-debug" ''
         #!${pkgs.bash}/bin/bash
         export ZCU_FTDI="1-1.3.2"
         export ZCU_SDWIRE="1-1.3.1"
 
         caliptra-fpga-boss --zcu104 $ZCU_FTDI --sdwire $ZCU_SDWIRE "$@"
-     ''))
+      '')
+    )
   ];
 }
