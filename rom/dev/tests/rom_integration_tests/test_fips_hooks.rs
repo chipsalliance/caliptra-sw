b373569047c0462de129e988937e7cff7783a28b
Christopher Swenson <chswenson@microsoft.com>
2025-11-12 17:37:45 -0800

    test: Fix FIPS self-test hook ROM test to use FPGA feature (#2746)
    It was using the non-FPGA ROM, which won't boot firmware on the FPGA.
    
    Test passes locally for me on the subsystem FPGA after adding this fix.
    
    Co-authored-by: Vishal Mhatre <38512878+mhatrevi@users.noreply.github.com>
    (cherry picked from commit 93712a091889309cfabbacfaa7366b45e0c34e7e)

--- rom/dev/tests/rom_integration_tests/test_fips_hooks.rs
+++ rom/dev/tests/rom_integration_tests/test_fips_hooks.rs
@@ -2,7 +2,10 @@
 
 use crate::helpers;
 use caliptra_api::SocManager;
-use caliptra_builder::firmware::{APP_WITH_UART, FMC_WITH_UART, ROM_WITH_FIPS_TEST_HOOKS};
+use caliptra_builder::firmware::{
+    APP_WITH_UART, APP_WITH_UART_FPGA, FMC_WITH_UART, ROM_WITH_FIPS_TEST_HOOKS,
+    ROM_WITH_FIPS_TEST_HOOKS_FPGA,
+};
 use caliptra_builder::ImageOptions;
 use caliptra_drivers::CaliptraError;
 use caliptra_hw_model::{BootParams, Fuses, HwModel, InitParams};
@@ -10,22 +13,35 @@ use caliptra_hw_model::{BootParams, Fuses, HwModel, InitParams};
 #[test]
 fn test_fips_hook_exit() {
     for pqc_key_type in helpers::PQC_KEY_TYPE.iter() {
+        let fpga = cfg!(any(feature = "fpga_realtime", feature = "fpga_subsystem"));
         let image_options = ImageOptions {
             pqc_key_type: *pqc_key_type,
             ..Default::default()
         };
-        let rom = caliptra_builder::build_firmware_rom(&ROM_WITH_FIPS_TEST_HOOKS).unwrap();
+        let rom = caliptra_builder::build_firmware_rom(if fpga {
+            &ROM_WITH_FIPS_TEST_HOOKS_FPGA
+        } else {
+            &ROM_WITH_FIPS_TEST_HOOKS
+        })
+        .unwrap();
 
         let fuses = Fuses {
             fuse_pqc_key_type: *pqc_key_type as u32,
             ..Default::default()
         };
 
-        let image_bundle =
-            caliptra_builder::build_and_sign_image(&FMC_WITH_UART, &APP_WITH_UART, image_options)
-                .unwrap()
-                .to_bytes()
-                .unwrap();
+        let image_bundle = caliptra_builder::build_and_sign_image(
+            &FMC_WITH_UART,
+            if fpga {
+                &APP_WITH_UART
+            } else {
+                &APP_WITH_UART_FPGA
+            },
+            image_options,
+        )
+        .unwrap()
+        .to_bytes()
+        .unwrap();
 
         let init_params = InitParams {
             rom: &rom,
