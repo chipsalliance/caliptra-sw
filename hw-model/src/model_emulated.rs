92f96f778408668b95675862dad7569b1afd3c72
mlvisaya <38512415+mlvisaya@users.noreply.github.com>
2025-11-14 18:13:30 +0900

    Fix Hitless Update Activate Firmware Mailbox command sequence (#2606)
    * Fix Hitless Update Activate Firmware Mailbox command sequence
    
    During HW testing, when MCU sends Activate Firmware mailbox command to
    perform MCU hitless update, clearing FW_EXEC_CTRL[2] didn't trigger MCI
    notif_cptra_mcu_reset_req_sts interrupt to MCU. Caliptra needs to
    trigger the interrupt via notif0_intr_trig_r register.
    
    After the interrupt occurs, MCU will reset itself and clears the
    interrupt, this will be an indication to Caliptra to clear
    FW_EXEC_CTRL[2].
    
    Removed the unnecesary check RESET_STATUS.MCU_RESET_STS. This indicates
    that the MCU is on reset, but the transition happens so fast that the SW
    will not catch the transition. It is enough for Caliptra to wait on
    notif0_intr_trig_r getting cleared to know that MCU already reset.
    
    Verify MCU FW after copying it from staging to MCU SRAM
    
    Generate interrupt when intr_block_rf_notif0_intr_trig_r is set
    
    intr_block_rf_notif0_intr_trig_r is a register to trigger interrupts,
    therefore setting intr_block_rf_notif0_intr_trig_r should trigger an
    interrupt.
    
    When activating fw, Caliptra will request interrupt through MCI.
    Caliptra will wait for Caliptra for the MCU to clear the interrupt.
    In the absence, of an MCU, the integration test will clear it.
    
    Avoid waiting for MCU reset on failed test cases and SOC only
    activatation
    
    Avoid waiting for MCU reset on failed test cases. If the parameters
    passed are invalid, then it is expected that the MCU will not reset.
    
    * Save memory by removing some helper functions
    
    FPGA build is failing due to ICCM size reached.
    Removing some helper functions that are only used once helped with
    optimizing the runtime size.
    
    ---------
    
    Co-authored-by: Vishal Mhatre <38512878+mhatrevi@users.noreply.github.com>
    (cherry picked from commit 1bddea54d88b17f065394002bcdae246a0b23695)

--- hw-model/src/model_emulated.rs
+++ hw-model/src/model_emulated.rs
@@ -22,6 +22,7 @@ use caliptra_emu_periph::dma::axi_root_bus::AxiRootBus;
 use caliptra_emu_periph::dma::recovery::RecoveryControl;
 use caliptra_emu_periph::ActionCb;
 use caliptra_emu_periph::MailboxExternal;
+use caliptra_emu_periph::Mci;
 use caliptra_emu_periph::ReadyForFwCb;
 use caliptra_emu_periph::{
     CaliptraRootBus, CaliptraRootBusArgs, MailboxRequester, SocToCaliptraBus, TbServicesCb,
@@ -82,6 +83,8 @@ pub struct ModelEmulated {
     events_to_caliptra: mpsc::Sender<Event>,
     events_from_caliptra: mpsc::Receiver<Event>,
     collected_events_from_caliptra: Vec<Event>,
+
+    pub mci: Mci,
 }
 
 #[cfg(feature = "coverage")]
@@ -237,6 +240,7 @@ impl HwModel for ModelEmulated {
         let mut hasher = DefaultHasher::new();
         std::hash::Hash::hash_slice(params.rom, &mut hasher);
         let image_tag = hasher.finish();
+        let mci_regs = cpu.bus.bus.mci_external_regs();
 
         let mut m = ModelEmulated {
             output,
@@ -252,6 +256,7 @@ impl HwModel for ModelEmulated {
             events_to_caliptra,
             events_from_caliptra,
             collected_events_from_caliptra: vec![],
+            mci: mci_regs,
         };
         // Turn tracing on if the trace path was set
         m.tracing_hint(true);
