a257411d6dcaf4f3930527e2c258376f4bc4ce16
Max Timkovich <max@timkovi.ch>
2025-12-02 14:36:21 -0800

    Rename fmc alias csr to include ecc384

--- fmc/src/flow/mod.rs
+++ fmc/src/flow/mod.rs
@@ -13,7 +13,7 @@ Abstract:
 --*/
 
 pub mod dice;
-mod fmc_alias_csr;
+mod fmc_alias_csr_ecc_384;
 mod pcr;
 mod rt_alias;
 mod tci;
@@ -38,7 +38,7 @@ pub fn run(env: &mut FmcEnv) -> CaliptraResult<()> {
         if reset_reason == ResetReason::ColdReset {
             cfi_assert_eq(env.soc_ifc.reset_reason(), ResetReason::ColdReset);
             // Generate the FMC Alias Certificate Signing Request (CSR)
-            fmc_alias_csr::generate_csr(env)?;
+            fmc_alias_csr_ecc_384::generate_csr(env)?;
         }
     }
 
