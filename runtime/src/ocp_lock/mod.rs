1b97755d0bbb7a805c31e438cfce344af213e30e
Carl Lundin <clundin@google.com>
2025-12-04 12:21:15 -0800

    OCP LOCK: Add DERIVE_MEK

--- runtime/src/ocp_lock/mod.rs
+++ runtime/src/ocp_lock/mod.rs
@@ -3,58 +3,114 @@
 use caliptra_cfi_lib_git::{cfi_assert, cfi_assert_eq};
 use caliptra_common::keyids::ocp_lock::{KEY_ID_EPK, KEY_ID_HEK, KEY_ID_MEK_SECRETS};
 use caliptra_drivers::{
-    hmac_kdf, Hmac, HmacKey, HmacMode, HmacTag, KeyReadArgs, KeyUsage, KeyVault, KeyWriteArgs,
-    SocIfc, Trng,
+    cmac_kdf, hmac_kdf, Aes, AesKey, AesOperation, Hmac, HmacKey, HmacMode, HmacTag, KeyReadArgs,
+    KeyUsage, KeyVault, KeyWriteArgs, LEArray4x16, LEArray4x8, SocIfc, Trng,
 };
 use caliptra_error::{CaliptraError, CaliptraResult};
 
-use zerocopy::{Immutable, IntoBytes, KnownLayout};
+use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
 use zeroize::ZeroizeOnDrop;
 
+mod derive_mek;
 mod get_algorithms;
 mod initialize_mek_secret;
 
+pub use derive_mek::DeriveMekCmd;
 pub use get_algorithms::GetAlgorithmsCmd;
 pub use initialize_mek_secret::InitializeMekSecretCmd;
 
-#[derive(IntoBytes, KnownLayout, Immutable, ZeroizeOnDrop)]
 /// Represents the SEK type from OCP LOCK.
+#[derive(IntoBytes, KnownLayout, Immutable, ZeroizeOnDrop)]
 pub struct Sek(pub [u8; 32]);
 
-#[derive(IntoBytes, KnownLayout, Immutable, ZeroizeOnDrop)]
 /// Represents the DPK type from OCP LOCK.
+#[derive(IntoBytes, KnownLayout, Immutable, ZeroizeOnDrop)]
 pub struct Dpk(pub [u8; 32]);
 
-/// Represents the MEK Secret from OCP LOCK.
+/// Represents the MEK Secret Seed from OCP LOCK.
 /// This is constructed from the EPK + DPK.
 ///
 /// NOTE: MPKs will be mixed into `MekSecretSeed`
 pub struct MekSecretSeed;
 
-// TODO(clundin): Erase MEK_SECRETS KV on drop? That would require holding a reference to the
-// key vault for the lifetime of `MekSecretSeed`
 impl MekSecretSeed {
-    /// Consumes the EPK and DPK to produce a `MekSecretSeed`
-    fn new(hmac: &mut Hmac, trng: &mut Trng, epk: Epk, dpk: Dpk) -> CaliptraResult<Self> {
+    /// Consumes `Self` to produce a `MekSecret`.
+    fn derive_intermediate_mek_secret<'a>(
+        self,
+        hmac: &mut Hmac,
+        trng: &mut Trng,
+        kv: &'a mut KeyVault,
+    ) -> CaliptraResult<IntermediateMekSecret<'a>> {
         hmac_kdf(
             hmac,
-            HmacKey::Key(KeyReadArgs::new(KEY_ID_EPK)),
-            b"ocp_lock_mek_secret_seed",
-            Some(dpk.as_bytes()),
+            HmacKey::Key(KeyReadArgs::new(KEY_ID_MEK_SECRETS)),
+            b"ocp_lock_derived_mek",
+            None,
             trng,
             HmacTag::Key(KeyWriteArgs::new(
                 KEY_ID_MEK_SECRETS,
-                // TODO(clundin): Will likely need to add AES key usage in a follow up.
-                KeyUsage::default().set_hmac_key_en(),
+                KeyUsage::default().set_aes_key_en(),
             )),
             HmacMode::Hmac512,
         )?;
+        Ok(IntermediateMekSecret { kv })
+    }
+}
 
-        // Manually drop EPK to Erase EPK KV.
-        // This should happen automatically but otherwise the linter complains that the EPK wasn't
-        // used.
-        drop(epk);
-        Ok(Self)
+/// Represents the MEK Secret from OCP LOCK.
+/// This is constructed from `MekSecretSeed`
+pub struct IntermediateMekSecret<'a> {
+    kv: &'a mut KeyVault,
+}
+
+impl IntermediateMekSecret<'_> {
+    /// Consumes `Self` to produce a `MekSeed`.
+    /// NOTE: This will erase the `MEK_SECRET` key vault.
+    fn derive_mek_seed(self, aes: &mut Aes) -> CaliptraResult<MekSeed> {
+        Ok(MekSeed(cmac_kdf(
+            aes,
+            AesKey::KV(KeyReadArgs::new(KEY_ID_MEK_SECRETS)),
+            b"ocp_lock_mek_seed",
+            None,
+            4,
+        )?))
+    }
+}
+
+impl Drop for IntermediateMekSecret<'_> {
+    // From Spec:
+    //   > Held in the Key Vault and zeroized after each use.
+    fn drop(&mut self) {
+        // We don't set a write or use lock so this should never fail.
+        let _ = self.kv.erase_key(KEY_ID_MEK_SECRETS);
+    }
+}
+
+/// Checksum of an `Mek`
+#[derive(Default, IntoBytes, KnownLayout, Immutable, ZeroizeOnDrop, PartialEq, Eq)]
+pub struct MekChecksum(pub [u8; 16]);
+
+/// Represents the MEK Seed from OCP LOCK.
+/// This is constructed from `MekSecret`
+#[derive(IntoBytes, KnownLayout, Immutable, ZeroizeOnDrop)]
+pub struct MekSeed(LEArray4x16);
+impl MekSeed {
+    fn checksum(&self, aes: &mut Aes) -> CaliptraResult<MekChecksum> {
+        let key = self
+            .0
+            .as_bytes()
+            .get(0..32)
+            .ok_or(CaliptraError::RUNTIME_OCP_LOCK_INVALID_MEK_SEED_SIZE)?;
+        let key = LEArray4x8::ref_from_bytes(key)
+            .map_err(|_| CaliptraError::RUNTIME_OCP_LOCK_INVALID_MEK_SEED_SIZE)?;
+        let mut output = [0; 16];
+        aes.aes_256_ecb(
+            AesKey::Array(key),
+            AesOperation::Encrypt,
+            &[0; 16],
+            &mut output,
+        )?;
+        Ok(MekChecksum(output))
     }
 }
 
@@ -86,6 +142,28 @@ impl<'a> Epk<'a> {
         )?;
         Ok(Self { kv })
     }
+
+    /// Consumes `Self` and DPK to produce a `MekSecretSeed`
+    fn derive_secret_seed(
+        self,
+        hmac: &mut Hmac,
+        trng: &mut Trng,
+        dpk: Dpk,
+    ) -> CaliptraResult<MekSecretSeed> {
+        hmac_kdf(
+            hmac,
+            HmacKey::Key(KeyReadArgs::new(KEY_ID_EPK)),
+            b"ocp_lock_mek_secret_seed",
+            Some(dpk.as_bytes()),
+            trng,
+            HmacTag::Key(KeyWriteArgs::new(
+                KEY_ID_MEK_SECRETS,
+                KeyUsage::default().set_hmac_key_en(),
+            )),
+            HmacMode::Hmac512,
+        )?;
+        Ok(MekSecretSeed)
+    }
 }
 
 impl Drop for Epk<'_> {
@@ -105,7 +183,7 @@ pub struct OcpLockContext {
     /// HEK is available
     hek_available: bool,
 
-    /// Holds MEK secret seed, initialized by calling MEK_SECRET_INITIALIZE. Some commands do not work until
+    /// Holds MEK secret seed, initialized by calling INITIALIZE_MEK_SECRET. Some commands do not work until
     /// `mek_secret_seed` has a value.
     mek_secret_seed: Option<MekSecretSeed>,
 }
@@ -142,8 +220,41 @@ impl OcpLockContext {
             cfi_assert!(self.hek_available);
         }
         let epk = Epk::new(hmac, trng, kv, sek)?;
-        let mek_secret_seed = MekSecretSeed::new(hmac, trng, epk, dpk)?;
+        let mek_secret_seed = epk.derive_secret_seed(hmac, trng, dpk)?;
         self.mek_secret_seed = Some(mek_secret_seed);
         Ok(())
+    }
+
+    /// Derives an MEK
+    ///
+    /// NOTE: This operation will consume `mek_secret_seed` and erase the MEK secret key vault on
+    /// completion.
+    pub fn derive_mek(
+        &mut self,
+        aes: &mut Aes,
+        hmac: &mut Hmac,
+        trng: &mut Trng,
+        kv: &mut KeyVault,
+        expect_mek_checksum: MekChecksum,
+    ) -> CaliptraResult<MekChecksum> {
+        // After `mek_secret_seed` is consumed a new MEK cannot be created without first calling
+        // `OCP_LOCK_INITIALIZE_MEK_SECRET` so we take it.
+        let Some(mek_secret_seed) = self.mek_secret_seed.take() else {
+            return Err(CaliptraError::RUNTIME_OCP_LOCK_MEK_NOT_INITIALIZED);
+        };
+        let mek_secret = mek_secret_seed.derive_intermediate_mek_secret(hmac, trng, kv)?;
+        let mek_seed = mek_secret.derive_mek_seed(aes)?;
+        let checksum = mek_seed.checksum(aes)?;
+
+        // If `expect_mek_checksum` is all zeros, skip checking that the MEK checksum matches.
+        // `expect_mek_checksum` should match the `checksum` that was derived.
+        // Otherwise we need to report an error.
+        if expect_mek_checksum != MekChecksum::default() && expect_mek_checksum != checksum {
+            return Err(CaliptraError::RUNTIME_OCP_LOCK_MEK_CHKSUM_FAIL);
+        }
+
+        // Do not release an MEK with a failing checksum!
+        // TODO(clundin): MEK should be released to KV here.
+        Ok(checksum)
     }
 }
---
+++ runtime/tests/runtime_integration_tests/test_ocp_lock/test_derive_mek.rs
@@ -0,0 +1,218 @@
+// Licensed under the Apache-2.0 license
+
+use caliptra_api::mailbox::{
+    CommandId, MailboxReq, MailboxReqHeader, OcpLockDeriveMekReq, OcpLockDeriveMekResp,
+};
+use caliptra_error::CaliptraError;
+use caliptra_hw_model::{HwModel, ModelError};
+
+use crate::test_ocp_lock::InitializeMekSecretParams;
+
+use super::{boot_ocp_lock_runtime, OcpLockBootParams};
+
+use zerocopy::{FromBytes, IntoBytes};
+
+/// Temporary checksum. This will be calculated from the full keyladder.
+const EXPECTED_MEK_CHECKSUM: [u8; 16] = [
+    243, 22, 202, 155, 137, 240, 133, 174, 210, 161, 117, 101, 142, 173, 20, 197,
+];
+
+// TODO(clundin): Follow up with the following test cases:
+//
+// These follow ups are pending an independently constructed key ladder / splitting into smaller
+// PRs.
+//
+// * Release MEK and validate MEK contents.
+// * Validate MEK checksum is correct.
+// * MEK and MEK checksum are the same after a warm reset.
+// * MEK and MEK checksum are the same after a hitless update to new firmware.
+
+#[cfg_attr(not(feature = "fpga_subsystem"), ignore)]
+#[test]
+fn test_derive_mek() {
+    let mut model = boot_ocp_lock_runtime(OcpLockBootParams {
+        hek_available: true,
+        force_ocp_lock_en: true,
+        init_mek_secret_params: Some(InitializeMekSecretParams {
+            sek: [0xAB; 32],
+            dpk: [0xCD; 32],
+        }),
+        ..Default::default()
+    });
+
+    let mut cmd = MailboxReq::OcpLockDeriveMek(OcpLockDeriveMekReq {
+        hdr: MailboxReqHeader { chksum: 0 },
+        reserved: 0,
+        mek_checksum: EXPECTED_MEK_CHECKSUM,
+        ..Default::default()
+    });
+    cmd.populate_chksum().unwrap();
+
+    let response = model
+        .mailbox_execute(
+            CommandId::OCP_LOCK_DERIVE_MEK.into(),
+            cmd.as_bytes().unwrap(),
+        )
+        .unwrap()
+        .unwrap();
+    let response = OcpLockDeriveMekResp::ref_from_bytes(response.as_bytes()).unwrap();
+    assert_eq!(&response.mek_checksum, &EXPECTED_MEK_CHECKSUM);
+}
+
+#[cfg_attr(not(feature = "fpga_subsystem"), ignore)]
+#[test]
+fn test_derive_corrupted_sek() {
+    let mut model = boot_ocp_lock_runtime(OcpLockBootParams {
+        hek_available: true,
+        force_ocp_lock_en: true,
+        init_mek_secret_params: Some(InitializeMekSecretParams {
+            sek: [0xBE; 32],
+            dpk: [0xCD; 32],
+        }),
+        ..Default::default()
+    });
+
+    let mut cmd = MailboxReq::OcpLockDeriveMek(OcpLockDeriveMekReq {
+        hdr: MailboxReqHeader { chksum: 0 },
+        reserved: 0,
+        mek_checksum: EXPECTED_MEK_CHECKSUM,
+        ..Default::default()
+    });
+    cmd.populate_chksum().unwrap();
+
+    let response = model.mailbox_execute(
+        CommandId::OCP_LOCK_DERIVE_MEK.into(),
+        cmd.as_bytes().unwrap(),
+    );
+    assert_eq!(
+        response.unwrap_err(),
+        ModelError::MailboxCmdFailed(CaliptraError::RUNTIME_OCP_LOCK_MEK_CHKSUM_FAIL.into(),)
+    );
+}
+
+#[cfg_attr(not(feature = "fpga_subsystem"), ignore)]
+#[test]
+fn test_derive_corrupted_sek_no_checksum() {
+    let mut model = boot_ocp_lock_runtime(OcpLockBootParams {
+        hek_available: true,
+        force_ocp_lock_en: true,
+        init_mek_secret_params: Some(InitializeMekSecretParams {
+            sek: [0xBE; 32],
+            dpk: [0xCD; 32],
+        }),
+        ..Default::default()
+    });
+
+    let mut cmd = MailboxReq::OcpLockDeriveMek(OcpLockDeriveMekReq {
+        hdr: MailboxReqHeader { chksum: 0 },
+        reserved: 0,
+        ..Default::default()
+    });
+    cmd.populate_chksum().unwrap();
+
+    let response = model
+        .mailbox_execute(
+            CommandId::OCP_LOCK_DERIVE_MEK.into(),
+            cmd.as_bytes().unwrap(),
+        )
+        .unwrap()
+        .unwrap();
+    let response = OcpLockDeriveMekResp::ref_from_bytes(response.as_bytes()).unwrap();
+    assert_ne!(&response.mek_checksum, &EXPECTED_MEK_CHECKSUM);
+}
+
+#[cfg_attr(not(feature = "fpga_subsystem"), ignore)]
+#[test]
+fn test_derive_missing_secret_seed() {
+    let mut model = boot_ocp_lock_runtime(OcpLockBootParams {
+        hek_available: true,
+        force_ocp_lock_en: true,
+        ..Default::default()
+    });
+
+    let mut cmd = MailboxReq::OcpLockDeriveMek(OcpLockDeriveMekReq {
+        hdr: MailboxReqHeader { chksum: 0 },
+        reserved: 0,
+        mek_checksum: [0; 16],
+        ..Default::default()
+    });
+    cmd.populate_chksum().unwrap();
+
+    let response = model.mailbox_execute(
+        CommandId::OCP_LOCK_DERIVE_MEK.into(),
+        cmd.as_bytes().unwrap(),
+    );
+    assert_eq!(
+        response.unwrap_err(),
+        ModelError::MailboxCmdFailed(CaliptraError::RUNTIME_OCP_LOCK_MEK_NOT_INITIALIZED.into(),)
+    );
+}
+
+#[cfg_attr(not(feature = "fpga_subsystem"), ignore)]
+#[test]
+fn test_derive_consumed_secret_seed() {
+    let mut model = boot_ocp_lock_runtime(OcpLockBootParams {
+        hek_available: true,
+        force_ocp_lock_en: true,
+        init_mek_secret_params: Some(InitializeMekSecretParams {
+            sek: [0xAB; 32],
+            dpk: [0xCD; 32],
+        }),
+        ..Default::default()
+    });
+
+    let mut cmd = MailboxReq::OcpLockDeriveMek(OcpLockDeriveMekReq {
+        hdr: MailboxReqHeader { chksum: 0 },
+        reserved: 0,
+        mek_checksum: [0; 16],
+        ..Default::default()
+    });
+    cmd.populate_chksum().unwrap();
+
+    // Consumes the `MEK_SECRET_SEED` so `DERIVE_MEK` will not work until another call to `INITIALIZE_MEK_SECRET`
+    let response = model
+        .mailbox_execute(
+            CommandId::OCP_LOCK_DERIVE_MEK.into(),
+            cmd.as_bytes().unwrap(),
+        )
+        .unwrap()
+        .unwrap();
+    let response = OcpLockDeriveMekResp::ref_from_bytes(response.as_bytes()).unwrap();
+    assert_eq!(&response.mek_checksum, &EXPECTED_MEK_CHECKSUM);
+
+    let response = model.mailbox_execute(
+        CommandId::OCP_LOCK_DERIVE_MEK.into(),
+        cmd.as_bytes().unwrap(),
+    );
+    assert_eq!(
+        response.unwrap_err(),
+        ModelError::MailboxCmdFailed(CaliptraError::RUNTIME_OCP_LOCK_MEK_NOT_INITIALIZED.into(),)
+    );
+}
+
+#[cfg_attr(not(feature = "fpga_subsystem"), ignore)]
+#[test]
+fn test_derive_mek_missing_hek() {
+    let mut model = boot_ocp_lock_runtime(OcpLockBootParams {
+        hek_available: false,
+        force_ocp_lock_en: true,
+        ..Default::default()
+    });
+
+    let mut cmd = MailboxReq::OcpLockDeriveMek(OcpLockDeriveMekReq {
+        hdr: MailboxReqHeader { chksum: 0 },
+        reserved: 0,
+        mek_checksum: [0; 16],
+        ..Default::default()
+    });
+    cmd.populate_chksum().unwrap();
+
+    let response = model.mailbox_execute(
+        CommandId::OCP_LOCK_DERIVE_MEK.into(),
+        cmd.as_bytes().unwrap(),
+    );
+    assert_eq!(
+        response.unwrap_err(),
+        ModelError::MailboxCmdFailed(CaliptraError::RUNTIME_OCP_LOCK_MEK_NOT_INITIALIZED.into(),)
+    );
+}
