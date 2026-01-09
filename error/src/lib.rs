27a8be33cc3bf3fc6bc52808071800147e69f91e
Zach Halvorsen <zhalvorsen@google.com>
2026-01-08 17:23:15 -0800

    [dpe] Use both RT PCRs in RT DPE context
    Fixes #2775
    
    The original design for DPE only considered only having one measurement
    per context. Later a design change added another to accomodate current
    and journey measurements. This was not reflected in the RT DPE context
    so journey was used for both measurements. This was partially due to the
    fact that the RT current measurement is also already reported in the RT
    alias certificate.
    
    This changes 2.x to report both current and journey measurements in its
    DPE context. A similar change will be added to 1.x as tracked in #3027.

--- error/src/lib.rs
+++ error/src/lib.rs
@@ -1563,6 +1563,11 @@ impl CaliptraError {
             0x000E0073,
             "Runtime Error: Reallocate DPE context requested fewer PL1 contexts than are used currently"
         ),
+        (
+            RUNTIME_RT_CURRENT_PCR_VALIDATION_FAILED,
+            0x000E0074,
+            "Runtime Error: RT current PCR validation failed"
+        ),
         // FMC Errors
         (FMC_GLOBAL_NMI, 0x000F0001, "FMC Error: Global NMI"),
         (
--- runtime/tests/runtime_integration_tests/test_update_reset.rs
+++ runtime/tests/runtime_integration_tests/test_update_reset.rs
@@ -50,7 +50,7 @@ pub fn mbox_test_image() -> &'static FwId<'static> {
 }
 
 #[test]
-fn test_rt_journey_pcr_updated_in_dpe() {
+fn test_rt_pcr_updated_in_dpe() {
     let image_options = ImageOptions {
         pqc_key_type: FwVerificationPqcKeyType::LMS,
         ..Default::default()
@@ -71,10 +71,18 @@ fn test_rt_journey_pcr_updated_in_dpe() {
     let rt_journey_pcr_resp = model.mailbox_execute(0x1000_0000, &[]).unwrap().unwrap();
     let rt_journey_pcr: [u8; 48] = rt_journey_pcr_resp.as_bytes().try_into().unwrap();
 
-    let dpe_root_measurement_resp = model.mailbox_execute(0x6000_0000, &[]).unwrap().unwrap();
+    let dpe_root_measurement_resp = model.mailbox_execute(0x6000_0001, &[]).unwrap().unwrap();
     let dpe_root_measurement: [u8; 48] = dpe_root_measurement_resp.as_bytes().try_into().unwrap();
 
     assert_eq!(dpe_root_measurement, rt_journey_pcr);
+
+    let rt_current_pcr_resp = model.mailbox_execute(0x1000_0001, &[]).unwrap().unwrap();
+    let rt_current_pcr: [u8; 48] = rt_current_pcr_resp.as_bytes().try_into().unwrap();
+
+    let dpe_root_measurement_resp = model.mailbox_execute(0x6000_0000, &[]).unwrap().unwrap();
+    let dpe_root_measurement: [u8; 48] = dpe_root_measurement_resp.as_bytes().try_into().unwrap();
+
+    assert_eq!(dpe_root_measurement, rt_current_pcr);
 }
 
 #[test]
