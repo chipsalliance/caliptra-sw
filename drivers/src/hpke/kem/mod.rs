d877acf805fd41b91cdfc07a58554c5d4a96cc18
Carl Lundin <clundin@google.com>
2026-01-13 14:26:32 -0800

    [hpke]: Zeroize SharedSecret on drop

--- drivers/src/hpke/kem/mod.rs
+++ drivers/src/hpke/kem/mod.rs
@@ -42,6 +42,7 @@ pub trait Kem<const NSK: usize, const NENC: usize, const NPK: usize, const NSECR
 }
 
 /// Shared Secret produced by `encap`.
+#[derive(ZeroizeOnDrop)]
 pub struct SharedSecret<const NSECRET: usize> {
     buf: [u8; NSECRET],
 }
