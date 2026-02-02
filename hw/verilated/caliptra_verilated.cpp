1dae97281d89f74c4a1b4acfa92c67516c2e52bc
Arthur Heymans <arthur.heymans@9elements.com>
2026-02-02 09:58:01 +0100

    Restore Verilator support for Caliptra 2.0
    Update the Verilator integration to work with Caliptra 2.0 RTL changes:
    
    - Replace APB interface with AXI subordinate interface
    - Add Adams Bridge (ABR) memory SRAMs for ML-DSA/ML-KEM support
      using proper 1R1W dual-port RAMs (abr_1r1w_ram, abr_1r1w_be_ram)
    - Add cptra_csr_hmac_key input signal support
    - Update security_state from 4-bit to security_state_t struct
    - Rename ready_for_fw_push to ready_for_mb_processing
    - Add ECC signal connections for ICCM/DCCM memory interfaces
    - Add Axi4PC.sv empty stub for AXI Protocol Checker
    - Add --timing flag and warning suppressions to Makefile
    - Add debug output for firmware error registers on test failure
    
    Tested with ML-KEM driver tests which now pass.

--- hw/verilated/caliptra_verilated.cpp
+++ hw/verilated/caliptra_verilated.cpp
@@ -16,6 +16,7 @@ struct caliptra_verilated* caliptra_verilated_new(struct caliptra_verilated_init
   auto result = new caliptra_verilated();
   result->v.security_state = init_args->security_state;
   memcpy(result->v.cptra_obf_key, init_args->cptra_obf_key, sizeof(result->v.cptra_obf_key));
+  memcpy(result->v.cptra_csr_hmac_key, init_args->cptra_csr_hmac_key, sizeof(result->v.cptra_csr_hmac_key));
   return result;
 }
 void caliptra_verilated_destroy(struct caliptra_verilated* model) {
@@ -53,18 +54,44 @@ void caliptra_verilated_eval(struct caliptra_verilated* model,
   v->cptra_pwrgood = in->cptra_pwrgood;
   v->cptra_rst_b = in->cptra_rst_b;
 
-  v->paddr = in->paddr;
-  v->pprot = in->pprot;
-  v->psel = in->psel;
-  v->penable = in->penable;
-  v->pwrite = in->pwrite;
-  v->pwdata = in->pwdata;
-  v->pauser = in->pauser;
+  // AXI Write Address Channel
+  v->s_axi_awid = in->s_axi_awid;
+  v->s_axi_awaddr = in->s_axi_awaddr;
+  v->s_axi_awburst = in->s_axi_awburst;
+  v->s_axi_awsize = in->s_axi_awsize;
+  v->s_axi_awlen = in->s_axi_awlen;
+  v->s_axi_awuser = in->s_axi_awuser;
+  v->s_axi_awvalid = in->s_axi_awvalid;
+  v->s_axi_awlock = in->s_axi_awlock;
 
+  // AXI Write Data Channel
+  v->s_axi_wdata = in->s_axi_wdata;
+  v->s_axi_wstrb = in->s_axi_wstrb;
+  v->s_axi_wvalid = in->s_axi_wvalid;
+  v->s_axi_wlast = in->s_axi_wlast;
+
+  // AXI Write Response Channel
+  v->s_axi_bready = in->s_axi_bready;
+
+  // AXI Read Address Channel
+  v->s_axi_arid = in->s_axi_arid;
+  v->s_axi_araddr = in->s_axi_araddr;
+  v->s_axi_arburst = in->s_axi_arburst;
+  v->s_axi_arsize = in->s_axi_arsize;
+  v->s_axi_arlen = in->s_axi_arlen;
+  v->s_axi_aruser = in->s_axi_aruser;
+  v->s_axi_arvalid = in->s_axi_arvalid;
+  v->s_axi_arlock = in->s_axi_arlock;
+
+  // AXI Read Data Channel
+  v->s_axi_rready = in->s_axi_rready;
+
+  // ROM backdoor write
   v->ext_imem_we = in->imem_we;
   v->ext_imem_addr = in->imem_addr;
   v->ext_imem_wdata = in->imem_wdata;
 
+  // SRAM backdoor writes
   v->ext_dccm_we = in->ext_dccm_we;
   v->ext_iccm_we = in->ext_iccm_we;
   v->ext_mbox_we = in->ext_mbox_we;
@@ -75,6 +102,7 @@ void caliptra_verilated_eval(struct caliptra_verilated* model,
   v->ext_xccm_wdata[3] = in->ext_xccm_wdata[3];
   v->ext_xccm_wdata[4] = in->ext_xccm_wdata[4];
 
+  // TRNG
   v->itrng_data = in->itrng_data;
   v->itrng_valid = in->itrng_valid;
 
@@ -84,30 +112,34 @@ void caliptra_verilated_eval(struct caliptra_verilated* model,
     model->tfp->dump(model->sim_time++);
   }
 
+  // Status outputs
   out->ready_for_fuses = v->ready_for_fuses;
-  out->ready_for_fw_push = v->ready_for_fw_push;
+  out->ready_for_mb_processing = v->ready_for_mb_processing;
 
-  out->pready = v->pready;
-  out->pslverr = v->pslverr;
-  out->prdata = v->prdata;
+  // AXI Write Address Channel
+  out->s_axi_awready = v->s_axi_awready;
+
+  // AXI Write Data Channel
+  out->s_axi_wready = v->s_axi_wready;
+
+  // AXI Write Response Channel
+  out->s_axi_bid = v->s_axi_bid;
+  out->s_axi_bresp = v->s_axi_bresp;
+  out->s_axi_bvalid = v->s_axi_bvalid;
+
+  // AXI Read Address Channel
+  out->s_axi_arready = v->s_axi_arready;
+
+  // AXI Read Data Channel
+  out->s_axi_rid = v->s_axi_rid;
+  out->s_axi_rdata = v->s_axi_rdata;
+  out->s_axi_rresp = v->s_axi_rresp;
+  out->s_axi_rlast = v->s_axi_rlast;
+  out->s_axi_rvalid = v->s_axi_rvalid;
 
   out->generic_output_wires = v->generic_output_wires;
 
   out->etrng_req = v->etrng_req;
 
-  out->uc_haddr = v->uc_haddr;
-  out->uc_hburst = v->uc_hburst;
-  out->uc_hmastlock = v->uc_hmastlock;
-  out->uc_hprot = v->uc_hprot;
-  out->uc_hsize = v->uc_hsize;
-  out->uc_htrans = v->uc_htrans;
-  out->uc_hwrite = v->uc_hwrite;
-  out->uc_hwdata = v->uc_hwdata;
-
-  out->uc_hrdata = v->uc_hrdata;
-  out->uc_hready = v->uc_hready;
-  out->uc_hresp = v->uc_hresp;
-
   out->cptra_error_fatal = v->cptra_error_fatal;
 }
-
