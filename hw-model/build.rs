14126272599d52a020118ffe5b2b1663bc5f5f3f
Chris Hinson <chris@hinson.dev>
2026-02-05 21:00:51 -0500

    Fix relative paths in build.rs

--- hw-model/build.rs
+++ hw-model/build.rs
@@ -1,8 +1,7 @@
 // Licensed under the Apache-2.0 license
 
 fn main() {
-    println!(
-        "cargo:rustc-env=OPENOCD_SYSFSGPIO_ADAPTER_CFG=../../../hw/fpga/openocd_sysfsgpio_adapter.cfg"
-    );
-    println!("cargo:rustc-env=OPENOCD_TAP_CFG=../../../hw/fpga/openocd_ss.cfg");
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    println!("cargo:rustc-env=OPENOCD_SYSFSGPIO_ADAPTER_CFG={manifest_dir}/../hw/fpga/openocd_sysfsgpio_adapter.cfg");
+    println!("cargo:rustc-env=OPENOCD_TAP_CFG={manifest_dir}/../hw/fpga/openocd_ss.cfg");
 }
