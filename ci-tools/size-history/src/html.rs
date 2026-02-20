8b7dfabed69b5b819671c816abe77f1ab09ae2bc
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2026-02-19 16:54:43 -0800

    [size-history]: Add OCP LOCK size history (#3352)

--- ci-tools/size-history/src/html.rs
+++ ci-tools/size-history/src/html.rs
@@ -11,13 +11,13 @@ use crate::{git, SizeRecord, Sizes};
 // The GitHub "HTML sanitizer" is incredibly sensitive to whitespace; do not attempt to break newlines.
 static TEMPLATE: &str = r#"
 <table>
-  <tr><th>Commit</th><th>Author</th><th>Commit</th><th>ROM prod size</th><th>ROM with-uart size</th><th>FMC size</th><th>App size</th></tr>
+  <tr><th>Commit</th><th>Author</th><th>Commit</th><th>ROM prod size</th><th>ROM with-uart size</th><th>FMC size</th><th>App size</th><th>App with OCP LOCK size</th></tr>
 {{ for record in records }}
   <tr>
     <td><a href="https://github.com/chipsalliance/caliptra-sw/commit/{ record.commit.id }">{ record.commit.id | trim_8 }</a></td>
     <td>{ record.commit.author | name_only }</td>
     {{ if record.important }}<td><strong>{ record.commit.title }</strong></td>{{ else }}<td>{ record.commit.title }</td>{{ endif }}
-    <td>{{ if record.sizes.rom_size_prod }}{ record.sizes.rom_size_prod.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.rom_size_prod.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.rom_size_with_uart }}{ record.sizes.rom_size_with_uart.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.rom_size_with_uart.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.fmc_size_with_uart }}{ record.sizes.fmc_size_with_uart.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.fmc_size_with_uart.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.app_size_with_uart }}{ record.sizes.app_size_with_uart.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.app_size_with_uart.delta | delta_format }){{ else }}build error{{ endif }}</td>
+    <td>{{ if record.sizes.rom_size_prod }}{ record.sizes.rom_size_prod.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.rom_size_prod.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.rom_size_with_uart }}{ record.sizes.rom_size_with_uart.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.rom_size_with_uart.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.fmc_size_with_uart }}{ record.sizes.fmc_size_with_uart.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.fmc_size_with_uart.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.app_size_with_uart }}{ record.sizes.app_size_with_uart.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.app_size_with_uart.delta | delta_format }){{ else }}build error{{ endif }}</td><td>{{ if record.sizes.app_size_with_uart_ocp_lock }}{ record.sizes.app_size_with_uart_ocp_lock.total }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>({ record.sizes.app_size_with_uart_ocp_lock.delta | delta_format }){{ else }}build error{{ endif }}</td>
   </tr>
 {{ endfor }}
 </table>
@@ -44,6 +44,10 @@ pub(crate) fn format_records(records: &[SizeRecord]) -> io::Result<String> {
             app_size_with_uart: ExtendedSizeInfo::from_change(
                 last_sizes.app_size_with_uart,
                 record.sizes.app_size_with_uart,
+            ),
+            app_size_with_uart_ocp_lock: ExtendedSizeInfo::from_change(
+                last_sizes.app_size_with_uart_ocp_lock,
+                record.sizes.app_size_with_uart_ocp_lock,
             ),
         };
         let mut ext_record = ExtendedRecord {
@@ -102,6 +106,7 @@ fn is_important(sizes: &ExtendedSizes) -> bool {
         || has_delta(&sizes.rom_size_with_uart)
         || has_delta(&sizes.fmc_size_with_uart)
         || has_delta(&sizes.app_size_with_uart)
+        || has_delta(&sizes.app_size_with_uart_ocp_lock)
 }
 fn name_only(val: &str) -> &str {
     if let Some((name, _)) = val.split_once('<') {
@@ -138,6 +143,7 @@ struct ExtendedSizes {
     rom_size_with_uart: Option<ExtendedSizeInfo>,
     fmc_size_with_uart: Option<ExtendedSizeInfo>,
     app_size_with_uart: Option<ExtendedSizeInfo>,
+    app_size_with_uart_ocp_lock: Option<ExtendedSizeInfo>,
 }
 
 #[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
