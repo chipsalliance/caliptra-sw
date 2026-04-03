8554a6db69bde921965f2917ee2bb130ae614dc0
Parvathi Bhogaraju <bhogaraju.parvathi@gmail.com>
2026-04-03 14:42:51 -0700

    add missing DICE EKU extension to RT alias certs (#3202)
    Cherry-picked RT alias changes from original commit eaf3fa41.
     Excludes IDevID, LDevID, and FMC alias changes that affect caliptra ROM.
    
     - Add TCG_DICE_KP_ECA extended key usage to RT alias ECC and ML-DSA certs
     - Regenerate X509 templates with the new EKU extension
     - Update DPE submodule to 769ae8d (ECA EKU verification fix)

--- fmc/tests/fmc_integration_tests/test_rtalias.rs
+++ fmc/tests/fmc_integration_tests/test_rtalias.rs
@@ -68,12 +68,14 @@ fn test_boot_status_reporting() {
         )
         .unwrap();
 
-        hw.step_until_boot_status(RT_ALIAS_MEASUREMENT_COMPLETE, true);
-        hw.step_until_boot_status(RT_ALIAS_DERIVED_CDI_COMPLETE, true);
-        hw.step_until_boot_status(RT_ALIAS_KEY_PAIR_DERIVATION_COMPLETE, true);
-        hw.step_until_boot_status(RT_ALIAS_SUBJ_ID_SN_GENERATION_COMPLETE, true);
-        hw.step_until_boot_status(RT_ALIAS_SUBJ_KEY_ID_GENERATION_COMPLETE, true);
-        hw.step_until_boot_status(RT_ALIAS_CERT_SIG_GENERATION_COMPLETE, true);
+        if cfg!(not(feature = "fpga_realtime")) {
+            hw.step_until_boot_status(RT_ALIAS_MEASUREMENT_COMPLETE, true);
+            hw.step_until_boot_status(RT_ALIAS_DERIVED_CDI_COMPLETE, true);
+            hw.step_until_boot_status(RT_ALIAS_KEY_PAIR_DERIVATION_COMPLETE, true);
+            hw.step_until_boot_status(RT_ALIAS_SUBJ_ID_SN_GENERATION_COMPLETE, true);
+            hw.step_until_boot_status(RT_ALIAS_SUBJ_KEY_ID_GENERATION_COMPLETE, true);
+            hw.step_until_boot_status(RT_ALIAS_CERT_SIG_GENERATION_COMPLETE, true);
+        }
         hw.step_until_boot_status(RT_ALIAS_DERIVATION_COMPLETE, true);
     }
 }
