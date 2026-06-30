9680b96c7435dbe5a256300dd4fa287450ecefca
Marvin Gudel <marvin.gudel@9elements.com>
2026-06-30 16:57:38 +0200

    Introduce feature flags for HW revisions in register crate

--- registers/src/lib.rs
+++ registers/src/lib.rs
@@ -2,4 +2,14 @@
 //
 #![no_std]
 
+use mutually_exclusive_features::exactly_one_of;
+exactly_one_of!("latest", "rev-2_0", "rev-2_1");
+
+#[cfg(feature = "latest")]
 pub use caliptra_registers_latest::*;
+
+#[cfg(feature = "rev-2_1")]
+compile_error!("TODO: add v2.1 HW register definitions");
+
+#[cfg(feature = "rev-2_0")]
+compile_error!("TODO: add v2.0 HW register definitions");
