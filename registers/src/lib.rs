8c4d36671f386b9911a17922fceb0d31ab83fe25
Marvin Gudel <marvin.gudel@9elements.com>
2026-07-07 16:29:46 +0200

    Use custom cfg for HW revision selection

--- registers/src/lib.rs
+++ registers/src/lib.rs
@@ -1,15 +1,18 @@
 // Licensed under the Apache-2.0 license
 //
 #![no_std]
+#![cfg_attr(hw_rev = "latest", doc = "Hardware revision: _latest_")]
+#![cfg_attr(hw_rev = "2.1", doc = "Hardware revision: _2.1_")]
+#![cfg_attr(hw_rev = "2.0", doc = "Hardware revision: _2.0_")]
 
-use mutually_exclusive_features::exactly_one_of;
-exactly_one_of!("latest", "rev-2_0", "rev-2_1");
+#[cfg(not(any(hw_rev = "latest", hw_rev = "2.1", hw_rev = "2.0")))]
+compile_error!("Select one of the supported HW revisions by setting the `hw_rev` cfg");
 
-#[cfg(feature = "latest")]
+#[cfg(hw_rev = "latest")]
 pub use caliptra_registers_latest::*;
 
-#[cfg(feature = "rev-2_1")]
-compile_error!("TODO: add v2.1 HW register definitions");
+#[cfg(hw_rev = "2.1")]
+pub use caliptra_registers_rev_2_1::*;
 
-#[cfg(feature = "rev-2_0")]
+#[cfg(hw_rev = "2.0")]
 compile_error!("TODO: add v2.0 HW register definitions");
