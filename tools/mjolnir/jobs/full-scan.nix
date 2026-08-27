c321d4c5f9f133d3efade0e4c55fd3bd88824e0d
Collin MacDonald <cmacd@google.com>
2026-08-27 15:14:20 -0700

    [mjolnir]: enable full scan workflow

---
+++ tools/mjolnir/jobs/full-scan.nix
@@ -0,0 +1,12 @@
+# Licensed under the Apache-2.0 license
+# SPDX-License-Identifier: Apache-2.0
+{
+  name = "Full Scan";
+  localDir = ".";
+  srcDirs = [
+    "runtime/src"
+    "drivers/src"
+    "rom/dev/src"
+    "fmc/src"
+  ];
+}
