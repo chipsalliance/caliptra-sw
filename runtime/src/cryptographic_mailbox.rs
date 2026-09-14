911a8e9fbaa55db93b8c67699a336df7e6410b7f
Marvin Gudel <marvin.gudel@9elements.com>
2026-09-14 12:08:30 +0200

    Allow ML-KEM keys in HKDF
    Allow 32 bytes IKM as HKDF input, to be able
    to use 32 byte ML-KEM keys in HKDF.
    Additionally different IKM length are now allowed
    regardless of selected hash algorithm.
    
    Signed-off-by: Marvin Gudel <marvin.gudel@9elements.com>

--- runtime/src/cryptographic_mailbox.rs
+++ runtime/src/cryptographic_mailbox.rs
@@ -2017,10 +2017,8 @@ impl Commands {
         let ikm = Self::decrypt_hmac_key(drivers, &cmd.ikm)?;
         let salt = Self::decrypt_hmac_key(drivers, &cmd.salt)?;
 
-        match (cm_hash_algorithm, ikm.length) {
-            (CmHashAlgorithm::Sha384, 48) => {}
-            (CmHashAlgorithm::Sha512, 64) => {}
-            _ => return Err(CaliptraError::RUNTIME_MAILBOX_INVALID_PARAMS)?,
+        if !matches!(ikm.length, 32 | 48 | 64) {
+            return Err(CaliptraError::RUNTIME_MAILBOX_INVALID_PARAMS)?;
         }
 
         let mut unencrypted_cmk = UnencryptedCmk {
